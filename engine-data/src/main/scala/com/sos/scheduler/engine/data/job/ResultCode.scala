614f7acb54fb797e2856bae6358e3902a1738921
Joacim Zschimmer <jogit@zschimmer.com>
2015-01-08 09:16:40 +0100

    chmod +x for moved scripts in engine-kernelcpp/make

---
+++ engine-data/src/main/scala/com/sos/scheduler/engine/data/job/ResultCode.scala
@@ -0,0 +1,14 @@
+package com.sos.scheduler.engine.data.job
+
+import ResultCode._
+import com.sos.scheduler.engine.common.scalautil.Logger
+
+/**
+ * @author Joacim Zschimmer
+ */
+final class ResultCode {
+}
+
+private object ResultCode {
+  private val logger = Logger(getClass)
+}
