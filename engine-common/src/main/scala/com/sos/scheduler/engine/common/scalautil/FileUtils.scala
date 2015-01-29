789d4c05d502a9d572d5d1febb4a3caa2681668e
Joacim Zschimmer <jogit@zschimmer.com>
2015-01-29 18:32:04 +0100

    FileUtils for Path

--- engine-common/src/main/scala/com/sos/scheduler/engine/common/scalautil/FileUtils.scala
+++ engine-common/src/main/scala/com/sos/scheduler/engine/common/scalautil/FileUtils.scala
@@ -1,7 +1,7 @@
 package com.sos.scheduler.engine.common.scalautil
 
 import com.google.common.base.Charsets.UTF_8
-import com.google.common.io.Files
+import com.google.common.io.{Files ⇒ GuavaFiles}
 import java.io.File
 import java.nio.charset.Charset
 import java.nio.file.Path
@@ -9,27 +9,48 @@ import scala.language.implicitConversions
 
 object FileUtils {
   object implicits {
-    implicit def pathToFile(o: Path): File = o.toFile
+    implicit def pathToFile(path: Path): File = path.toFile
 
-    implicit def fileToPath(o: File): Path = o.toPath
+    implicit def fileToPath(file: File): Path = file.toPath
+
+    implicit class RichPath(val delegate: Path) extends AnyVal {
+
+      def /(filename: String): Path = new File(delegate, filename).toFile
+
+      def contentBytes: Array[Byte] = file.contentBytes
+
+      def contentBytes_=(o: Array[Byte]): Unit = file.contentBytes = o
+
+      def contentString: String = file.contentString
+
+      def contentString_=(o: String): Unit = file.contentString = o
+
+      def contentString(encoding: Charset) = file.contentString(encoding)
+
+      def write(string: String, encoding: Charset = UTF_8): Unit =  file.write(string, encoding)
+
+      def append(o: String, encoding: Charset = UTF_8): Unit = file.append(o, encoding)
+
+      private def file = delegate.toFile
+    }
 
     implicit class RichFile(val delegate: File) extends AnyVal {
 
       def /(filename: String) = new File(delegate, filename)
 
-      def contentBytes: Array[Byte] = Files.toByteArray(delegate)
+      def contentBytes: Array[Byte] = GuavaFiles.toByteArray(delegate)
 
-      def contentBytes_=(o: Array[Byte]): Unit = Files.write(o, delegate)
+      def contentBytes_=(o: Array[Byte]): Unit = GuavaFiles.write(o, delegate)
 
       def contentString: String = contentString(UTF_8)
 
       def contentString_=(o: String): Unit = write(o, UTF_8)
 
-      def contentString(encoding: Charset) = Files.toString(delegate, encoding)
+      def contentString(encoding: Charset) = GuavaFiles.toString(delegate, encoding)
 
-      def write(string: String, encoding: Charset = UTF_8): Unit =  Files.write(string, delegate, encoding)
+      def write(string: String, encoding: Charset = UTF_8): Unit =  GuavaFiles.write(string, delegate, encoding)
 
-      def append(o: String, encoding: Charset = UTF_8): Unit = Files.append(o, delegate, encoding)
+      def append(o: String, encoding: Charset = UTF_8): Unit = GuavaFiles.append(o, delegate, encoding)
     }
   }
 }
--- engine-common/src/test/scala/com/sos/scheduler/engine/common/scalautil/FileUtilTest.scala
+++ engine-common/src/test/scala/com/sos/scheduler/engine/common/scalautil/FileUtilTest.scala
@@ -17,10 +17,9 @@ import org.scalatest.{BeforeAndAfterAll, FreeSpec}
 final class FileUtilTest extends FreeSpec with BeforeAndAfterAll {
 
   private lazy val file = Files.createTempFile("FileUtilTest-", ".tmp").toFile
+  private lazy val path = file.toPath
 
-  override def afterAll(): Unit = {
-    Files.delete(file)
-  }
+  override def afterAll() = Files.delete(file)
 
   "implicit fileToPath" in {
     new File("/a"): Path
@@ -30,30 +29,60 @@ final class FileUtilTest extends FreeSpec with BeforeAndAfterAll {
     new File("/a").toPath: File
   }
 
-  "slash" in {
-    assert(new File("/a") / "b" == new File("/a", "b"))
+  "File extention methods" - {
+    "slash" in {
+      assert(new File("/a") / "b" == new File("/a", "b"))
+    }
+
+    "contentString" in {
+      file.contentString = TestString
+      file.contentString shouldEqual TestString
+      new String(Files.readAllBytes(file), UTF_8) shouldEqual TestString
+    }
+
+    "contentBytes" in {
+      file.contentBytes shouldEqual TestBytes
+      file.contentBytes = Array[Byte](1, 2)
+      file.contentBytes shouldEqual Vector[Byte](1, 2)
+    }
+
+    "write" in {
+      file.write(TestString, UTF_16BE)
+      file.contentBytes shouldEqual TestString.getBytes(UTF_16BE)
+    }
+
+    "append" in {
+      file.append("X", UTF_16BE)
+      file.contentString(UTF_16BE) shouldEqual TestString + "X"
+    }
   }
 
-  "contentString" in {
-    file.contentString = TestString
-    file.contentString shouldEqual TestString
-    new String(Files.readAllBytes(file), UTF_8) shouldEqual TestString
-  }
+  "Path extention methods" - {
+    "slash" in {
+      assert(new File("/a").toPath / "b" == new File("/a", "b").toPath)
+    }
 
-  "contentBytes" in {
-    file.contentBytes shouldEqual TestBytes
-    file.contentBytes = Array[Byte](1, 2)
-    file.contentBytes shouldEqual Vector[Byte](1, 2)
-  }
+    "contentString" in {
+      path.contentString = TestString
+      path.contentString shouldEqual TestString
+      new String(Files.readAllBytes(path), UTF_8) shouldEqual TestString
+    }
 
-  "write" in {
-    file.write(TestString, UTF_16BE)
-    file.contentBytes shouldEqual TestString.getBytes(UTF_16BE)
-  }
+    "contentBytes" in {
+      path.contentBytes shouldEqual TestBytes
+      path.contentBytes = Array[Byte](1, 2)
+      path.contentBytes shouldEqual Vector[Byte](1, 2)
+    }
 
-  "append" in {
-    file.append("X", UTF_16BE)
-    file.contentString(UTF_16BE) shouldEqual TestString + "X"
+    "write" in {
+      path.write(TestString, UTF_16BE)
+      path.contentBytes shouldEqual TestString.getBytes(UTF_16BE)
+    }
+
+    "append" in {
+      path.append("X", UTF_16BE)
+      path.contentString(UTF_16BE) shouldEqual TestString + "X"
+    }
   }
 }
 
