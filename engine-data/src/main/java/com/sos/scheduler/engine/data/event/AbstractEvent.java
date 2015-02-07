76b0e64fbc4bb68fc76d5e5f9f551066c62b0ce0
Joacim Zschimmer <jogit@zschimmer.com>
2015-02-07 09:58:27 +0100

    - AbstractEvent.toString removed (override Scala's generated case class toString)

--- engine-data/src/main/java/com/sos/scheduler/engine/data/event/AbstractEvent.java
+++ engine-data/src/main/java/com/sos/scheduler/engine/data/event/AbstractEvent.java
@@ -1,7 +1,4 @@
 package com.sos.scheduler.engine.data.event;
 
 public abstract class AbstractEvent implements Event {
-    @Override public String toString() {
-        return getClass().getSimpleName();
-    }
 }
