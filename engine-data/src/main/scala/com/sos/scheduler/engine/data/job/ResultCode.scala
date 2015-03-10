e8cab870806768455e37cebcb1593f3567f2abab
Joacim Zschimmer <jogit@zschimmer.com>
2015-03-10 10:01:01 +0100

    JS-1291: SchedulerLogger moved to engine-data, PrefixLog.scala

--- engine-data/src/main/scala/com/sos/scheduler/engine/data/job/ResultCode.scala
+++ engine-data/src/main/scala/com/sos/scheduler/engine/data/job/ResultCode.scala
@@ -3,4 +3,6 @@ package com.sos.scheduler.engine.data.job
 /**
  * @author Joacim Zschimmer
  */
-final case class ResultCode(value: Int)
+final case class ResultCode(value: Int) {
+  def isSuccess = value == 0
+}
