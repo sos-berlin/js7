aea1c852379ac51f51202b2c387680f22379026d
Joacim Zschimmer <jogit@zschimmer.com>
2015-03-16 12:42:06 +0100

    JS-1291 JavaProcess

---
+++ engine-taskserver/src/main/scala/com/sos/scheduler/engine/taskserver/task/JavaProcess.scala
@@ -0,0 +1,40 @@
+package com.sos.scheduler.engine.taskserver.task
+
+import com.sos.scheduler.engine.common.scalautil.FileUtils.implicits._
+import com.sos.scheduler.engine.common.scalautil.ScalaUtils.RichAny
+import com.sos.scheduler.engine.common.system.OperatingSystem._
+import com.sos.scheduler.engine.taskserver.task.process.RichProcess
+import java.io.File
+
+/**
+ * @author Joacim Zschimmer
+ */
+object JavaProcess {
+  lazy val OwnClasspath = systemProperty("java.class.path")
+  private lazy val JavaHome = new File(systemProperty("java.home"))
+
+  def startJava(options: Seq[String], classpath: Option[String], mainClass: String, arguments: Seq[String]) =
+    RichProcess.start(
+      arguments = Vector(JavaExecutable.getPath) ++
+        options ++
+        (classpath.toVector flatMap { o ⇒ Vector("-classpath", o.substitute("" → File.pathSeparator)) }) ++    // java does not like empty classpath
+        Vector(mainClass) ++
+        arguments,
+      infoProgramFile = JavaExecutable)
+
+  private lazy val JavaExecutable: File = {
+    // See http://stackoverflow.com/questions/4421658/how-can-i-start-a-second-java-process
+    val result = if (isWindows) WindowsJava else UnixJava
+    if (!result.exists || result.isDirectory) throw new RuntimeException(s"Missing Java executable expected at $result")
+    result
+  }
+
+  private def WindowsJava = JavaHome / "bin/java.exe"   // Without sufficient memory, javaw.exe blocks with a Windows message box "Could not create the Java Virtual Machine"
+  private def UnixJava = JavaHome / "bin/java"
+
+  private def systemProperty(name: String): String =
+    sys.props(name) match {
+      case null ⇒ throw new RuntimeException(s"Missing Java system property '$name'")
+      case o ⇒ o
+    }
+}
---
+++ engine-taskserver/src/test/scala/com/sos/scheduler/engine/taskserver/task/JavaProcessTest.scala
@@ -0,0 +1,54 @@
+package com.sos.scheduler.engine.taskserver.task
+
+import com.sos.scheduler.engine.common.scalautil.Logger
+import com.sos.scheduler.engine.common.time.Stopwatch
+import com.sos.scheduler.engine.data.job.ResultCode
+import com.sos.scheduler.engine.taskserver.task.JavaProcessTest._
+import java.lang.System.{err, exit, out}
+import org.junit.runner.RunWith
+import org.scalatest.FreeSpec
+import org.scalatest.junit.JUnitRunner
+import scala.collection.mutable
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
+
+/**
+ * @author Joacim Zschimmer
+ */
+@RunWith(classOf[JUnitRunner])
+final class JavaProcessTest extends FreeSpec {
+  "JavaProcess" in {
+    val lines = mutable.Buffer[String]()
+    val stopwatch = new Stopwatch
+    val process = JavaProcess.startJava(
+      options = List("-Xmx10m", s"-Dtest=$TestValue"),
+      classpath = Some(JavaProcess.OwnClasspath),
+      mainClass = JavaProcessTest.getClass.getName stripSuffix "$",   // Scala object class name ends with '$'
+      arguments = Arguments)
+    try {
+      val returnCode = process.waitForTermination(lines += _)
+      logger.error(lines mkString "\n")
+      assert(returnCode == ResultCode(77))
+      assert(lines contains s"STDOUT $TestValue")
+      assert(lines contains s"STDERR $TestValue")
+    }
+    finally process.close()
+    Await.result(process.closed, 10.seconds)
+    logger.info(s"$stopwatch for Java process")
+  }
+}
+
+object JavaProcessTest {
+  private val TestValue = "TEST TEST"
+  private val Arguments = Vector("a", "1 2")
+  private val logger = Logger(getClass)
+
+  def main(args: Array[String]): Unit = {
+    if (args.toVector == Arguments) {
+      out.println(s"STDOUT $TestValue")
+      err.println(s"STDERR $TestValue")
+      exit(77)
+    } else
+      exit(3)
+  }
+}
