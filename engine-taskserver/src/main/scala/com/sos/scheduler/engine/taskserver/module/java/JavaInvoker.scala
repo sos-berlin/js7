abe72f00e31a1fed97942199f372eccdbd52ef4f
Joacim Zschimmer <jogit@zschimmer.com>
2015-03-18 10:55:46 +0100

    JS-1291 JavaInvoker invokes correctly

--- engine-taskserver/src/main/scala/com/sos/scheduler/engine/taskserver/module/java/JavaInvoker.scala
+++ engine-taskserver/src/main/scala/com/sos/scheduler/engine/taskserver/module/java/JavaInvoker.scala
@@ -1,36 +1,41 @@
 package com.sos.scheduler.engine.taskserver.module.java
 
 import com.sos.scheduler.engine.minicom.idispatch.IDispatch.implicits._
-import com.sos.scheduler.engine.minicom.idispatch.{DISPATCH_METHOD, DISPATCH_PROPERTYGET, DISPATCH_PROPERTYPUT, DispatchType, IDispatch, Invocable, InvocableIDispatch}
+import com.sos.scheduler.engine.minicom.idispatch.{DISPATCH_PROPERTYGET, DISPATCH_PROPERTYPUT, DISPID, IDispatch, Invocable, InvocableIDispatch}
 import com.sos.scheduler.engine.taskserver.module.java.JavaInvoker._
+import scala.runtime.BoxedUnit.UNIT
 
 /**
  * @author Joacim Zschimmer
  */
 private[java] class JavaInvoker(iDispatch: IDispatch) extends sos.spooler.Invoker {
 
-  private[java] def call(richName: String, params: Array[AnyRef]) = {
-    val (dispatchType, name) = resolveNameSyntax(richName)
-    val dispid = iDispatch.getIdOfName(name)
-    iDispatch.invoke(dispid, Set(dispatchType), params).asInstanceOf[AnyRef] match {
+  private[java] def call(richName: String, arguments: Array[AnyRef]) =
+    (richName.head match {
+      case '<' ⇒
+        val dispId = iDispatch.getIdOfName(richName.tail)
+        iDispatch.invoke(dispId, Set(DISPATCH_PROPERTYGET), arguments)
+      case '>' ⇒
+        require(arguments.nonEmpty, s"Missing arguments to set property $richName")
+        val dispId = iDispatch.getIdOfName(richName.tail)
+        val (subnames, values) = arguments splitAt arguments.size - 1
+        iDispatch.invoke(dispId, Set(DISPATCH_PROPERTYPUT), arguments = subnames, namedArguments = values map { DISPID.PROPERTYPUT → _ })
+      case _ ⇒
+        val dispId = iDispatch.getIdOfName(richName)
+        iDispatch.invokeMethod(dispId, arguments)
+    })
+    .asInstanceOf[AnyRef] match {
       case result: IDispatch ⇒ toSosSpoolerIdispatch(result)
+      case UNIT ⇒ ""  // Unit results from COM VT_EMPTY, which is expected to be an empty string
       case o ⇒ o
     }
-  }
 }
 
 private[java] object JavaInvoker {
   private[java] def apply(o: Invocable) = new JavaInvoker(InvocableIDispatch(o))
 
-  private[java] def resolveNameSyntax(richName: String): (DispatchType, String) =
-    richName.head match {
-      case '<' ⇒ (DISPATCH_PROPERTYGET, richName.tail)
-      case '>' ⇒ (DISPATCH_PROPERTYPUT, richName.tail)
-      case _ ⇒ (DISPATCH_METHOD, richName)
-    }
-
   private def toSosSpoolerIdispatch(iDispatch: IDispatch): sos.spooler.Idispatch = {
-    val className = try iDispatch.invokeGet("java_class_name").asInstanceOf[String]
+    val className = iDispatch.invokeGet("java_class_name").asInstanceOf[String]
     val constructor = Class.forName(className).getDeclaredConstructor(classOf[sos.spooler.Invoker])
     constructor.newInstance(new JavaInvoker(iDispatch)).asInstanceOf[sos.spooler.Idispatch]
   }
--- engine-taskserver/src/test/scala/com/sos/scheduler/engine/taskserver/module/java/JavaInvokerTest.scala
+++ engine-taskserver/src/test/scala/com/sos/scheduler/engine/taskserver/module/java/JavaInvokerTest.scala
@@ -14,28 +14,45 @@ import org.scalatest.mock.MockitoSugar.mock
 @RunWith(classOf[JUnitRunner])
 final class JavaInvokerTest extends FreeSpec {
 
-  private val iDispatch = mock[IDispatch]
-  when(iDispatch.getIdOfName("name")) thenReturn TestDispatchId
-  when(iDispatch.invoke(TestDispatchId, Set(DISPATCH_METHOD), List(1, "zwei"))).thenReturn(MethodResult, Nil: _*)
-  when(iDispatch.invoke(TestDispatchId, Set(DISPATCH_PROPERTYGET), Nil)).thenReturn(GetPropertyResult, Nil: _*)
-  when(iDispatch.invoke(TestDispatchId, Set(DISPATCH_PROPERTYPUT), List("value"))).thenReturn(Unit, Nil: _*)
-  private val javaInvoker = new JavaInvoker(iDispatch)
-
   "call method" in {
+    val iDispatch = mockIDispatch()
+    when(iDispatch.invoke(TestDispatchId, Set(DISPATCH_METHOD), List(1, "zwei"))).thenReturn(MethodResult, Nil: _*)
+    val javaInvoker = new JavaInvoker(iDispatch)
     assert(javaInvoker.call("name", Array(Integer valueOf 1, "zwei")) == MethodResult)
   }
 
   "call <get-property" in {
+    val iDispatch = mockIDispatch()
+    when(iDispatch.invoke(TestDispatchId, Set(DISPATCH_PROPERTYGET), Nil)).thenReturn(GetPropertyResult, Nil: _*)
+    val javaInvoker = new JavaInvoker(iDispatch)
     assert(javaInvoker.call("<name", Array()) == GetPropertyResult)
   }
 
   "call >put-property" in {
-    assert(javaInvoker.call(">name", Array("value")) == Unit)
+    val iDispatch = mockIDispatch()
+    when(iDispatch.invoke(TestDispatchId, Set(DISPATCH_PROPERTYPUT), Array[Any](), namedArguments = List(DISPID.PROPERTYPUT → "value"))).thenReturn(PutPropertyResult, Nil: _*)
+    val javaInvoker = new JavaInvoker(iDispatch)
+    assert(javaInvoker.call(">name", Array("value")) == PutPropertyResult)  // Normally, a property-put does not return any value
+  }
+
+  "call >put-property with two values" in {
+    val iDispatch = mockIDispatch()
+    when(iDispatch.invoke(TestDispatchId, Set(DISPATCH_PROPERTYPUT), List("name2"), namedArguments = List(DISPID.PROPERTYPUT → "value"))).thenReturn(Put2PropertyResult, Nil: _*)
+    val javaInvoker = new JavaInvoker(iDispatch)
+    assert(javaInvoker.call(">name", Array("name2", "value")) == Put2PropertyResult)  // Normally, a property-put does not return any value
   }
 }
 
 object JavaInvokerTest {
   private object MethodResult
   private object GetPropertyResult
+  private object PutPropertyResult
+  private object Put2PropertyResult
   private val TestDispatchId = DISPID(42)
+
+  private def mockIDispatch() = {
+    val iDispatch = mock[IDispatch]
+    when(iDispatch.getIdOfName("name")) thenReturn TestDispatchId
+    iDispatch
+  }
 }
