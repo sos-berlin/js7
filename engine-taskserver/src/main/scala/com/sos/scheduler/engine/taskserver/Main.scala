f17efe48982b4700ae456853ea667b4d2e47f719
Joacim Zschimmer <jogit@zschimmer.com>
2015-03-18 10:56:50 +0100

    JS-1291 TaskServer command line parsing refactored

--- engine-taskserver/src/main/scala/com/sos/scheduler/engine/taskserver/Main.scala
+++ engine-taskserver/src/main/scala/com/sos/scheduler/engine/taskserver/Main.scala
@@ -1,5 +1,6 @@
 package com.sos.scheduler.engine.taskserver
 
+import com.sos.scheduler.engine.common.commandline.CommandLineArguments
 import com.sos.scheduler.engine.taskserver.task.TaskStartArguments
 
 /**
@@ -8,13 +9,25 @@ import com.sos.scheduler.engine.taskserver.task.TaskStartArguments
 object Main {
   def main(args: Array[String]): Unit = {
     try {
-      val startArguments = new TaskStartArguments(controllerAddress = args(0) stripPrefix "-controller=")
+      val arguments = Arguments(args)
+      val startArguments = new TaskStartArguments(controllerAddress = arguments.controllerAddress)
       SimpleTaskServer.run(startArguments)
     } catch {
       case t: Throwable ⇒
         System.err.println(t.toString)
         t.printStackTrace(System.err)
         System.exit(1)
+    }
+  }
+
+  private case class Arguments(controllerAddress: String)
+
+  private object Arguments {
+    def apply(args: Seq[String]) = {
+      val a = CommandLineArguments(args)
+      val controllerAddress = a.string("-controller=")
+      a.requireNoMoreArguments()
+      new Arguments(controllerAddress = controllerAddress)
     }
   }
 }
