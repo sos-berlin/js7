a3c7725355d77f1db81bc57b413d0c0161fe9fb4
AL-SOS <al.git@sos-berlin.com>
2015-04-10 17:41:30 +0200

    JS-1353 first log test

---
+++ engine-agent/src/test/scala/com/sos/scheduler/engine/agent/tests/api/ApiJob.java
@@ -0,0 +1,7 @@
+package com.sos.scheduler.engine.agent.tests.api;
+
+/**
+ * Created by Andreas Liebert on 10.04.2015.
+ */
+public class ApiJob {
+}
---
+++ engine-agent/src/test/scala/com/sos/scheduler/engine/agent/tests/api/SchedulerAPIIT.scala
@@ -0,0 +1,8 @@
+package com.sos.scheduler.engine.agent.tests.api
+
+/**
+ * Created by Andreas Liebert on 09.04.2015.
+ */
+class SchedulerAPIIT {
+
+}
