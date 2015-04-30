07723eab2f67d01f2908333c90b0a2eb61dde117
Joacim Zschimmer <jogit@zschimmer.com>
2015-04-30 14:05:14 +0200

    JS-1352 Agent: Java job

--- engine-taskserver/src/main/scala/com/sos/scheduler/engine/taskserver/task/JavaProcessTask.scala
+++ engine-taskserver/src/main/scala/com/sos/scheduler/engine/taskserver/task/JavaProcessTask.scala
@@ -2,6 +2,7 @@ package com.sos.scheduler.engine.taskserver.task
 
 import com.sos.scheduler.engine.common.scalautil.Closers.implicits.RichClosersAutoCloseable
 import com.sos.scheduler.engine.common.scalautil.{HasCloser, Logger}
+import com.sos.scheduler.engine.taskserver.module.Module._
 import com.sos.scheduler.engine.taskserver.module.NamedInvocables
 import com.sos.scheduler.engine.taskserver.module.java.JavaModule
 import com.sos.scheduler.engine.taskserver.task.JavaProcessTask._
@@ -21,24 +22,48 @@ extends Task with HasCloser {
 
   private val monitorProcessor = new MonitorProcessor(monitors, namedInvocables, jobName = jobName).closeWithCloser
   private val instance: sos.spooler.Job_impl = module.newJobInstance(namedInvocables)
-
-  // FIXME Implemented incompletely, only as needed to test JS-1301.
+  private var openCalled = false
+  private var closeCalled = false
+  private var exitCalled = false
 
   def start() = {
-    //??? monitorProcessor.preTask()
-    instance.spooler_init() && instance.spooler_open()
+    // TODO Monitor calls: monitorProcessor.preTask()
+    require(monitorProcessor.isEmpty, "Monitors for Java jobs are not yet implemented")
+    instance.spooler_init()
   }
 
   def callIfExists(methodWithSignature: String) = {
-    val NameAndSignature(name, "", resultType) = methodWithSignature
-    instance.getClass.getMethod(name).invoke(instance)
+    if (ignoreCall(methodWithSignature)) {
+      logger.debug(s"Call ignored: $methodWithSignature")
+      ()
+    } else {
+      methodWithSignature match {
+        case SpoolerOpenSignature ⇒ openCalled = true
+        case SpoolerExitSignature ⇒ exitCalled = true
+        case _ ⇒
+      }
+      val NameAndSignature(name, "", _) = methodWithSignature
+      instance.getClass.getMethod(name).invoke(instance)
+    }
+  }
+
+  private def ignoreCall(methodWithSignature: String): Boolean = {
+    // As in C++ Module_instance::call__end
+    methodWithSignature match {
+      case SpoolerOnSuccessSignature if !openCalled ⇒ true
+      case SpoolerOnErrorSignature if !openCalled ⇒ true
+      case SpoolerExitSignature if exitCalled ⇒ true
+      case _ ⇒ false
+    }
   }
 
   def step() = instance.spooler_process()
 
   def end() = {
-//    instance.spooler_close()
-//    instance.spooler_exit()
+    if (openCalled && !closeCalled) {
+      closeCalled = true
+      instance.spooler_close()
+    }
   }
 }
 
