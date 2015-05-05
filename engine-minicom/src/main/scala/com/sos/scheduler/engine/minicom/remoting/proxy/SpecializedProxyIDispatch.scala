6dfeec9a25888012892209cf6b23c8e12a8cf3ed
Joacim Zschimmer <jogit@zschimmer.com>
2015-05-05 15:08:32 +0200

    JS-1371 SpecializedProxyIDpsatch.forEachProperty

--- engine-minicom/src/main/scala/com/sos/scheduler/engine/minicom/remoting/proxy/SpecializedProxyIDispatch.scala
+++ engine-minicom/src/main/scala/com/sos/scheduler/engine/minicom/remoting/proxy/SpecializedProxyIDispatch.scala
@@ -1,6 +1,20 @@
 package com.sos.scheduler.engine.minicom.remoting.proxy
 
+import java.util.NoSuchElementException
+
 /**
  * @author Joacim Zschimmer
  */
 trait SpecializedProxyIDispatch extends ProxyIDispatch
+
+object SpecializedProxyIDispatch  {
+
+  /**
+   * For simple reading of proxy properties in proxy classes.
+   */
+  def forEachProperty(keyValues: Iterable[(String, Any)])(handleProperty: PartialFunction[(String, Any), Unit]): Unit = {
+    for (kv ← keyValues) handleProperty.applyOrElse(kv, { o: (String, Any) ⇒ throwNoSuchProperty(o._1) })
+  }
+
+  private def throwNoSuchProperty(name: String) = throw new NoSuchElementException(s"Unknown local proxy property '$name' for sos.spooler.Log")
+}
