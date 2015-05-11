1d7370d7818b0dc47f6732062c614a587727f0a5
Joacim Zschimmer <jogit@zschimmer.com>
2015-05-11 14:26:36 +0200

    JS-1300 Web service /jobscheduler/agent/fileOrderSource/files, new artifact agent-data with FileOrderSourceContent

--- engine-agent/src/main/scala/com/sos/scheduler/engine/agent/web/AgentWebService.scala
+++ engine-agent/src/main/scala/com/sos/scheduler/engine/agent/web/AgentWebService.scala
@@ -1,11 +1,15 @@
 package com.sos.scheduler.engine.agent.web
 
+import com.sos.scheduler.engine.agent.data.FileOrderSourceContent
+import com.sos.scheduler.engine.common.scalautil.Collections.implicits.RichJavaStream
 import java.net.InetAddress
+import java.nio.file.{Files, NoSuchFileException, Paths}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import spray.http.HttpEntity
 import spray.http.MediaTypes._
 import spray.http.StatusCodes.BadRequest
+import spray.httpx.SprayJsonSupport._
 import spray.routing.{HttpService, HttpServiceActor, RequestEntityExpectedRejection, UnsupportedRequestContentTypeRejection}
 
 /**
@@ -17,20 +21,39 @@ private[agent] trait AgentWebService extends HttpService {
 
   private[agent] def route =
     (decompressRequest() | compressResponseIfRequested(())) {
-      path("jobscheduler" / "engine" / "command") {
-        post {
-          entity(as[HttpEntity]) {
-            case HttpEntity.Empty ⇒ reject(RequestEntityExpectedRejection)
-            case httpEntity: HttpEntity.NonEmpty ⇒
-              if (!(Set(`application/xml`, `text/xml`) contains httpEntity.contentType.mediaType)) reject(UnsupportedRequestContentTypeRejection("application/xml expected"))
-              optionalHeaderValueByName("Remote-Address") {  // Requires Spray configuration spray.can.remote-address-header = on
-                case None ⇒ complete(BadRequest, "Client's IP address is unknown")
-                case Some(clientIPAddress) ⇒
-                  val future = executeCommand(clientIPAddress = InetAddress.getByName(clientIPAddress), command = httpEntity.asString)
-                  onSuccess(future) {
-                    response ⇒ complete(response)
-                  }
+      pathPrefix("jobscheduler") {
+        path("engine" / "command") {
+          post {
+            entity(as[HttpEntity]) {
+              case HttpEntity.Empty ⇒ reject(RequestEntityExpectedRejection)
+              case httpEntity: HttpEntity.NonEmpty ⇒
+                if (!(Set(`application/xml`, `text/xml`) contains httpEntity.contentType.mediaType)) reject(UnsupportedRequestContentTypeRejection("application/xml expected"))
+                optionalHeaderValueByName("Remote-Address") {
+                  // Requires Spray configuration spray.can.remote-address-header = on
+                  case None ⇒ complete(BadRequest, "Client's IP address is unknown")
+                  case Some(clientIPAddress) ⇒
+                    val future = executeCommand(clientIPAddress = InetAddress.getByName(clientIPAddress), command = httpEntity.asString)
+                    onSuccess(future) {
+                      response ⇒ complete(response)
+                    }
+                }
+            }
+          }
+        } ~
+        pathPrefix("agent") {
+          get {
+            path("fileOrderSource" / "files") {
+              parameter(('directory, 'order ! "latest-access-first")) { directoryString ⇒
+                complete {
+                  val directory = Paths.get(directoryString)
+                  FileOrderSourceContent(Files.list(directory).toVector flatMap {
+                    case f ⇒
+                      try Some(FileOrderSourceContent.Entry(f.toString, Files.getLastModifiedTime(f).toMillis))
+                      catch { case _: NoSuchFileException ⇒ None }
+                  } sortBy { _.lastModifiedTime })
+                }
               }
+            }
           }
         }
       }
@@ -39,6 +62,6 @@ private[agent] trait AgentWebService extends HttpService {
 
 object AgentWebService {
   trait AsActor extends HttpServiceActor with AgentWebService {
-    def receive = runRoute(route)
+    final def receive = runRoute(route)
   }
 }
--- engine-agent/src/test/scala/com/sos/scheduler/engine/agent/web/AgentWebServiceTest.scala
+++ engine-agent/src/test/scala/com/sos/scheduler/engine/agent/web/AgentWebServiceTest.scala
@@ -1,31 +1,47 @@
 package com.sos.scheduler.engine.agent.web
 
+import com.google.common.io.Files.touch
 import com.sos.scheduler.engine.agent.configuration.Akkas._
+import com.sos.scheduler.engine.agent.data.FileOrderSourceContent
+import com.sos.scheduler.engine.common.scalautil.FileUtils.implicits._
 import com.sos.scheduler.engine.common.scalautil.HasCloser
+import com.sos.scheduler.engine.common.utils.JsonUtils.jsonQuote
 import java.net.InetAddress
+import java.nio.file.Files.{createTempDirectory, setLastModifiedTime}
+import java.nio.file.attribute.FileTime
+import java.nio.file.{Files, Paths}
 import org.junit.runner.RunWith
-import org.scalatest.FreeSpec
+import org.scalatest.{BeforeAndAfterAll, FreeSpec}
 import org.scalatest.Matchers._
 import org.scalatest.junit.JUnitRunner
 import scala.concurrent.Future
+import spray.http.HttpHeaders.Accept
 import spray.http.StatusCodes.InternalServerError
+import spray.http.{MediaTypes, Uri}
+import spray.httpx.SprayJsonSupport._
+import spray.json._
 import spray.testkit.ScalatestRouteTest
 
 /**
  * @author Joacim Zschimmer
  */
 @RunWith(classOf[JUnitRunner])
-final class AgentWebServiceTest extends FreeSpec with ScalatestRouteTest with AgentWebService with HasCloser {
+final class AgentWebServiceTest extends FreeSpec with BeforeAndAfterAll with ScalatestRouteTest with AgentWebService with HasCloser {
 
   implicit lazy val actorRefFactory = newActorSystem("TEST")(closer)
 
-  "Good command" in {
+  override def afterAll(): Unit = {
+    close()
+    super.afterAll()
+  }
+
+  "Command" in {
     postCommand("test") ~> check {
       responseAs[String] shouldEqual "<ok/>"
     }
   }
 
-  "Exception" in {
+  "Command throwing Exception" in {
     postCommand("error") ~> check {
       assert(status == InternalServerError)
     }
@@ -41,4 +57,37 @@ final class AgentWebServiceTest extends FreeSpec with ScalatestRouteTest with Ag
       case "test" ⇒ <ok/>
       case _ ⇒ throw new Exception("TEST EXCEPTION")
     })
+
+  "agent/fileOrderSource/files" in {
+    val dir = createTempDirectory("agent-")
+    val aTime = 1000L*1000*1000*1000
+    val xTime = aTime + 2000
+    val cTime = aTime + 4000
+    onClose { Files.delete(dir) }
+    val expectedResult = FileOrderSourceContent(List(
+      FileOrderSourceContent.Entry((dir / "a").toString, aTime),
+      FileOrderSourceContent.Entry((dir / "x").toString, xTime),
+      FileOrderSourceContent.Entry((dir / "c").toString, cTime)))
+    for (entry ← expectedResult.files) {
+      val path = Paths.get(entry.path)
+      touch(path)
+      setLastModifiedTime(path, FileTime.fromMillis(entry.lastModifiedTime))
+      onClose { Files.delete(path) }
+    }
+
+    Get(Uri(s"/jobscheduler/agent/fileOrderSource/files?directory=$dir&order=latest-access-first")) ~>
+      Accept(MediaTypes.`application/json`) ~>
+      route ~> check
+    {
+      assert(responseAs[FileOrderSourceContent] == expectedResult)
+      assert(responseAs[String].parseJson ==
+        s"""{
+          "files": [
+            { "path": ${jsonQuote(dir / "a")}, "lastModifiedTime": $aTime},
+            { "path": ${jsonQuote(dir / "x")}, "lastModifiedTime": $xTime},
+            { "path": ${jsonQuote(dir / "c")}, "lastModifiedTime": $cTime}
+          ]
+        }""".parseJson)
+    }
+  }
 }
