dd4116cea4ea949ed2c1b4cefcb384bc02e6a399
Joacim Zschimmer <jogit@zschimmer.com>
2015-05-11 23:36:26 +0200

    JS-1291 System property jobscheduler.agent.useThread (for debugging) is logged

--- engine-agent/src/main/scala/com/sos/scheduler/engine/agent/process/StandardAgentProcessFactory.scala
+++ engine-agent/src/main/scala/com/sos/scheduler/engine/agent/process/StandardAgentProcessFactory.scala
@@ -1,9 +1,11 @@
 package com.sos.scheduler.engine.agent.process
 
 import com.google.common.base.Splitter
-import com.sos.scheduler.engine.agent.commands.{StartSeparateProcess, StartProcess, StartThread}
+import com.sos.scheduler.engine.agent.commands.{StartProcess, StartSeparateProcess, StartThread}
 import com.sos.scheduler.engine.agent.configuration.AgentConfiguration
+import com.sos.scheduler.engine.agent.process.StandardAgentProcessFactory._
 import com.sos.scheduler.engine.common.scalautil.Collections.implicits._
+import com.sos.scheduler.engine.common.scalautil.Logger
 import com.sos.scheduler.engine.data.agent.AgentProcessId
 import com.sos.scheduler.engine.taskserver.SimpleTaskServer
 import com.sos.scheduler.engine.taskserver.task.{SeparateProcessTaskServer, TaskStartArguments}
@@ -24,9 +26,10 @@ final class StandardAgentProcessFactory @Inject private(agentConfiguration: Agen
     val taskStartArguments = TaskStartArguments(
       controllerAddress = command.controllerAddress,
       environment = agentConfiguration.environment)
-    if (sys.props contains "jobscheduler.agent.useThread")   // For debugging
+    if (sys.props contains UseThreadPropertyName) { // For debugging
+      logger.warn(s"Due to system property $UseThreadPropertyName, task does not use an own process")
       new SimpleTaskServer(taskStartArguments)
-    else
+    } else
       command match {
         case _: StartThread ⇒ new SimpleTaskServer(taskStartArguments)
         case o: StartSeparateProcess ⇒ new SeparateProcessTaskServer(
@@ -38,4 +41,9 @@ final class StandardAgentProcessFactory @Inject private(agentConfiguration: Agen
 
   private def splitJavaOptions(options: String) =
     Splitter.on(Pattern.compile("\\s+")).trimResults.omitEmptyStrings.split(options).toImmutableSeq
+}
+
+private object StandardAgentProcessFactory {
+  private val logger = Logger(getClass)
+  private val UseThreadPropertyName = "jobscheduler.agent.useThread"
 }
