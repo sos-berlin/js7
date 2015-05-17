065ddf7d548a07af03b4287055d18f7ca1ed41e4
Joacim Zschimmer <jogit@zschimmer.com>
2015-05-17 11:19:19 +0200

    JS-1291 StdoutStderrWell

---
+++ engine-taskserver/src/main/scala/com/sos/scheduler/engine/taskserver/task/StdoutStderrWell.scala
@@ -0,0 +1,70 @@
+package com.sos.scheduler.engine.taskserver.task
+
+import com.sos.scheduler.engine.common.async.ConcurrentCaller
+import com.sos.scheduler.engine.common.scalautil.Closers.implicits.RichClosersAutoCloseable
+import com.sos.scheduler.engine.common.scalautil.{ClosedFuture, HasCloser}
+import com.sos.scheduler.engine.common.system.OperatingSystem._
+import com.sos.scheduler.engine.common.time.ScalaTime._
+import com.sos.scheduler.engine.taskserver.task.process.FilesLineCollector
+import com.sos.scheduler.engine.taskserver.task.process.StdoutStderr.{Stdout, StdoutStderrType}
+import java.nio.charset.Charset
+import java.nio.file.Path
+import scala.concurrent.Future
+
+/**
+ * Reads stdout and stderr output files and outputs the arrived data then with every `apply`.
+ *
+ * @author Joacim Zschimmer
+ */
+final class StdoutStderrWell(stdFiles: Map[StdoutStderrType, Path], fileEncoding: Charset, output: String ⇒ Unit)
+extends HasCloser {
+
+  private val lineCollector = new FilesLineCollector(Nil ++ stdFiles.values, fileEncoding).closeWithCloser
+  private val firstLineCollector = new FirstStdoutLineCollector
+  def firstStdoutLine = firstLineCollector.firstStdoutLine
+
+  def apply() = synchronized {
+    lineCollector.nextLinesIterator foreach { case (file, line) ⇒
+      output(line)
+      firstLineCollector.apply(file, line)
+    }
+  }
+
+  private class FirstStdoutLineCollector {
+    private val maxLineNr = if (isWindows) 2 else 1  // Windows stdout may start with an empty first line
+    private var stdoutLineNr = 0
+    var firstStdoutLine = ""
+
+    def apply(file: Path, line: String) =
+      if (firstStdoutLine.isEmpty) {
+        if (file == stdFiles(Stdout)) {
+          stdoutLineNr += 1
+          if (stdoutLineNr <= maxLineNr)
+            firstStdoutLine = line
+        }
+      }
+  }
+}
+
+object StdoutStderrWell {
+  private val PollPeriod = 10.s
+
+  final class Concurrent(name: String, stdFiles: Map[StdoutStderrType, Path], fileEncoding: Charset, output: String ⇒ Unit)
+  extends HasCloser with ClosedFuture {
+
+    private val well = new StdoutStderrWell(stdFiles, fileEncoding, output).closeWithCloser
+    private val concurrentCaller = new ConcurrentCaller(Iterator continually PollPeriod, well.apply, name).closeWithCloser
+
+    def start() = concurrentCaller.start()
+
+    def finish() = {
+      concurrentCaller.close()
+      well.apply()
+      close()
+    }
+
+    def terminated: Future[Unit] = concurrentCaller.terminated
+
+    def firstStdoutLine = well.firstStdoutLine
+  }
+}
---
+++ engine-taskserver/src/test/scala/com/sos/scheduler/engine/taskserver/task/StdoutStderrWellTest.scala
@@ -0,0 +1,45 @@
+package com.sos.scheduler.engine.taskserver.task
+
+import com.sos.scheduler.engine.common.scalautil.Closers.implicits.{RichClosersAutoCloseable, RichClosersCloser}
+import com.sos.scheduler.engine.common.scalautil.Closers.withCloser
+import com.sos.scheduler.engine.common.scalautil.FileUtils.implicits._
+import com.sos.scheduler.engine.taskserver.task.process.StdoutStderr.{Stderr, Stdout}
+import java.io.{FileOutputStream, OutputStreamWriter}
+import java.nio.charset.StandardCharsets.UTF_8
+import java.nio.file.Files.{createTempFile, delete}
+import org.junit.runner.RunWith
+import org.scalatest.FreeSpec
+import org.scalatest.junit.JUnitRunner
+import scala.collection.mutable
+
+/**
+ * @author Joacim Zschimmer
+ */
+@RunWith(classOf[JUnitRunner])
+final class StdoutStderrWellTest extends FreeSpec {
+
+  "StdoutStderrWell" in {
+    withCloser { implicit closer ⇒
+      val lines = mutable.Buffer[String]()
+      val stdFiles = Map(Stdout → createTempFile("test", ".tmp"), Stderr → createTempFile("test", ".tmp"))
+      closer.onClose { stdFiles.values foreach delete }
+      val List(out, err) = List(Stdout, Stderr) map { o ⇒ new OutputStreamWriter(new FileOutputStream(stdFiles(o)).closeWithCloser) }
+      val well = new StdoutStderrWell(stdFiles, UTF_8, lines += _).closeWithCloser
+
+      out.write("OUT\n")
+      out.flush()
+      err.write("ERR\n")
+      err.flush()
+      well.apply()
+      assert(lines.toSet == Set("OUT", "ERR"))
+
+      lines.clear()
+      out.write("OUT-2\n")
+      out.flush()
+      err.write("ERR-2\n")
+      err.flush()
+      well.apply()
+      assert(lines.toSet == Set("OUT-2", "ERR-2"))
+    }
+  }
+}
