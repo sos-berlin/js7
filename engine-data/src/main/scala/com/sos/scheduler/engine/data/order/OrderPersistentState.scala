65698ca820226adc6cf15d481bfbd941148ec603
Joacim Zschimmer <jogit@zschimmer.com>
2015-05-27 19:09:00 +0200

    JS-1300 OrderPersistentState.isOnBlacklist

--- engine-data/src/main/scala/com/sos/scheduler/engine/data/order/OrderPersistentState.scala
+++ engine-data/src/main/scala/com/sos/scheduler/engine/data/order/OrderPersistentState.scala
@@ -2,7 +2,9 @@ package com.sos.scheduler.engine.data.order
 
 import com.sos.scheduler.engine.data.base.HasKey
 import com.sos.scheduler.engine.data.jobchain.JobChainPath
+import com.sos.scheduler.engine.data.order.OrderPersistentState._
 import com.sos.scheduler.engine.data.scheduler.ClusterMemberId
+import java.time.Instant
 import org.joda.time.ReadableInstant
 
 final case class OrderPersistentState(
@@ -23,6 +25,14 @@ final case class OrderPersistentState(
 
 extends HasKey[OrderKey] {
 
-  def key =
-    OrderKey(jobChainPath, orderId)
+  def key = OrderKey(jobChainPath, orderId)
+  //Compiles, but is wrongly typed (Joda vs Java Time): def isOnBlacklist = distributedNextTimeOption contains BlacklistDatabaseDistributedNextTime
+  def isOnBlacklist = distributedNextTimeOption map { _.getMillis } contains BlacklistDatabaseDistributedNextTime.toEpochMilli
+}
+
+object OrderPersistentState {
+  private val NowDatabaseDistributedNextTime         = Instant.parse("2000-01-01T00:00:00Z")        // Auftrag ist verteilt und ist sofort ausführbar
+  private val NeverDatabaseDistributedNextTime       = Instant.parse("3111-11-11T00:00:00Z")        // Auftrag ist verteilt, hat aber keine Startzeit (weil z.B. suspendiert)
+  private val BlacklistDatabaseDistributedNextTime   = Instant.parse("3111-11-11T00:01:00Z")        // Auftrag ist auf der schwarzen Liste
+  private val ReplacementDatabaseDistributedNextTime = Instant.parse("3111-11-11T00:02:00Z")        // <order replacement="yes">
 }
