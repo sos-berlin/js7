ace035207663324774cee67ed6c93f23a32fbd70
Joacim Zschimmer <jogit@zschimmer.com>
2015-06-03 09:22:51 +0200

    JS-1291 Agent commands and responses are numbered and logged

--- engine-agent/src/main/scala/com/sos/scheduler/engine/agent/commandexecutor/AgentCommandExecutor.scala
+++ engine-agent/src/main/scala/com/sos/scheduler/engine/agent/commandexecutor/AgentCommandExecutor.scala
@@ -1,8 +1,11 @@
 package com.sos.scheduler.engine.agent.commandexecutor
 
+import com.sos.scheduler.engine.agent.commandexecutor.AgentCommandExecutor._
 import com.sos.scheduler.engine.agent.data.commands.{Command, ProcessCommand, RequestFileOrderSourceContent}
 import com.sos.scheduler.engine.agent.fileordersource.RequestFileOrderSourceContentExecutor
 import com.sos.scheduler.engine.agent.process.ProcessCommandExecutor
+import com.sos.scheduler.engine.common.scalautil.Logger
+import java.util.concurrent.atomic.AtomicLong
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -15,11 +18,23 @@ import scala.concurrent.Future
 final class AgentCommandExecutor @Inject private(processCommandExecutor: ProcessCommandExecutor)
 extends CommandExecutor {
 
+  private val atomicLong = new AtomicLong(0)
+
   def executeCommand(command: Command): Future[command.Response] = {
+    val number = atomicLong.incrementAndGet()
+    logger.info(s"#$number ${command.toShortString}")
+    if (command.toStringIsLonger) logger.debug(s"#$number $command")  // Complete string
     val future = command match {
       case command: ProcessCommand ⇒ processCommandExecutor.apply(command)
       case command: RequestFileOrderSourceContent ⇒ RequestFileOrderSourceContentExecutor.apply(command)
     }
-    future map { _.asInstanceOf[command.Response] }
+    future map { response ⇒
+      logger.debug(s"Response to #$number ${command.getClass.getSimpleName}: $response")
+      response.asInstanceOf[command.Response]
+    }
   }
+}
+
+object AgentCommandExecutor {
+  private val logger = Logger(getClass)
 }
