0a083d9aeca662440359ff0ca5651cf666451ee6
Joacim Zschimmer <jogit@zschimmer.com>
2015-06-06 09:20:44 +0200

    JS-1371 Local proxies spooler_task.stdout_text, .stdout_path, .stderr_text and .stderr_path

--- engine-taskserver/src/main/scala/com/sos/scheduler/engine/taskserver/spoolerapi/ProxySpoolerTask.scala
+++ engine-taskserver/src/main/scala/com/sos/scheduler/engine/taskserver/spoolerapi/ProxySpoolerTask.scala
@@ -1,18 +1,25 @@
 package com.sos.scheduler.engine.taskserver.spoolerapi
 
+import com.google.inject.Injector
+import com.sos.scheduler.engine.common.guice.GuiceImplicits.RichInjector
+import com.sos.scheduler.engine.common.scalautil.FileUtils.implicits.RichPath
 import com.sos.scheduler.engine.common.scalautil.Logger
 import com.sos.scheduler.engine.data.message.MessageCode
 import com.sos.scheduler.engine.minicom.idispatch.DISPID
 import com.sos.scheduler.engine.minicom.idispatch.IDispatch.implicits._
+import com.sos.scheduler.engine.minicom.idispatch.annotation.invocable
 import com.sos.scheduler.engine.minicom.remoting.calls.ProxyId
 import com.sos.scheduler.engine.minicom.remoting.proxy.{ClientRemoting, ProxyIDispatchFactory, SpecializedProxyIDispatch}
 import com.sos.scheduler.engine.minicom.types.CLSID
+import com.sos.scheduler.engine.taskserver.task.TaskStartArguments
+import com.sos.scheduler.engine.taskserver.task.process.StdoutStderr.{Stderr, Stdout, StdoutStderrType}
+import java.nio.charset.StandardCharsets.ISO_8859_1
 import java.util.UUID
 
 /**
  * @author Joacim Zschimmer
  */
-final class ProxySpoolerTask private(protected val remoting: ClientRemoting, val id: ProxyId, val name: String)
+final class ProxySpoolerTask private(taskStartArguments: TaskStartArguments, protected val remoting: ClientRemoting, val id: ProxyId, val name: String)
 extends SpoolerTask with SpecializedProxyIDispatch {
 
   def setErrorCodeAndText(code: MessageCode, text: String): Unit =
@@ -25,14 +32,30 @@ extends SpoolerTask with SpecializedProxyIDispatch {
   def orderParamsXml = this.invokeGet(DISPID(36)).asInstanceOf[String]
 
   def orderParamsXml_=(o: String) = this.invokePut(DISPID(36), o)
+
+  @invocable
+  def stdout_path: String = filePath(Stdout)
+
+  @invocable
+  def stderr_path: String = filePath(Stderr)
+
+  @invocable
+  def stdout_text: String = fileText(Stdout)
+
+  @invocable
+  def stderr_text: String = fileText(Stderr)
+
+  private def fileText(s: StdoutStderrType) = taskStartArguments.stdFileMap.get(s) map { _.contentString(ISO_8859_1) } getOrElse ""
+
+  private def filePath(s: StdoutStderrType) = taskStartArguments.stdFileMap.get(s) map { _.toString } getOrElse ""
 }
 
 object ProxySpoolerTask extends ProxyIDispatchFactory {
   val clsid = CLSID(UUID fromString "feee47aa-6c1b-11d8-8103-000476ee8afb")
   private val logger = Logger(getClass)
 
-  def apply(remoting: ClientRemoting, id: ProxyId, name: String, properties: Iterable[(String, Any)]) = {
+  def apply(injector: Injector, remoting: ClientRemoting, id: ProxyId, name: String, properties: Iterable[(String, Any)]) = {
     if (properties.nonEmpty) logger.warn(s"IGNORED: $properties")  // TODO Weitere lokale Properties
-    new ProxySpoolerTask(remoting, id, name)
+    new ProxySpoolerTask(injector.instance[TaskStartArguments], remoting, id, name)
   }
 }
---
+++ engine-taskserver/src/test/scala/com/sos/scheduler/engine/taskserver/spoolerapi/ProxySpoolerTaskTest.scala
@@ -0,0 +1,62 @@
+package com.sos.scheduler.engine.taskserver.spoolerapi
+
+import com.google.inject.Guice
+import com.sos.scheduler.engine.common.guice.ScalaAbstractModule
+import com.sos.scheduler.engine.common.scalautil.Closers.implicits._
+import com.sos.scheduler.engine.common.scalautil.FileUtils.implicits.RichPath
+import com.sos.scheduler.engine.common.scalautil.HasCloser
+import com.sos.scheduler.engine.minicom.idispatch.IDispatch.implicits._
+import com.sos.scheduler.engine.minicom.idispatch._
+import com.sos.scheduler.engine.minicom.remoting.calls.ProxyId
+import com.sos.scheduler.engine.minicom.remoting.proxy.ClientRemoting
+import com.sos.scheduler.engine.taskserver.task.TaskStartArguments
+import com.sos.scheduler.engine.taskserver.task.process.RichProcess
+import com.sos.scheduler.engine.taskserver.task.process.StdoutStderr.{Stderr, Stdout}
+import java.nio.charset.StandardCharsets.ISO_8859_1
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.mock.MockitoSugar.mock
+import org.scalatest.{BeforeAndAfterAll, FreeSpec}
+import scala.util.Random
+
+/**
+ * @author Joacim Zschimmer
+ */
+@RunWith(classOf[JUnitRunner])
+final class ProxySpoolerTaskTest extends FreeSpec with BeforeAndAfterAll with HasCloser {
+
+  private lazy val stdFileMap = {
+    val r = RichProcess.createTemporaryStdFiles()
+    closer.onClose { RichProcess.tryDeleteFiles(r.values) }
+    r
+  }
+
+  private lazy val injector = Guice.createInjector(new ScalaAbstractModule {
+    def configure() = bindInstance[TaskStartArguments](TaskStartArguments(controllerAddress = "XX", stdFileMap = stdFileMap))
+  })
+
+  private lazy val spoolerTask = InvocableIDispatch(
+    ProxySpoolerTask(injector, mock[ClientRemoting], ProxyId(Random.nextLong()), name = "TEST", properties = Nil))
+
+  override def afterAll() = closer.close()
+
+  "stdout_path" in {
+    assert(spoolerTask.invokeGet("stdout_path") == stdFileMap(Stdout).toString)
+  }
+
+  "stderr_path" in {
+    assert(spoolerTask.invokeGet("stderr_path") == stdFileMap(Stderr).toString)
+  }
+
+  "stdout_text" in {
+    val string = "STDOUT äöü"
+    stdFileMap(Stdout).write(string, ISO_8859_1)
+    assert(spoolerTask.invokeGet("stdout_text") == string)
+  }
+
+  "stderr_text" in {
+    val string = "STDERR ÄÖÜ"
+    stdFileMap(Stderr).write(string, ISO_8859_1)
+    assert(spoolerTask.invokeGet("stderr_text") == string)
+  }
+}
