9d311555bfbd5af670e376e81c19044aa7a16f6b
Joacim Zschimmer <jogit@zschimmer.com>
2015-06-09 14:19:05 +0200

    JsObjectMarshallers

--- engine-agent/src/main/scala/com/sos/scheduler/engine/agent/web/marshal/JsObjectMarshallers.scala
+++ engine-agent/src/main/scala/com/sos/scheduler/engine/agent/web/marshal/JsObjectMarshallers.scala
@@ -11,11 +11,25 @@ import spray.json._
  * @author Joacim Zschimmer
  */
 object JsObjectMarshallers {
-  implicit val marshaller = Marshaller.of[JsObject](`application/json`) { (value, contentType, ctx) ⇒
+  // Needed for AgentWebServiceTest.
+  // Spray does not include these methods ???
+
+  implicit val JsObjectMarshaller = Marshaller.of[JsObject](`application/json`) { (value, contentType, ctx) ⇒
     ctx.marshalTo(HttpEntity(`application/json`, value.compactPrint.getBytes(UTF_8)))
   }
 
-  implicit val unmarshaller = Unmarshaller[JsObject](`application/json`) {
+  implicit val JsObjectUnmarshaller = Unmarshaller[JsObject](`application/json`) {
     case HttpEntity.NonEmpty(contentType, data) ⇒ data.asString(UTF_8).parseJson.asJsObject
   }
+
+//  implicit val JsArrayMarshaller = Marshaller.of[JsArray](`application/json`) { (value, contentType, ctx) ⇒
+//    ctx.marshalTo(HttpEntity(`application/json`, value.compactPrint.getBytes(UTF_8)))
+//  }
+//
+//  implicit val JsArrayUnmarshaller = Unmarshaller[JsArray](`application/json`) {
+//    case HttpEntity.NonEmpty(contentType, data) ⇒ data.asString(UTF_8).parseJson match {
+//      case o: JsArray ⇒ o
+//      case o => sys.error(s"JSON array expected instead of ${o.getClass.getSimpleName}")
+//    }
+//  }
 }
