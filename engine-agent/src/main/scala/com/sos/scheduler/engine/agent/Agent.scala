c1fab495d8fe2bfb0b99dcabcb4721713f3c671e
Joacim Zschimmer <jogit@zschimmer.com>
2015-06-15 10:50:37 +0200

    New artifact engine-agent-main for Agent main()

--- engine-agent/src/main/scala/com/sos/scheduler/engine/agent/Agent.scala
+++ engine-agent/src/main/scala/com/sos/scheduler/engine/agent/Agent.scala
@@ -8,7 +8,6 @@ import com.sos.scheduler.engine.agent.configuration.inject.AgentModule
 import com.sos.scheduler.engine.agent.process.ProcessHandler
 import com.sos.scheduler.engine.agent.web.AgentWebServer
 import com.sos.scheduler.engine.common.guice.GuiceImplicits._
-import com.sos.scheduler.engine.common.scalautil.AutoClosing.autoClosing
 import com.sos.scheduler.engine.common.scalautil.Futures.awaitResult
 import com.sos.scheduler.engine.common.time.ScalaTime.MaxDuration
 import com.sos.scheduler.engine.common.utils.FreeTcpPortFinder._
@@ -33,18 +32,15 @@ final class Agent(configuration: AgentConfiguration) extends AutoCloseable {
 
   def close(): Unit = closer.close()
 
+  def run(): Unit = {
+    start()
+    awaitResult(terminated, MaxDuration)
+  }
+
   def terminated: Future[Unit] = processHandler.terminated
 }
 
 object Agent {
-  def main(args: Array[String]): Unit = run(AgentConfiguration(args))
-
-  def run(conf: AgentConfiguration): Unit =
-    autoClosing(new Agent(conf)) { agent ⇒
-      agent.start()
-      awaitResult(agent.terminated, MaxDuration)
-    }
-
   def forTest(): Agent = forTest(httpPort = findRandomFreeTcpPort())
 
   def forTest(httpPort: Int): Agent =
