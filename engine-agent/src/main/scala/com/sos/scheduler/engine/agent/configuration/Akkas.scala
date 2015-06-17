c5cfb1b5144cbf98e33ea4cca239d5809e323bd7
Joacim Zschimmer <jogit@zschimmer.com>
2015-06-17 15:37:05 +0200

    JS-1400 Agent: ActorSystem is given 5s to complete shutdown

--- engine-agent/src/main/scala/com/sos/scheduler/engine/agent/configuration/Akkas.scala
+++ engine-agent/src/main/scala/com/sos/scheduler/engine/agent/configuration/Akkas.scala
@@ -5,13 +5,20 @@ import com.google.common.io.Closer
 import com.sos.scheduler.engine.common.scalautil.Closers.implicits._
 import com.sos.scheduler.engine.common.scalautil.SideEffect._
 import com.typesafe.config.ConfigFactory
+import scala.concurrent.duration._
 
 /**
  * @author Joacim Zschimmer
  */
 object Akkas {
   private val ConfigurationResourcePath = "com/sos/scheduler/engine/agent/configuration/akka.conf"
+  private val ShutdownDuration = 5.seconds
 
   def newActorSystem(name: String)(implicit closer: Closer): ActorSystem =
-    ActorSystem(name, ConfigFactory.load(ConfigurationResourcePath)) sideEffect { o ⇒ closer.onClose { o.shutdown() } }
+    ActorSystem(name, ConfigFactory.load(ConfigurationResourcePath)) sideEffect { o ⇒
+      closer.onClose {
+        o.shutdown()
+        o.awaitTermination(ShutdownDuration)
+      }
+    }
 }
