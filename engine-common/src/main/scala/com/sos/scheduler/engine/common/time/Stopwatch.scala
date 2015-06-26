a1cdd8888ad5d10139c5897ca249da665273909b
Joacim Zschimmer <jogit@zschimmer.com>
2015-06-26 14:28:21 +0200

    Stopwatch.measureTime

--- engine-common/src/main/scala/com/sos/scheduler/engine/common/time/Stopwatch.scala
+++ engine-common/src/main/scala/com/sos/scheduler/engine/common/time/Stopwatch.scala
@@ -1,17 +1,38 @@
 package com.sos.scheduler.engine.common.time
 
-import java.lang.System.currentTimeMillis
+import com.sos.scheduler.engine.common.scalautil.Logger
+import com.sos.scheduler.engine.common.time.ScalaTime._
+import java.lang.System.nanoTime
 import java.time.Duration
 
 /**
  * @author Joacim Zschimmer
  */
 final class Stopwatch {
-  private val start = currentTimeMillis()
+  private val start = nanoTime()
 
-  def elapsedMs = currentTimeMillis - start
+  def elapsedMs: Long = duration.toMillis
 
-  def duration = Duration.ofMillis(elapsedMs)
+  def duration: Duration = Duration.ofNanos(nanoTime() - start)
 
-  override def toString = s"${elapsedMs / 1000.0}s"
+  override def toString = duration.pretty
+}
+
+object Stopwatch {
+  private val logger = Logger(getClass)
+
+  def measureTime(n: Int, itemName: String)(body: ⇒ Unit): Result = {
+    body  // Warm-up
+    val start = nanoTime()
+    for (_ ← 1 to n) body
+    val duration = Duration.ofNanos(nanoTime() - start)
+    val r = Result(n, itemName, duration)
+    logger.debug(r.toString)
+    r
+  }
+
+  final case class Result(n: Int, itemName: String, totalDuration: Duration) {
+    val singleDuration = totalDuration / n
+    override def toString = s"${singleDuration.pretty}/$itemName (${totalDuration.pretty}/$n)"
+  }
 }
