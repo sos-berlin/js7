c06a6619b829f82f494b0049b61c5c20d393e168
Joacim Zschimmer <jogit@zschimmer.com>
2015-07-08 15:31:08 +0200

    JS-1386 Akka maximum Timeout calculation

---
+++ engine-common/src/main/scala/com/sos/scheduler/engine/common/akkautils/Akkas.scala
@@ -0,0 +1,37 @@
+package com.sos.scheduler.engine.common.akkautils
+
+import akka.actor.ActorSystem.Settings
+import akka.util.Timeout
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration._
+
+/**
+ * @author Joacim Zschimmer
+ */
+object Akkas {
+
+  /**
+   * 8 months, assuming the Akka confinguration default akka.scheduler.tick-duration = 10.ms
+   */
+  val MaximumTimeout = tickDurationToMaximumTimeout(tickMillis = 10)
+
+  /**
+   * Returns the a Timeout accepted for HTTP request, dependent on Akkas configuration akka.scheduler.tick-duration.
+   * <ul>
+   * <li>68 years for akka.scheduler.tick-duration = 1s</li>
+   * <li>8 month for akka.scheduler.tick-duration = 10ms (the default)</li>
+   * <li>24 days for akka.scheduler.tick-duration = 1ms</li>
+   * </ul>
+   *
+   * @see https://github.com/typesafehub/akka-contrib-extra/issues/21
+   * @param settings = actorSystem.settings
+   * @return A Timeout which Akka http accepts
+   */
+  def maximumTimeout(settings: Settings) =
+    tickDurationToMaximumTimeout(tickMillis = settings.config.getDuration("akka.scheduler.tick-duration", TimeUnit.MILLISECONDS))
+
+  def tickDurationToMaximumTimeout(tickMillis: Long): Timeout = {
+    // 68 years, maximum for scheduler.tick-duration = 1s, 8 months when tick-duration = 10ms
+    Timeout(1000L * Int.MaxValue / (1000 / tickMillis) - 2000, MILLISECONDS)
+  }
+}
---
+++ engine-common/src/test/scala/com/sos/scheduler/engine/common/akkautils/AkkasTest.scala
@@ -0,0 +1,36 @@
+package com.sos.scheduler.engine.common.akkautils
+
+import akka.actor.ActorSystem
+import akka.util.Timeout
+import com.sos.scheduler.engine.common.akkautils.Akkas._
+import com.typesafe.config.ConfigFactory
+import java.util.concurrent.TimeUnit
+import org.junit.runner.RunWith
+import org.scalatest.FreeSpec
+import org.scalatest.junit.JUnitRunner
+
+/**
+ * @author Joacim Zschimmer
+ */
+@RunWith(classOf[JUnitRunner])
+final class AkkasTest extends FreeSpec {
+
+  "MaximumTimeout" in {
+    val millis = Int.MaxValue * 10L - 2000
+    assert(millis / 1000 / 3600 / 24 / 30 == 8)  // Months
+    assert(MaximumTimeout == Timeout.apply(millis, TimeUnit.MILLISECONDS))
+  }
+
+  "maximumTimeout" in {
+    val millis = Int.MaxValue * 10L - 2000
+    assert(millis / 1000 / 3600 / 24 / 30 == 8)  // Months
+    assert(maximumTimeout(ActorSystem().settings) == Timeout.apply(millis, TimeUnit.MILLISECONDS))
+  }
+
+  "maximumTimeout with tick-duration = 1s" in {
+    val millis = Int.MaxValue * 1000L - 2000
+    assert(millis / 1000 / 3600 / 24 / 365 == 68)  // Years
+    val config = ConfigFactory.parseString("akka.scheduler.tick-duration = 1s")
+    assert(maximumTimeout(ActorSystem("TEST", config).settings) == Timeout.apply(millis, TimeUnit.MILLISECONDS))
+  }
+}
