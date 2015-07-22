0bd161f37492ad193964439cbd155c91ba69dbcd
Joacim Zschimmer <jogit@zschimmer.com>
2015-07-22 18:13:51 +0200

    JS-1422 Agent: FileCommandExecutor for DeleteFile and MoveFile

---
+++ engine-agent/src/main/scala/com/sos/scheduler/engine/agent/fileordersource/FileCommandExecutor.scala
@@ -0,0 +1,26 @@
+package com.sos.scheduler.engine.agent.fileordersource
+
+import com.sos.scheduler.engine.agent.data.commands._
+import com.sos.scheduler.engine.agent.data.responses.EmptyResponse
+import java.nio.file.Files._
+import java.nio.file.StandardCopyOption._
+import java.nio.file.{Files, Paths}
+
+/**
+ * @author Joacim Zschimmer
+ */
+object FileCommandExecutor {
+
+  def executeCommand(command: FileCommand): EmptyResponse.type =
+    command match {
+      case DeleteFile(path) ⇒
+        Files.delete(Paths.get(path))
+        EmptyResponse
+      case MoveFile(pathString, toDirectoryString) ⇒
+        val path = Paths.get(pathString)
+        val toDirectory = Paths.get(toDirectoryString)
+        require(isDirectory(toDirectory), s"Move destination is not a directory: $toDirectory")
+        Files.move(path, toDirectory resolve path.getFileName, REPLACE_EXISTING)
+        EmptyResponse
+    }
+}
