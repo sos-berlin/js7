80d3db3b5aaedae29f05977c9d12c89efb50ae95
Joacim Zschimmer <jogit@zschimmer.com>
2015-09-04 10:38:00 +0200

    (JS-1163) ReturnCode.normalized for interrupted processes

--- engine-data/src/main/scala/com/sos/scheduler/engine/data/job/ReturnCode.scala
+++ engine-data/src/main/scala/com/sos/scheduler/engine/data/job/ReturnCode.scala
@@ -1,14 +1,23 @@
 package com.sos.scheduler.engine.data.job
 
+import com.sos.scheduler.engine.base.process.ProcessSignal
+
 /**
  * @author Joacim Zschimmer
  */
 final case class ReturnCode(toInt: Int) {
   def isSuccess = toInt == 0
+
+  /**
+   * Maps this, as calculated by C++ JobScheduler Engine when process is interrupted, to normal unmodified ReturnCode.
+   */
+  def normalized = if (toInt >= 0) this else ReturnCode(128 - toInt)
 }
 
 object ReturnCode {
   def apply(o: Boolean): ReturnCode = if (o) Success else StandardFailure
+
+  def apply(signal: ProcessSignal) = new ReturnCode(128 + signal.value)
 
   val Success = new ReturnCode(0)
   val StandardFailure = new ReturnCode(1)
