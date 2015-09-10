e66bd437252c7a904dec12a62fe7953ad4782eae
Joacim Zschimmer <jogit@zschimmer.com>
2015-09-10 09:15:25 +0200

    JS-1500 Agent starts shell script file /bin/sh -c, respecting the shebang #! line

--- engine-taskserver/src/test/scala/com/sos/scheduler/engine/taskserver/task/process/ProcessesTest.scala
+++ engine-taskserver/src/test/scala/com/sos/scheduler/engine/taskserver/task/process/ProcessesTest.scala
@@ -6,6 +6,7 @@ import com.sos.scheduler.engine.taskserver.task.process.Processes._
 import com.sos.scheduler.engine.taskserver.task.process.StdoutStderr.Stdout
 import java.lang.ProcessBuilder.Redirect.PIPE
 import java.nio.file.Files.{delete, exists}
+import java.nio.file.Paths
 import org.junit.runner.RunWith
 import org.scalatest.FreeSpec
 import org.scalatest.junit.JUnitRunner
@@ -17,26 +18,48 @@ import scala.collection.JavaConversions._
 @RunWith(classOf[JUnitRunner])
 final class ProcessesTest extends FreeSpec {
 
+  private val args = if (isWindows) List("1-one", "2-'two", "3\\three", "4-*")  // "-key=value" and several other strings does not work !!!
+                     else List("1 one", "2 'two", "3 \"three", "4\\four", "5 *", "-key=value")
+
   "processToPidOption, toShellCommandArguments" in {
     if (isWindows) {
       val process = new ProcessBuilder(toShellCommandArguments("rem")).start()
       assert(processToPidOption(process).isEmpty)
       process.waitFor()
     } else {
-      val process = new ProcessBuilder(toShellCommandArguments("echo $$")).redirectInput(PIPE).start()
-      val echoLine = io.Source.fromInputStream(process.getInputStream).getLines.next
+      val args = toShellCommandArguments("echo $$")
+      assert(args == List("/bin/sh", "-c", "echo $$"))
+      val process = new ProcessBuilder(args).redirectInput(PIPE).start()
+      val echoLine = io.Source.fromInputStream(process.getInputStream).getLines().next()
       assert(processToPidOption(process) contains Pid(echoLine.toLong))
       process.waitFor()
     }
   }
 
-  "newTemporaryShellFile returns executable file, toShellCommandArguments" in {
+  "toShellCommandArguments" in {
+    val file = Paths.get("FILE")
+    val a = toShellCommandArguments(file, args)
+    if (isWindows) {
+      assert(a.toList.tail == List("/C", "FILE") ++ args)
+      intercept[IllegalArgumentException] { toShellCommandArguments(file, List(" ")) }
+      intercept[IllegalArgumentException] { toShellCommandArguments(file, List("\"")) }
+    } else {
+      assert(a.toList == List("/bin/sh", "-c", """'FILE' "$@"""") ++ List("FILE") ++ args)
+    }
+  }
+
+  "newTemporaryShellFile, toShellCommandArguments and script execution" in {
     val file = newTemporaryShellFile("NAME")
     assert(exists(file))
     assert(!(file.toString contains "--"))
-    file.contentString = "echo HELLO TEST\n"
-    val process = new ProcessBuilder(toShellCommandArguments(file)).redirectOutput(PIPE).start()
-    io.Source.fromInputStream(process.getInputStream).getLines exists { _ contains "HELLO TEST" }
+    file.contentString =
+      if (isWindows) "@echo off\n" + (0 to args.size map { i ⇒ s"echo %$i\n" } mkString "")
+      else 0 to args.size map { i ⇒ s"""echo "$$$i"""" + '\n' } mkString ""
+    val process = new ProcessBuilder(toShellCommandArguments(file, args)).redirectOutput(PIPE).start()
+    val echoLines = io.Source.fromInputStream(process.getInputStream).getLines().toList
+    val expectedLines = List(file.toString) ++ args
+    for ((a, b) ← echoLines zip expectedLines) assert(a == b)
+    assert(echoLines.size == expectedLines.size)
     process.waitFor()
     delete(file)
   }
