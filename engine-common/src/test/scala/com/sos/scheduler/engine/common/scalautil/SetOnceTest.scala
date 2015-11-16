6f398ceb4f46f39b2a7822d1efa05b571219f161
Joacim Zschimmer <jogit@zschimmer.com>
2015-11-16 11:17:40 +0100

    SetOnce

---
+++ engine-common/src/test/scala/com/sos/scheduler/engine/common/scalautil/SetOnceTest.scala
@@ -0,0 +1,42 @@
+package com.sos.scheduler.engine.common.scalautil
+
+import org.junit.runner.RunWith
+import org.scalatest.FreeSpec
+import org.scalatest.Matchers._
+import org.scalatest.junit.JUnitRunner
+
+/**
+ * @author Joacim Zschimmer
+ */
+@RunWith(classOf[JUnitRunner])
+final class SetOnceTest extends FreeSpec {
+
+  "SetOnce" in {
+    val a = new SetOnce[Int]
+    assert(a.isEmpty)
+    assert(!a.nonEmpty)
+    assert(!a.isDefined)
+    intercept[IllegalStateException] { a() }
+    assert(a.get == None)
+    assert((a getOrElse -1) == -1)
+    a := 0
+    assert(!a.isEmpty)
+    assert(a.nonEmpty)
+    assert(a.isDefined)
+    assert(a() == 0)
+    assert(a.get == Some(0))
+    assert((a getOrElse -1) == -0)
+    intercept[IllegalStateException] { a := 0 } .getMessage should include ("SetOnce[java.lang.Integer]")
+    assert((for (i ← a) yield (i: Int) + 3) == Some(3))
+    var r = 7
+    for (i ← a) r = a()
+    assert(r == 0)
+  }
+
+  "SetOnce with Implicit" in {
+    val a = new SetOnce[Int] with SetOnce.Implicit
+    intercept[IllegalStateException] { a: Int }
+    a := 0
+    assert((a: Int) == 0)
+  }
+}
--- engine-taskserver/src/main/scala/com/sos/scheduler/engine/taskserver/OwnProcessTaskServer.scala
+++ engine-taskserver/src/main/scala/com/sos/scheduler/engine/taskserver/OwnProcessTaskServer.scala
@@ -4,7 +4,7 @@ import com.google.common.io.Closer
 import com.sos.scheduler.engine.base.process.ProcessSignal
 import com.sos.scheduler.engine.common.scalautil.AutoClosing._
 import com.sos.scheduler.engine.common.scalautil.Closers.implicits.RichClosersCloser
-import com.sos.scheduler.engine.common.scalautil.Logger
+import com.sos.scheduler.engine.common.scalautil.{Logger, SetOnce}
 import com.sos.scheduler.engine.taskserver.OwnProcessTaskServer._
 import com.sos.scheduler.engine.taskserver.data.TaskStartArguments
 import com.sos.scheduler.engine.taskserver.task.process.{JavaProcess, ProcessConfiguration, RichProcess}
@@ -19,7 +19,7 @@ import spray.json._
 final class OwnProcessTaskServer(val taskStartArguments: TaskStartArguments, javaOptions: Seq[String], javaClasspath: String)
 extends TaskServer {
 
-  private var process: RichProcess = null
+  private val processOnce = new SetOnce[RichProcess]
   private val terminatedPromise = Promise[Unit]()
   def terminated = terminatedPromise.future
 
@@ -28,7 +28,7 @@ extends TaskServer {
     val closer = Closer.create()
     closeOnError(closer) {
       closer.onClose { RichProcess.tryDeleteFiles(stdFileMap.values) }
-      process = JavaProcess.startJava(
+      val process = JavaProcess.startJava(
         ProcessConfiguration(
           stdFileMap,
           additionalEnvironment = taskStartArguments.environment,
@@ -38,6 +38,7 @@ extends TaskServer {
         classpath = Some(javaClasspath + File.pathSeparator + JavaProcess.OwnClasspath),
         mainClass = TaskServerMain.getClass.getName stripSuffix "$", // Strip Scala object class suffix
         arguments = Nil)
+      processOnce := process
       process.closed.onComplete { tried ⇒
         for (t ← tried.failed) logger.error(t.toString, t)
         try closer.close()
@@ -50,15 +51,15 @@ extends TaskServer {
   }
 
   override def close(): Unit = {
-    if (process != null) {
+    for (p ← processOnce) {
       // Wait for process _after_ Tunnel, registered with registerCloseable, has been closed
-      try process.waitForTermination()
-      finally process.close()
+      try p.waitForTermination()
+      finally p.close()
     }
   }
 
   def sendProcessSignal(signal: ProcessSignal) =
-    for (p ← Option(process)) p.sendProcessSignal(signal)
+    for (p ← processOnce) p.sendProcessSignal(signal)
 }
 
 object OwnProcessTaskServer {
