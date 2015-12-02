e576685c756e783c8391f3f1998a183c99127b4a
Joacim Zschimmer <jogit@zschimmer.com>
2015-12-02 13:09:47 +0100

    JS-1523 Test

--- engine-agent/src/main/scala/com/sos/scheduler/engine/agent/Agent.scala
+++ engine-agent/src/main/scala/com/sos/scheduler/engine/agent/Agent.scala
@@ -30,7 +30,7 @@ final class Agent(module: Module) extends AutoCloseable {
     this(new AgentModule(configuration))
   }
 
-  private val injector = Guice.createInjector(PRODUCTION, module)
+  val injector = Guice.createInjector(PRODUCTION, module)
   val configuration = injector.instance[AgentConfiguration]
   val localUri = s"http://127.0.0.1:${configuration.httpPort}/${configuration.strippedUriPathPrefix}" stripSuffix "/"
   private val server = injector.instance[AgentWebServer]
