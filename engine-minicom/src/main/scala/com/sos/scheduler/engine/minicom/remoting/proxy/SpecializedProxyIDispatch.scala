4decdd05a2cee91a62b8a787b445617fc1b070d2
Joacim Zschimmer <jogit@zschimmer.com>
2015-12-16 21:18:22 +0100

    Task server: DISPIDCache accellerates COM RPC

--- engine-minicom/src/main/scala/com/sos/scheduler/engine/minicom/remoting/proxy/SpecializedProxyIDispatch.scala
+++ engine-minicom/src/main/scala/com/sos/scheduler/engine/minicom/remoting/proxy/SpecializedProxyIDispatch.scala
@@ -5,7 +5,7 @@ import java.util.NoSuchElementException
 /**
  * @author Joacim Zschimmer
  */
-trait SpecializedProxyIDispatch extends ProxyIDispatch
+trait SpecializedProxyIDispatch extends CachingProxyIDispatch
 
 object SpecializedProxyIDispatch  {
 
