dacb72ee702cbd905a3886581df238cdce26b2a5
Oliver Haufe <oliver.haufe@sos-berlin.com>
2016-01-07 14:24:52 +0100

    JS-1552 kill scripts log to file which is set in environment variable KILL_TASK_LOG_FILE

--- engine-taskserver/src/main/resources/com/sos/scheduler/engine/taskserver/task/process/scripts/unix/jobscheduler_agent_kill_task.sh
+++ engine-taskserver/src/main/resources/com/sos/scheduler/engine/taskserver/task/process/scripts/unix/jobscheduler_agent_kill_task.sh
@@ -4,19 +4,19 @@
 #  Purpose: "kill script" for JobScheduler Agent
 #  ------------------------------------------------------------
 
-KILL_TASK_LOG_FILE="/dev/null"
+test -z "${KILL_TASK_LOG_FILE}" && KILL_TASK_LOG_FILE="/dev/null"
 KILL_TASK_EXIT=0
 PIDS_TREE=
 
 killtree() {
     for _pid in $PIDS_TREE
     do
-      echo "killing pid ${_pid}..." >> ${KILL_TASK_LOG_FILE}
+      date "+%Y-%m-%d %T,%3N %z [info]  killing pid ${_pid}..." >> "${KILL_TASK_LOG_FILE}"
       kill -9 ${_pid}
       kill_err=$?
       if [ $kill_err -ne 0 ]
       then
-        echo "...kill pid ${_pid} failed!" >> ${KILL_TASK_LOG_FILE}
+        date "+%Y-%m-%d %T,%3N %z [error] ...kill pid ${_pid} failed!" >> "${KILL_TASK_LOG_FILE}"
         KILL_TASK_EXIT=$kill_err
       fi
     done
@@ -25,38 +25,51 @@ killtree() {
 stoptree() {
     _pid="$1"
     PIDS_TREE="${_pid} ${PIDS_TREE}"
-    echo "stopping pid ${_pid}..." >> ${KILL_TASK_LOG_FILE}
+    date "+%Y-%m-%d %T,%3N %z [info]  stopping pid ${_pid}..." >> "${KILL_TASK_LOG_FILE}"
     kill -stop ${_pid} # needed to stop quickly forking parent from producing children between child killing and parent killing
     for _child in `ps ax -o "pid= ppid=" | egrep " ${_pid}$" | awk '{print $1}'`; do
-        stoptree "${_child}"
+        stoptree "${_child}" 
     done
 }
 
 for param in "$@"
 do
   case "$param" in
-         -kill-agent-task-id=*)     KILL_TASK_ID=`echo "$param" | sed -e 's/-kill-agent-task-id=//'`
+         -kill-agent-task-id=*)     KILL_TASK_ID=`echo "$param" | sed 's/-kill-agent-task-id=//'`
+                            ;;
+  case "$param" in
+         -kill-master-task-id=*)    MASTER_TASK_ID=`echo "$param" | sed 's/-kill-master-task-id=//'`
+                            ;;
+  case "$param" in
+         -job-name=*)               JOB_NAME=`echo "$param" | sed 's/-job-name=//'`
                             ;;
   esac
 done
 
 if [ -z "${KILL_TASK_ID}" ]
 then
-  echo "option -kill-agent-task-id is not set" >> ${KILL_TASK_LOG_FILE}
+  date "+%Y-%m-%d %T,%3N %z [error] option -kill-agent-task-id is not set" >> "${KILL_TASK_LOG_FILE}"
   exit 2
+fi
+
+if [ ! -z "${JOB_NAME}" ]
+then
+  date "+%Y-%m-%d %T,%3N %z [info]  Task \"${MASTER_TASK_ID}\" of Job \"${JOB_NAME}\" with Agent task id \"${KILL_TASK_ID}\" will be killed." >> "${KILL_TASK_LOG_FILE}"
+else
+  date "+%Y-%m-%d %T,%3N %z [info]  Task with Agent task id \"${KILL_TASK_ID}\" will be killed." >> "${KILL_TASK_LOG_FILE}"
 fi
 
 KILL_TASK_PID=`ps -ef -w -w | grep " -agent-task-id=${KILL_TASK_ID}" | grep -v "grep" | awk '{print $2}'`
 
 if [ -z "${KILL_TASK_PID}" ]
 then
-  echo "process with -agent-task-id=${KILL_TASK_PID} doesn't exist." >> ${KILL_TASK_LOG_FILE}
+  date "+%Y-%m-%d %T,%3N %z [info]  process with -agent-task-id=${KILL_TASK_ID} doesn't exist." >> "${KILL_TASK_LOG_FILE}"
   exit 0
 fi
 
-echo "killing task with pid ${KILL_TASK_PID}" >> ${KILL_TASK_LOG_FILE}
+date "+%Y-%m-%d %T,%3N %z [info]  Killing task with pid ${KILL_TASK_PID} and its children" >> "${KILL_TASK_LOG_FILE}"
 
 stoptree ${KILL_TASK_PID}
 killtree
 
-exit $KILL_TASK_EXIT
+exit $KILL_TASK_EXIT 
--- engine-taskserver/src/main/resources/com/sos/scheduler/engine/taskserver/task/process/scripts/windows/jobscheduler_agent_kill_task.cmd
+++ engine-taskserver/src/main/resources/com/sos/scheduler/engine/taskserver/task/process/scripts/windows/jobscheduler_agent_kill_task.cmd
@@ -7,7 +7,11 @@ rem #  ------------------------------------------------------------
 SETLOCAL
 
 set KILL_TASK_PID=
-set KILL_TASK_LOG_FILE=nul
+set KILL_TASK_ID=
+set MASTER_TASK_ID=
+set JOB_NAME=
+if not defined KILL_TASK_LOG_FILE set KILL_TASK_LOG_FILE=nul
+if not x%KILL_TASK_LOG_FILE%==xnul set KILL_TASK_LOG_FILE="%KILL_TASK_LOG_FILE%"
 goto read_command_line_options_begin
 
 :read_command_line_options
@@ -22,26 +26,41 @@ if "%~1" == "-kill-agent-task-id" (
   set KILL_TASK_ID=%~2
 )
 
+if "%~1" == "-kill-master-task-id" (
+  if "%OPTION_VALUE%" == "" goto read_command_line_options
+  if "%OPTION_VALUE:~0,1" == "-" goto read_command_line_options
+  set MASTER_TASK_ID=%~2
+)
+
+if "%~1" == "-job-name" (
+  if "%OPTION_VALUE%" == "" goto read_command_line_options
+  if "%OPTION_VALUE:~0,1" == "-" goto read_command_line_options
+  set JOB_NAME=%~2
+)
+
 goto read_command_line_options
-:read_command_line_options_end
+:read_command_line_options_end 
 
 if not defined KILL_TASK_ID (
-  echo option -kill-agent-task-id is not set >> %KILL_TASK_LOG_FILE%
+  echo %DATE% %TIME% [error] option -kill-agent-task-id is not set. >> %KILL_TASK_LOG_FILE%
   exit /b 2
 )
+
+if defined JOB_NAME echo %DATE% %TIME% [info]  Task "%MASTER_TASK_ID%" of Job "%JOB_NAME%" with Agent task id "%KILL_TASK_ID%" will be killed. >> %KILL_TASK_LOG_FILE%
+if not defined JOB_NAME echo %DATE% %TIME% [info]  Task with Agent task id "%KILL_TASK_ID%" will be killed. >> %KILL_TASK_LOG_FILE%
 
 for /f "usebackq tokens=2 delims==" %%i in (`wmic process where "commandline like '%% -agent-task-id=%KILL_TASK_ID%%%' and not commandline like '%%-kill-agent-task-id%%'" get processid /format:value 2^>nul`) do set KILL_TASK_PID=%%i
 
 if x%KILL_TASK_PID% == x (
-  echo process with -agent-task-id=%KILL_TASK_ID% doesn't exist. >> %KILL_TASK_LOG_FILE%
+  echo %DATE% %TIME% [info]  process with -agent-task-id=%KILL_TASK_ID% doesn't exist. >> %KILL_TASK_LOG_FILE%
   exit /b 0
 )
-
-echo killing task with pid %KILL_TASK_PID% >> %KILL_TASK_LOG_FILE%
+  
+echo %DATE% %TIME% [info]  Killing task with pid %KILL_TASK_PID% and its children >> %KILL_TASK_LOG_FILE%
 taskkill /PID %KILL_TASK_PID% /F /T
 
 if ERRORLEVEL 1 (
-  echo ...kill task failed! >> %KILL_TASK_LOG_FILE%
+  echo %DATE% %TIME% [error] ...kill task failed! >> %KILL_TASK_LOG_FILE%
 )
 
 exit /b %ERRORLEVEL%
