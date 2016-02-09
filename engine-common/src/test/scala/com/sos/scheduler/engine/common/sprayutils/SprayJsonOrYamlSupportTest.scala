4b7b2266c0172c5b67804cea495f06a7b6ec7472
Joacim Zschimmer <jogit@zschimmer.com>
2016-02-09 13:00:34 +0100

    More Agent web service tests for content types

--- engine-common/src/test/scala/com/sos/scheduler/engine/common/sprayutils/SprayJsonOrYamlSupportTest.scala
+++ engine-common/src/test/scala/com/sos/scheduler/engine/common/sprayutils/SprayJsonOrYamlSupportTest.scala
@@ -7,7 +7,7 @@ import org.scalatest.FreeSpec
 import org.scalatest.junit.JUnitRunner
 import spray.http.HttpHeaders.Accept
 import spray.http.MediaTypes._
-import spray.http.{ContentType, ContentTypes}
+import spray.http.{ContentTypes, MediaRanges}
 import spray.json.DefaultJsonProtocol._
 import spray.json.{JsObject, JsString}
 import spray.routing.HttpService
@@ -22,43 +22,70 @@ final class SprayJsonOrYamlSupportTest extends FreeSpec with ScalatestRouteTest 
   implicit lazy val actorRefFactory = ActorSystem(getClass.getSimpleName)
 
   private val expectedJsObject = JsObject("test" → JsString("TEST"))
+  private val expectedYaml = "test: TEST\n"
+
   private def route = complete { expectedJsObject }
 
   "Response is compact, application/json " - {
     "Accept: application/json" in {
       Get("/") ~> Accept(`application/json`) ~> route ~> check {
-        assert(contentType == ContentTypes.`application/json`)
-        assert(responseAs[JsObject] == expectedJsObject)
-        assert(!(responseAs[String] contains " "))
+        requireJson()
       }
     }
 
     "Accept: application/json, text/plain;q=0.5" in {
       Get("/") ~> Accept(`application/json`, `text/plain` withQValue 0.5) ~> route ~> check {
-        assert(contentType == ContentTypes.`application/json`)
-        assert(responseAs[JsObject] == expectedJsObject)
-        assert(!(responseAs[String] contains " "))
+        requireJson()
       }
+    }
+
+    "Accept: text/plain;q=0.5, application/json" in {
+      Get("/") ~> Accept(`text/plain` withQValue 0.5, `application/json`) ~> route ~> check {
+        requireJson()
+      }
+    }
+
+    def requireJson() = {
+      assert(contentType == ContentTypes.`application/json`)
+      assert(responseAs[JsObject] == expectedJsObject)
+      assert(!(responseAs[String] contains " "))
     }
   }
 
   "Response is pretty, text/plain YAML" - {
+    "Without Accept" in {
+      Get("/") ~> route ~> check {
+        requireYaml()
+      }
+    }
+
+    "Accept: */*" in {
+      Get("/") ~> Accept(MediaRanges.`*/*`) ~> route ~> check {
+        requireYaml()
+      }
+    }
+
     "Accept: text/plain" in {
       Get("/") ~> Accept(`text/plain`) ~> route ~> check {
-        requirePrettyTextPlain(contentType, responseAs[String])
+        requireYaml()
       }
     }
 
     "Accept: text/plain, application/json" in {
       Get("/") ~> Accept(`text/plain`, `application/json`) ~> route ~> check {
-        requirePrettyTextPlain(contentType, responseAs[String])
+        requireYaml()
       }
     }
 
-    def requirePrettyTextPlain(contentType: ContentType, string: String): Unit = {
+    "Accept: application/json, text/plain" in {
+      Get("/") ~> Accept(`application/json`, `text/plain`) ~> route ~> check {
+        requireYaml()
+      }
+    }
+
+    def requireYaml() = {
       assert(contentType == ContentTypes.`text/plain(UTF-8)`)
-      //It's YAML, not JSON: assert(JsonParser(string) == expectedJsObject)
-      assert(string contains " ")
+      assert(responseAs[String] == expectedYaml)
     }
   }
 }
