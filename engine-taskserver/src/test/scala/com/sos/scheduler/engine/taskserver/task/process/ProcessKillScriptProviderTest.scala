d1341114061134781fc6f74c60777a7dee3911c3
Joacim Zschimmer <jogit@zschimmer.com>
2016-04-13 22:42:33 +0200

    JS-1609 Java's Process.exitValue returns numeric value of signal, without adding 128

--- engine-taskserver/src/test/scala/com/sos/scheduler/engine/taskserver/task/process/ProcessKillScriptProviderTest.scala
+++ engine-taskserver/src/test/scala/com/sos/scheduler/engine/taskserver/task/process/ProcessKillScriptProviderTest.scala
@@ -6,7 +6,7 @@ import com.sos.scheduler.engine.base.process.ProcessSignal.SIGKILL
 import com.sos.scheduler.engine.common.scalautil.AutoClosing.autoClosing
 import com.sos.scheduler.engine.common.scalautil.FileUtils.implicits._
 import com.sos.scheduler.engine.common.system.FileUtils._
-import com.sos.scheduler.engine.common.system.OperatingSystem.{isUnix, isWindows}
+import com.sos.scheduler.engine.common.system.OperatingSystem.{isSolaris, isUnix, isWindows}
 import com.sos.scheduler.engine.common.time.ScalaTime._
 import com.sos.scheduler.engine.taskserver.task.process.Processes.{Pid, ShellFileExtension, processToPidOption}
 import java.nio.file.Files._
@@ -65,7 +65,7 @@ final class ProcessKillScriptProviderTest extends FreeSpec {
     sleep(1.s)
     runKillScript(agentTaskId, processToPidOption(process))
     process.waitFor(10, SECONDS)
-    assert(process.exitValue == (if (isWindows) 1 else 128 + SIGKILL.value))
+    assert(process.exitValue == (if (isWindows) 1 else if (isSolaris) SIGKILL.value else 128 + SIGKILL.value))
     sleep(1.s) // Time to let kill take effect
     val beforeKill = out.contentString
     sleep(2.s)
