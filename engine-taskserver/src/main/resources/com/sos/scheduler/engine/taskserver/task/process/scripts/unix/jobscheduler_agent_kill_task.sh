fd29361f636868d95ec9bd36ca3c58c824bb763a
Joacim Zschimmer <jogit@zschimmer.com>
2016-04-14 13:19:16 +0200

    JS-1574 Refactored kill script

--- engine-taskserver/src/main/resources/com/sos/scheduler/engine/taskserver/task/process/scripts/unix/jobscheduler_agent_kill_task.sh
+++ engine-taskserver/src/main/resources/com/sos/scheduler/engine/taskserver/task/process/scripts/unix/jobscheduler_agent_kill_task.sh
@@ -14,11 +14,11 @@ log() {
 
 killtree() {
     for _pid in $PIDS_TREE; do
-        log info "kill -KILL ${_pid}"
-        kill -KILL ${_pid}
+        log info "kill -KILL $_pid"
+        kill -KILL $_pid
         kill_err=$?
         if [ $kill_err -ne 0 ]; then
-            log error "...kill pid ${_pid} failed!"
+            log error "...kill pid $_pid failed!"
             KILL_TASK_EXIT=$kill_err
         fi
     done
@@ -41,11 +41,11 @@ fi
 stoptree() {
     # First stop all processes to inhibit quickly forking parent from producing children between child killing and parent killing
     _pid="$1"
-    PIDS_TREE="${_pid} ${PIDS_TREE}"
-    log info "kill -STOP ${_pid} "
-    kill -STOP ${_pid}
-    for _child in `$psTree | egrep " ${_pid}$" | awk '{print $1}'`; do
-        stoptree "${_child}"
+    PIDS_TREE="$_pid $PIDS_TREE"
+    log info "kill -STOP $_pid "
+    kill -STOP $_pid
+    for _child in `$psTree | egrep " $_pid$" | awk '{print $1}'`; do
+        stoptree "$_child"
     done
 }
 
@@ -67,28 +67,28 @@ for param in "$@"; do
     esac
 done
 
-if [ -z "${KILL_TASK_ID}$PID" ]; then
+if [ -z "$KILL_TASK_ID$PID" ]; then
     log error "Option -kill-agent-task-id is not set"
     exit 2
 fi
 
-if [ ! -z "${JOB_NAME}" ]; then
-    log info "Task '${MASTER_TASK_ID}' of Job '${JOB_NAME}' with Agent task id '${KILL_TASK_ID}' will be killed"
+if [ ! -z "$JOB_NAME" ]; then
+    log info "Task '$MASTER_TASK_ID' of Job '$JOB_NAME' with Agent task id '$KILL_TASK_ID' will be killed"
 else
-    log info "Task with Agent task id '${KILL_TASK_ID}' will be killed"
+    log info "Task with Agent task id '$KILL_TASK_ID' will be killed"
 fi
 
-KILL_TASK_PID=`ps ww | grep " -agent-task-id=${KILL_TASK_ID}" | grep -v "grep" | awk '{ print $1 }'`
+KILL_TASK_PID=`ps ww | grep " -agent-task-id=$KILL_TASK_ID" | grep -v "grep" | awk '{ print $1 }'`
 
 [ ! -z "$KILL_TASK_PID" ] || KILL_TASK_PID="$PID"
 
-if [ -z "${KILL_TASK_PID}" ]; then
-    log info "Process with -agent-task-id=${KILL_TASK_ID} doesn't exist"
+if [ -z "$KILL_TASK_PID" ]; then
+    log info "Process with -agent-task-id=$KILL_TASK_ID doesn't exist"
     exit 0
 fi
 
-log info "Killing task with pid ${KILL_TASK_PID} and its children"
-stoptree ${KILL_TASK_PID}
+log info "Killing task with pid $KILL_TASK_PID and its children"
+stoptree "$KILL_TASK_PID"
 killtree
 
 exit $KILL_TASK_EXIT
