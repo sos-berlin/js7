dc6867ae9d3d1d92de0688c7ea6c62692b5f511c
Joacim Zschimmer <jogit@zschimmer.com>
2016-04-14 13:14:39 +0200

    JS-1574 Refactored kill script logging

--- engine-taskserver/src/main/resources/com/sos/scheduler/engine/taskserver/task/process/scripts/unix/jobscheduler_agent_kill_task.sh
+++ engine-taskserver/src/main/resources/com/sos/scheduler/engine/taskserver/task/process/scripts/unix/jobscheduler_agent_kill_task.sh
@@ -4,18 +4,21 @@
 #  Purpose: "kill script" for JobScheduler Agent
 #  ------------------------------------------------------------
 
-test -z "${KILL_TASK_LOG_FILE}" && KILL_TASK_LOG_FILE="/dev/null"
 KILL_TASK_EXIT=0
 PIDS_TREE=
 
+log() {
+    echo "[$1]  $2" 1>&2
+    test -z "$KILL_TASK_LOG_FILE" || echo "`date '+%Y-%m-%d %T,%3N %z'` [$1]  $2" >> "$KILL_TASK_LOG_FILE"
+}
+
 killtree() {
     for _pid in $PIDS_TREE; do
-        date "+%Y-%m-%d %T,%3N %z [info]  killing pid ${_pid}..." >> "${KILL_TASK_LOG_FILE}"
-        echo kill -KILL ${_pid} 1>&2
+        log info "kill -KILL ${_pid}"
         kill -KILL ${_pid}
         kill_err=$?
         if [ $kill_err -ne 0 ]; then
-            date "+%Y-%m-%d %T,%3N %z [error] ...kill pid ${_pid} failed!" >> "${KILL_TASK_LOG_FILE}"
+            log error "...kill pid ${_pid} failed!"
             KILL_TASK_EXIT=$kill_err
         fi
     done
@@ -39,8 +42,7 @@ stoptree() {
     # First stop all processes to inhibit quickly forking parent from producing children between child killing and parent killing
     _pid="$1"
     PIDS_TREE="${_pid} ${PIDS_TREE}"
-    date "+%Y-%m-%d %T,%3N %z [info]  stopping pid ${_pid}..." >> "${KILL_TASK_LOG_FILE}"
-    echo kill -STOP ${_pid} 1>&2
+    log info "kill -STOP ${_pid} "
     kill -STOP ${_pid}
     for _child in `$psTree | egrep " ${_pid}$" | awk '{print $1}'`; do
         stoptree "${_child}"
@@ -66,14 +68,14 @@ for param in "$@"; do
 done
 
 if [ -z "${KILL_TASK_ID}$PID" ]; then
-    date "+%Y-%m-%d %T,%3N %z [error] option -kill-agent-task-id is not set" >> "${KILL_TASK_LOG_FILE}"
+    log error "Option -kill-agent-task-id is not set"
     exit 2
 fi
 
 if [ ! -z "${JOB_NAME}" ]; then
-    date "+%Y-%m-%d %T,%3N %z [info]  Task \"${MASTER_TASK_ID}\" of Job \"${JOB_NAME}\" with Agent task id \"${KILL_TASK_ID}\" will be killed." >> "${KILL_TASK_LOG_FILE}"
+    log info "Task '${MASTER_TASK_ID}' of Job '${JOB_NAME}' with Agent task id '${KILL_TASK_ID}' will be killed"
 else
-    date "+%Y-%m-%d %T,%3N %z [info]  Task with Agent task id \"${KILL_TASK_ID}\" will be killed." >> "${KILL_TASK_LOG_FILE}"
+    log info "Task with Agent task id '${KILL_TASK_ID}' will be killed"
 fi
 
 KILL_TASK_PID=`ps ww | grep " -agent-task-id=${KILL_TASK_ID}" | grep -v "grep" | awk '{ print $1 }'`
@@ -81,11 +83,11 @@ KILL_TASK_PID=`ps ww | grep " -agent-task-id=${KILL_TASK_ID}" | grep -v "grep" |
 [ ! -z "$KILL_TASK_PID" ] || KILL_TASK_PID="$PID"
 
 if [ -z "${KILL_TASK_PID}" ]; then
-    date "+%Y-%m-%d %T,%3N %z [info]  process with -agent-task-id=${KILL_TASK_ID} doesn't exist." >> "${KILL_TASK_LOG_FILE}"
+    log info "Process with -agent-task-id=${KILL_TASK_ID} doesn't exist"
     exit 0
 fi
 
-date "+%Y-%m-%d %T,%3N %z [info]  Killing task with pid ${KILL_TASK_PID} and its children" >> "${KILL_TASK_LOG_FILE}"
+log info "Killing task with pid ${KILL_TASK_PID} and its children"
 stoptree ${KILL_TASK_PID}
 killtree
 
