066552a867024960dabfe423e634ddb2ac461a09
Oliver Haufe <oliver.haufe@sos-berlin.com>
2016-04-22 00:47:55 +0200

    JS-1574
    windows kill script without log file

--- engine-taskserver/src/main/resources/com/sos/scheduler/engine/taskserver/task/process/scripts/windows/jobscheduler_agent_kill_task.cmd
+++ engine-taskserver/src/main/resources/com/sos/scheduler/engine/taskserver/task/process/scripts/windows/jobscheduler_agent_kill_task.cmd
@@ -10,8 +10,6 @@ set KILL_TASK_PID=
 set KILL_TASK_ID=
 set MASTER_TASK_ID=
 set JOB_PATH=
-if not defined KILL_TASK_LOG_FILE set KILL_TASK_LOG_FILE=nul
-if not x%KILL_TASK_LOG_FILE%==xnul set KILL_TASK_LOG_FILE="%KILL_TASK_LOG_FILE%"
 goto read_command_line_options_begin
 
 :read_command_line_options
@@ -42,25 +40,25 @@ goto read_command_line_options
 :read_command_line_options_end
 
 if not defined KILL_TASK_ID (
-  echo %DATE% %TIME% [error] option -kill-agent-task-id is not set. >> %KILL_TASK_LOG_FILE%
+  echo %DATE% %TIME% [error] option -kill-agent-task-id is not set. 1>&2
   exit /b 2
 )
 
-if defined JOB_PATH echo %DATE% %TIME% [info]  Task "%MASTER_TASK_ID%" of Job "%JOB_PATH%" with Agent task id "%KILL_TASK_ID%" will be killed. >> %KILL_TASK_LOG_FILE%
-if not defined JOB_PATH echo %DATE% %TIME% [info]  Task with Agent task id "%KILL_TASK_ID%" will be killed. >> %KILL_TASK_LOG_FILE%
+if defined JOB_PATH echo %DATE% %TIME% [info]  Task "%MASTER_TASK_ID%" of Job "%JOB_PATH%" with Agent task id "%KILL_TASK_ID%" will be killed. 1>&2
+if not defined JOB_PATH echo %DATE% %TIME% [info]  Task with Agent task id "%KILL_TASK_ID%" will be killed. 1>&2
 
 for /f "usebackq tokens=2 delims==" %%i in (`wmic process where "commandline like '%% -agent-task-id=%KILL_TASK_ID%%%' and not commandline like '%%-kill-agent-task-id%%'" get processid /format:value 2^>nul`) do set KILL_TASK_PID=%%i
 
 if x%KILL_TASK_PID% == x (
-  echo %DATE% %TIME% [info]  process with -agent-task-id=%KILL_TASK_ID% doesn't exist. >> %KILL_TASK_LOG_FILE%
+  echo %DATE% %TIME% [info]  process with -agent-task-id=%KILL_TASK_ID% doesn't exist. 1>&2
   exit /b 0
 )
 
-echo %DATE% %TIME% [info]  Killing task with pid %KILL_TASK_PID% and its children >> %KILL_TASK_LOG_FILE%
-taskkill /PID %KILL_TASK_PID% /F /T
+echo %DATE% %TIME% [info]  Killing task with pid %KILL_TASK_PID% and its children 1>&2
+taskkill /PID %KILL_TASK_PID% /F /T 1>&2
 
 if ERRORLEVEL 1 (
-  echo %DATE% %TIME% [error] ...kill task failed! >> %KILL_TASK_LOG_FILE%
+  echo %DATE% %TIME% [error] ...kill task failed! 1>&2
 )
 
 exit /b %ERRORLEVEL%
