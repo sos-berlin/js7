0f8c1f27150d0830134d233dab106c7c9a6fdbd0
Joacim Zschimmer <jogit@zschimmer.com>
2016-06-14 11:13:12 +0200

    ConcurrentMemoizer

---
+++ engine-common/src/main/scala/com/sos/scheduler/engine/common/scalautil/ConcurrentMemoizer.scala
@@ -0,0 +1,35 @@
+package com.sos.scheduler.engine.common.scalautil
+
+/**
+  * Memoizes all computed results - fast, but in case of concurrent calls, results may be computed twice.
+  * For strictly one computation, see `ConcurrentMemoizer.strict`.
+  *
+  * @author Joacim Zschimmer
+  */
+final class ConcurrentMemoizer[A, B](f: A ⇒ B) extends (A ⇒ B) {
+
+  private val memory = new ScalaConcurrentHashMap[A, B]
+
+  def apply(a: A): B = memory.getOrElseUpdate(a, f(a))
+
+  override def toString = s"ConcurrentMemoizer($f, ${memory.size} results memoized)"
+}
+
+object ConcurrentMemoizer {
+  def apply[A, B](f: A ⇒ B) = new ConcurrentMemoizer[A, B](f)
+
+  def apply[A, B, R](f: (A, B) ⇒ R) = Function.untupled(new ConcurrentMemoizer[(A, B), R](f.tupled))
+
+  /**
+    * Memoizes all computed results and computes each result strictly once.
+    */
+  def strict[A, B](f: A ⇒ B): A ⇒ B = {
+    val memoizer = new ConcurrentMemoizer[A, B](f)
+    (a: A) ⇒ memoizer.synchronized { memoizer(a) }
+  }
+
+  /**
+    * Memoizes all computed results and computes each result strictly once.
+    */
+  def strict[A, B, R](f: (A, B) ⇒ R) = Function.untupled(strict[(A, B), R](f.tupled))
+}
