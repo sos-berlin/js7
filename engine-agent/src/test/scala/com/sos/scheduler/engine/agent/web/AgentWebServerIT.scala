ab7463283105b0fe7337d27616837efd8fbe7332
Joacim Zschimmer <jogit@zschimmer.com>
2016-06-21 08:46:03 +0200

    AgentWebServiceIT with (inactivated) performance test

--- engine-agent/src/test/scala/com/sos/scheduler/engine/agent/web/AgentWebServerIT.scala
+++ engine-agent/src/test/scala/com/sos/scheduler/engine/agent/web/AgentWebServerIT.scala
@@ -4,17 +4,22 @@ import akka.actor.ActorSystem
 import com.google.inject.Guice
 import com.sos.scheduler.engine.agent.configuration.AgentConfiguration
 import com.sos.scheduler.engine.agent.configuration.inject.AgentModule
+import com.sos.scheduler.engine.agent.data.views.TaskHandlerOverview
 import com.sos.scheduler.engine.agent.test.AgentConfigDirectoryProvider
 import com.sos.scheduler.engine.agent.views.AgentOverview
 import com.sos.scheduler.engine.agent.web.AgentWebServerIT._
 import com.sos.scheduler.engine.base.generic.SecretString
 import com.sos.scheduler.engine.common.guice.GuiceImplicits._
 import com.sos.scheduler.engine.common.scalautil.Closers.implicits.RichClosersAny
-import com.sos.scheduler.engine.common.scalautil.Futures.implicits.SuccessFuture
-import com.sos.scheduler.engine.common.scalautil.HasCloser
+import com.sos.scheduler.engine.common.scalautil.Futures.implicits._
+import com.sos.scheduler.engine.common.scalautil.ScalaUtils.implicitClass
+import com.sos.scheduler.engine.common.scalautil.{HasCloser, Logger}
+import com.sos.scheduler.engine.common.sprayutils.JsObjectMarshallers.JsObjectUnmarshaller
 import com.sos.scheduler.engine.common.sprayutils.https.Https._
 import com.sos.scheduler.engine.common.sprayutils.https.KeystoreReference
 import com.sos.scheduler.engine.common.time.ScalaTime._
+import com.sos.scheduler.engine.common.time.Stopwatch
+import com.sos.scheduler.engine.common.time.timer.TimerServiceOverview
 import com.sos.scheduler.engine.common.utils.FreeTcpPortFinder.findRandomFreeTcpPorts
 import com.sos.scheduler.engine.common.utils.JavaResource
 import java.net.InetSocketAddress
@@ -24,15 +29,18 @@ import org.scalatest.junit.JUnitRunner
 import org.scalatest.{BeforeAndAfterAll, FreeSpec}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.reflect.ClassTag
 import spray.client.pipelining._
 import spray.http.CacheDirectives.{`no-cache`, `no-store`}
 import spray.http.HttpHeaders.{Accept, `Cache-Control`}
 import spray.http.MediaTypes._
 import spray.http.StatusCodes.Unauthorized
-import spray.http.{BasicHttpCredentials, HttpRequest}
+import spray.http.{BasicHttpCredentials, HttpRequest, HttpResponse, Uri}
 import spray.httpx.SprayJsonSupport._
 import spray.httpx.UnsuccessfulResponseException
 import spray.httpx.encoding.Gzip
+import spray.httpx.unmarshalling.FromResponseUnmarshaller
+import spray.json.JsObject
 
 /**
  * @author Joacim Zschimmer
@@ -42,21 +50,21 @@ final class AgentWebServerIT extends FreeSpec with HasCloser with BeforeAndAfter
 
   private lazy val List(httpsPort, httpPort) = findRandomFreeTcpPorts(2)
   private lazy val agentConfiguration = AgentConfiguration
-    .forTest(Some(dataDirectory))
+    .forTest(Some(dataDirectory)) //, config = ConfigFactory.parseMap(Map("spray.can.server.pipelining-limit" → "100", "spray.can.host-connector.pipelining" → "on")))
     .copy(
       httpAddress = Some(new InetSocketAddress("127.0.0.1", httpPort)))
     .withHttpsInetSocketAddress(new InetSocketAddress("127.0.0.1", httpsPort))
   private lazy val webServer = Guice.createInjector(new AgentModule(agentConfiguration)).instance[AgentWebServer]
   private implicit lazy val actorSystem = ActorSystem("AgentWebServerIT") withCloser { _.shutdown() }
 
-  private def pipeline(password: Option[String]): HttpRequest ⇒ Future[AgentOverview] =
+  private def pipeline[A: FromResponseUnmarshaller](password: Option[String]): HttpRequest ⇒ Future[A] =
     (password map { o ⇒ addCredentials(BasicHttpCredentials("SHA512-USER", o)) } getOrElse identity: RequestTransformer) ~>
     addHeader(Accept(`application/json`)) ~>
     addHeader(`Cache-Control`(`no-cache`, `no-store`)) ~>
     encode(Gzip) ~>
     sendReceive ~>
     decode(Gzip) ~>
-    unmarshal[AgentOverview]
+    unmarshal[A]
 
   override protected def beforeAll() = {
     acceptTlsCertificateFor(ClientKeystoreRef, webServer.localHttpsUriOption.get)
@@ -72,57 +80,83 @@ final class AgentWebServerIT extends FreeSpec with HasCloser with BeforeAndAfter
     webServer.start() await 10.s
   }
 
-  "HTTPS" - {
-    lazy val uri = s"${webServer.localHttpsUriOption.get}/$OverviewPath"
-
-    "Unauthorized due to missing credentials" in {
-      intercept[UnsuccessfulResponseException] {
-        pipeline(password = None)(Get(uri)) await 10.s
-      }
-      .response.status shouldEqual Unauthorized
-    }
-
-    "Unauthorized due to wrong credentials" in {
-      intercept[UnsuccessfulResponseException] {
-        pipeline(Some("WRONG-PASSWORD")).apply(Get(uri)) await 10.s
-      }
-      .response.status shouldEqual Unauthorized
-    }
-
-    "Authorized" in {
-      val overview = pipeline(Some("SHA512-PASSWORD")).apply(Get(uri)) await 10.s
-      assert(overview.totalTaskCount == 0)
-    }
-  }
-
-  "HTTP" - {
-    lazy val uri = s"${webServer.localHttpUriOption.get}/$OverviewPath"
-
-    "Without credentials" in {
-      val overview = pipeline(password = None)(Get(uri)) await 10.s
-      assert(overview.totalTaskCount == 0)
-    }
-
-    "Credentials are ignored" in {
-      val overview = pipeline(Some("WRONG-PASSWORD")).apply(Get(uri)) await 10.s
-      assert(overview.totalTaskCount == 0)
-    }
-  }
-
   "WebService fails when HTTP port is not available" in {
     val webServer = Guice.createInjector(new AgentModule(agentConfiguration)).instance[AgentWebServer]
     intercept[RuntimeException] { webServer.start() await 10.s }
       .getMessage should include (s"127.0.0.1:$httpPort")
   }
 
+  "HTTPS" - {
+    lazy val uri = s"${webServer.localHttpsUriOption.get}/$Api"
+
+    "Unauthorized due to missing credentials" in {
+      intercept[UnsuccessfulResponseException] {
+        pipeline[HttpResponse](password = None).apply(Get(s"$uri/task")) await 10.s
+      }
+      .response.status shouldEqual Unauthorized
+    }
+
+    "Unauthorized due to wrong credentials" in {
+      intercept[UnsuccessfulResponseException] {
+        pipeline[AgentOverview](Some("WRONG-PASSWORD")).apply(Get(uri)) await 10.s
+      }
+      .response.status shouldEqual Unauthorized
+    }
+
+    "Authorized" in {
+      val overview = pipeline[AgentOverview](Some("SHA512-PASSWORD")).apply(Get(uri)) await 10.s
+      assert(overview.totalTaskCount == 0)
+    }
+
+    addThroughputMeasurementTests(uri)
+  }
+
+  "HTTP" - {
+    lazy val uri = s"${webServer.localHttpUriOption.get}/$Api"
+
+    "Without credentials" in {
+      val overview = pipeline[AgentOverview](password = None).apply(Get(uri)) await 10.s
+      assert(overview.totalTaskCount == 0)
+    }
+
+    "Credentials are ignored" in {
+      val overview = pipeline[AgentOverview](Some("WRONG-PASSWORD")).apply(Get(uri)) await 10.s
+      assert(overview.totalTaskCount == 0)
+    }
+
+    addThroughputMeasurementTests(uri)
+  }
+
   "close" in {
     webServer.close()
+  }
+
+  private def addThroughputMeasurementTests(uri: Uri): Unit = {
+    if (false) {
+      addThroughputMeasurementTest[TaskHandlerOverview](s"$uri/task")
+      addThroughputMeasurementTest[TimerServiceOverview](s"$uri/timer")
+    }
+  }
+
+  private def addThroughputMeasurementTest[A: FromResponseUnmarshaller: ClassTag](uri: Uri) {
+    s"Measure throughput of ${implicitClass[A].getSimpleName}" in {
+      val get = Get(uri)
+      val p = pipeline[A](Some("SHA512-PASSWORD"))
+      val m = 2 * sys.runtime.availableProcessors
+      val n = 1000
+      val result: A = p(get) await 10.s
+      logger.info(s"${m * n} $result")
+      val stopwatch = new Stopwatch
+      (for (_ ← 1 to m) yield Future { for (i ← 1 to n) p(get) await 10.s }) await 60.s
+      logger.info(stopwatch.itemsPerSecondString(m * n, "request"))
+    }
   }
 }
 
 private object AgentWebServerIT {
-  private val OverviewPath = "jobscheduler/agent/api"
+  private val Api = "jobscheduler/agent/api"
   private val ClientKeystoreRef = KeystoreReference(
     JavaResource("com/sos/scheduler/engine/agent/test/https.jks").url,
     Some(SecretString("jobscheduler")))
+  private val logger = Logger(getClass)
 }
