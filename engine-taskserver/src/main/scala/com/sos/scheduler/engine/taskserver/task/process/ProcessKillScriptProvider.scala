f334bbe6fd9c43d4c89f34999e8d045447aa2d4f
Joacim Zschimmer <jogit@zschimmer.com>
2016-06-23 13:38:03 +0200

    JS-1633 ProcessKillScriptProvider works for concurrent tests

--- engine-taskserver/src/main/scala/com/sos/scheduler/engine/taskserver/task/process/ProcessKillScriptProvider.scala
+++ engine-taskserver/src/main/scala/com/sos/scheduler/engine/taskserver/task/process/ProcessKillScriptProvider.scala
@@ -1,17 +1,16 @@
 package com.sos.scheduler.engine.taskserver.task.process
 
-import com.google.common.io.Files.asByteSink
-import com.google.common.io.Resources
 import com.sos.scheduler.engine.agent.data.ProcessKillScript
-import com.sos.scheduler.engine.common.process.Processes
 import com.sos.scheduler.engine.common.scalautil.FileUtils.implicits._
 import com.sos.scheduler.engine.common.scalautil.{HasCloser, Logger}
 import com.sos.scheduler.engine.common.system.OperatingSystem._
 import com.sos.scheduler.engine.common.utils.Exceptions.ignoreException
 import com.sos.scheduler.engine.common.utils.JavaResource
 import com.sos.scheduler.engine.taskserver.task.process.ProcessKillScriptProvider._
-import java.nio.file.Files.{createFile, delete, deleteIfExists}
+import java.nio.file.Files.{delete, exists, setPosixFilePermissions}
 import java.nio.file.Path
+import java.nio.file.attribute.PosixFilePermission.{OWNER_EXECUTE, OWNER_READ}
+import scala.collection.JavaConversions._
 
 /**
  * @author Joacim Zschimmer
@@ -21,12 +20,17 @@ final class ProcessKillScriptProvider extends HasCloser {
   def provideTo(directory: Path): ProcessKillScript = {
     val resource = if (isWindows) WindowsScriptResource else UnixScriptResource
     val file = directory / resource.simpleName
-    deleteIfExists(file)
-    createFile(file, Processes.ShellFileAttributes: _*)
-    Resources.asByteSource(resource.url) copyTo asByteSink(file)
-    onClose {
-      ignoreException(logger.error) { delete(file) }
+    val content = resource.contentBytes
+    if (!exists(file) || !file.contentBytes.sameElements(content)) {
+      file.contentBytes = content
+      onClose {
+        ignoreException(logger.error) { delete(file) }
+      }
+      if (isUnix)  {
+        setPosixFilePermissions(file, Set(OWNER_READ, OWNER_EXECUTE))
+      }
     }
+
     ProcessKillScript(file)
   }
 }
