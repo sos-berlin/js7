7fcbef355ba2d5d5d3d4717c009fc043cea7ada3
Joacim Zschimmer <jogit@zschimmer.com>
2016-06-30 15:35:15 +0200

    JS-1642 Refactored CppTimeConversions

--- engine-data/src/main/java/com/sos/scheduler/engine/data/configuration/SchedulerDataConstants.java
+++ engine-data/src/main/java/com/sos/scheduler/engine/data/configuration/SchedulerDataConstants.java
@@ -3,6 +3,6 @@ package com.sos.scheduler.engine.data.configuration;
 public final class SchedulerDataConstants {
     private SchedulerDataConstants() {}
 
-    public static final long eternalCppMillis = Integer.MAX_VALUE * 1000L;     // Wie C++ time::eternal
-    public static final long neverCppMillis = Integer.MAX_VALUE * 1000L;     // Wie C++ Duration::never
+    public static final long EternalCppMillis = Integer.MAX_VALUE * 1000L;     // Wie C++ time::eternal
+    public static final long NeverCppMillis = Integer.MAX_VALUE * 1000L;     // Wie C++ Duration::never
 }
