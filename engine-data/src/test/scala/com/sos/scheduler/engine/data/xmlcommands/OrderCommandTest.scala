dd93ccf4261308736fe8a561b17b6e1978d025db
Joacim Zschimmer <jogit@zschimmer.com>
2016-06-30 04:08:55 +0200

    JS-1642 OrderCommand with at=

--- engine-data/src/test/scala/com/sos/scheduler/engine/data/xmlcommands/OrderCommandTest.scala
+++ engine-data/src/test/scala/com/sos/scheduler/engine/data/xmlcommands/OrderCommandTest.scala
@@ -2,6 +2,7 @@ package com.sos.scheduler.engine.data.xmlcommands
 
 import com.sos.scheduler.engine.data.jobchain.JobChainPath
 import com.sos.scheduler.engine.data.order.OrderState
+import java.time.Instant
 import org.junit.runner.RunWith
 import org.scalatest.FreeSpec
 import org.scalatest.Matchers._
@@ -15,10 +16,11 @@ final class OrderCommandTest extends FreeSpec {
   "OrderCommand" in {
     val orderKey = JobChainPath("/JOBCHAIN") orderKey "ID"
     OrderCommand(orderKey).xmlElem shouldEqual <order job_chain="/JOBCHAIN" id="ID"/>
-    OrderCommand(orderKey, suspended = Some(true)).xmlElem shouldEqual <order job_chain="/JOBCHAIN" id="ID" suspended="true"/>
     OrderCommand(orderKey, state = Some(OrderState("STATE"))).xmlElem shouldEqual <order job_chain="/JOBCHAIN" id="ID" state="STATE"/>
+    OrderCommand(orderKey, at = Some(Instant.parse("2016-06-29T12:33:44Z"))).xmlElem shouldEqual <order job_chain="/JOBCHAIN" id="ID" at="2016-06-29T12:33:44Z"/>
+    OrderCommand(orderKey, suspended = Some(true)).xmlElem shouldEqual <order job_chain="/JOBCHAIN" id="ID" suspended="true"/>
     OrderCommand(orderKey, title= Some("TITLE")).xmlElem shouldEqual <order job_chain="/JOBCHAIN" id="ID" title="TITLE"/>
-    OrderCommand(orderKey, parameters = Map("a" -> "1", "b" -> "2")).xmlElem shouldEqual
+    OrderCommand(orderKey, parameters = Map("a" → "1", "b" → "2")).xmlElem shouldEqual
       <order job_chain="/JOBCHAIN" id="ID"><params><param name="a" value="1"/><param name="b" value="2"/></params></order>
   }
 }
