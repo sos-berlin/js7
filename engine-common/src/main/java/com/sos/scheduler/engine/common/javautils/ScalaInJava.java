240debfca7e20c8ba40d9c616285bf819173d014
Joacim Zschimmer <jogit@zschimmer.com>
2016-06-30 13:19:19 +0200

    JS-1642 Java adapter

--- engine-common/src/main/java/com/sos/scheduler/engine/common/javautils/ScalaInJava.java
+++ engine-common/src/main/java/com/sos/scheduler/engine/common/javautils/ScalaInJava.java
@@ -1,5 +1,8 @@
 package com.sos.scheduler.engine.common.javautils;
 
+import java.util.Optional;
+import java.util.concurrent.Future;
+
 /**
  * Public functions for use of Scala in Java.
  *
@@ -16,5 +19,13 @@ public class ScalaInJava {
         @SuppressWarnings("varargs")
         scala.collection.immutable.Set<A> result = ScalaInJavaHelper$.MODULE$.toScalaSet(o);
         return result;
+    }
+
+    public static <A> Future<A> asJavaFuture(scala.concurrent.Future<A> scalaFuture) {
+        return new ScalaInJavaFuture<>(scalaFuture);
+    }
+
+    public static <A> Optional<A> toJavaOptional(scala.Option<A> scalaOption) {
+        return scalaOption.isDefined() ? Optional.of(scalaOption.get()) : Optional.empty();
     }
 }
