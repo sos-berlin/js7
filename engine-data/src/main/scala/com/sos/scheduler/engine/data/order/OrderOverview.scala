bdfc54f69e4bd7642f402c311c7227295c58a1b0
Joacim Zschimmer <jogit@zschimmer.com>
2016-06-30 13:18:26 +0200

    JS-1642 OrderOverview

---
+++ engine-data/src/main/scala/com/sos/scheduler/engine/data/order/OrderOverview.scala
@@ -0,0 +1,28 @@
+package com.sos.scheduler.engine.data.order
+
+import com.sos.scheduler.engine.base.sprayjson.JavaTimeJsonFormats.implicits._
+import com.sos.scheduler.engine.data.filebased.{FileBasedOverview, FileBasedState}
+import com.sos.scheduler.engine.data.job.TaskId
+import java.time.Instant
+import scala.language.implicitConversions
+import spray.json.DefaultJsonProtocol._
+
+/**
+  * @author Joacim Zschimmer
+  */
+final case class OrderOverview(
+  path: OrderKey,
+  fileBasedState: FileBasedState,
+  orderState: OrderState,
+  nextStepAt: Option[Instant] = None,
+  setbackUntil: Option[Instant] = None,
+  taskId: Option[TaskId] = None,
+  isOnBlacklist: Boolean = false,
+  isSuspended: Boolean = false
+)
+extends FileBasedOverview
+
+object OrderOverview {
+  private implicit val FileBasedStateJsonFormat = FileBasedState.MyJsonFormat
+  implicit val MyJsonFormat = jsonFormat8(apply)
+}
