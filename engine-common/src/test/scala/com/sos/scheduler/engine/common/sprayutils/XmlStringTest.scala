9887fe6312f66cb3be26118ea269529f62aabaad
Joacim Zschimmer <jogit@zschimmer.com>
2016-07-04 12:44:13 +0200

    XmlStringTest

--- engine-common/src/test/scala/com/sos/scheduler/engine/common/sprayutils/XmlStringTest.scala
+++ engine-common/src/test/scala/com/sos/scheduler/engine/common/sprayutils/XmlStringTest.scala
@@ -1,36 +1,55 @@
 package com.sos.scheduler.engine.common.sprayutils
 
+import com.sos.scheduler.engine.common.sprayutils.XmlStringTest._
 import java.nio.charset.StandardCharsets.UTF_8
 import org.junit.runner.RunWith
 import org.scalatest.FreeSpec
 import org.scalatest.junit.JUnitRunner
 import spray.http.HttpCharsets.`UTF-8`
 import spray.http.HttpEntity
-import spray.http.MediaTypes.{`application/xml`, `text/xml`}
+import spray.http.MediaTypes._
+import spray.http.StatusCodes.OK
 import spray.httpx.marshalling._
 import spray.httpx.unmarshalling._
+import spray.routing.Directives._
+import spray.routing.Route
+import spray.testkit.ScalatestRouteTest
 
 /**
  * @author Joacim Zschimmer
  */
 @RunWith(classOf[JUnitRunner])
-final class XmlStringTest extends FreeSpec {
+final class XmlStringTest extends FreeSpec with ScalatestRouteTest {
 
-  val xmlString = "<a><Ä/></a>"
-  val entityBytes = xmlString.getBytes(UTF_8)
+  private val xmlString = XmlString("<a><Ä/></a>")
+  private val bytes = xmlString.string.getBytes(UTF_8)
 
   "Marshal as application/xml" in {
-    val entity = HttpEntity(`application/xml`, entityBytes)
-    assert(marshal(XmlString(xmlString)) == Right(entity))
+    val entity = HttpEntity(`application/xml`, bytes)
+    assert(marshal(xmlString) == Right(entity))
   }
 
   "Unmarshal application/xml" in {
-    val entity = HttpEntity(`application/xml`, entityBytes)
-    assert(entity.as[XmlString] == Right(XmlString(xmlString)))
+    val entity = HttpEntity(`application/xml`, bytes)
+    assert(entity.as[XmlString] == Right(xmlString))
   }
 
   "Unmarshal text/xml" in {
-    val entity = HttpEntity(`text/xml` withCharset `UTF-8`, entityBytes)
-    assert(entity.as[XmlString] == Right(XmlString(xmlString)))
+    val entity = HttpEntity(`text/xml` withCharset `UTF-8`, bytes)
+    assert(entity.as[XmlString] == Right(xmlString))
   }
+
+  "Web service" in {
+    Post("/", xmlString) ~> testRoute(xmlString) ~> check {
+      assert(status == OK)
+    }
+  }
+}
+
+private object XmlStringTest {
+  private def testRoute(expected: XmlString): Route =
+    entity(as[XmlString]) { entity ⇒
+      assert(entity == expected)
+      complete(OK)
+    }
 }
