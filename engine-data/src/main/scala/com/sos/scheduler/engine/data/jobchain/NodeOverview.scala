e5990ea3579f0cfe7efc26e55c3b44638c28212e
Joacim Zschimmer <jogit@zschimmer.com>
2016-07-11 16:12:13 +0200

    JobChainOverview

--- engine-data/src/main/scala/com/sos/scheduler/engine/data/jobchain/NodeOverview.scala
+++ engine-data/src/main/scala/com/sos/scheduler/engine/data/jobchain/NodeOverview.scala
@@ -4,20 +4,41 @@ import com.sos.scheduler.engine.data.order.OrderState
 import spray.json._
 
 trait NodeOverview {
-  val orderState: OrderState
+  def orderState: OrderState
 }
 
 
 object NodeOverview {
+  private val TypeName = "$TYPE"
+  private val SimpleJobTypeName = JsString("SimpleJob")
+  private val SinkTypeName = JsString("Sink")
+  private val NestedJobChainTypeName = JsString("NestedJobChain")
+  private val EndTypeName = JsString("End")
+
   implicit object MyJsonFormat extends RootJsonFormat[NodeOverview]{
-    override def write(o: NodeOverview) =
+    def write(o: NodeOverview) =
       o match {
-        case o: SimpleJobNodeOverview ⇒ o.toJson
-        case o: SinkNodeOverview ⇒ o.toJson
-        case o: NestedJobChainNodeOverview ⇒ o.toJson
-        case o: EndNodeOverview ⇒ o.toJson
+        case o: SimpleJobNodeOverview      ⇒ typed(o.toJson, SimpleJobTypeName)
+        case o: SinkNodeOverview           ⇒ typed(o.toJson, SinkTypeName)
+        case o: NestedJobChainNodeOverview ⇒ typed(o.toJson, NestedJobChainTypeName)
+        case o: EndNodeOverview            ⇒ typed(o.toJson, EndTypeName)
       }
 
-    override def read(json: JsValue) = throw new UnsupportedOperationException
+    def read(json: JsValue) = {
+      val fields = json.asJsObject.fields
+      val untyped = JsObject(fields - TypeName)
+      fields(TypeName) match {
+        case SimpleJobTypeName      ⇒ untyped.convertTo[SimpleJobNodeOverview]
+        case SinkTypeName           ⇒ untyped.convertTo[SinkNodeOverview]
+        case NestedJobChainTypeName ⇒ untyped.convertTo[NestedJobChainNodeOverview]
+        case EndTypeName            ⇒ untyped.convertTo[EndNodeOverview]
+        case typ ⇒ throw new IllegalArgumentException(s"Unknown $$TYPE='$typ' for NodeOverview")
+      }
+    }
+  }
+
+  private def typed(jsValue: JsValue, typ: JsString): JsObject = {
+    val o = jsValue.asJsObject
+    o.copy(fields = o.fields + (TypeName → typ))
   }
 }
