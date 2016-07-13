d030b0b257c2973ba0f99415915c681815b50852
Joacim Zschimmer <jogit@zschimmer.com>
2016-07-13 12:59:10 +0200

    JS-1642 FolderTree, OrderOverview.Statistics

--- engine-data/src/main/scala/com/sos/scheduler/engine/data/order/OrderOverview.scala
+++ engine-data/src/main/scala/com/sos/scheduler/engine/data/order/OrderOverview.scala
@@ -4,6 +4,7 @@ import com.sos.scheduler.engine.base.sprayjson.JavaTimeJsonFormats.implicits._
 import com.sos.scheduler.engine.data.filebased.{FileBasedOverview, FileBasedState}
 import com.sos.scheduler.engine.data.job.TaskId
 import java.time.Instant
+import scala.collection.immutable
 import scala.language.implicitConversions
 import spray.json.DefaultJsonProtocol._
 
@@ -33,4 +34,11 @@ object OrderOverview {
   implicit val MyJsonFormat = jsonFormat9(apply)
 
   implicit val ordering: Ordering[OrderOverview] = Ordering by { o ⇒ (o.orderKey.jobChainPath, o.orderState, o.orderKey.id) }
+
+  final class Statistics(val orderOverviews: immutable.Seq[OrderOverview]) {
+    def count = orderOverviews.size
+    lazy val inProcessCount = orderOverviews count { _.taskId.isDefined }
+    lazy val suspendedCount = orderOverviews count { _.isSuspended }
+    lazy val blacklistedCount = orderOverviews count { _.isBlacklisted }
+  }
 }
