881fe298d699f65651d626e3d2b8c9924b19e208
Joacim Zschimmer <jogit@zschimmer.com>
2016-08-10 07:33:26 +0200

    JS-1659 TypedJsonFormat for subtypes

--- engine-base/src/main/scala/com/sos/scheduler/engine/base/sprayjson/TypedJsonFormat.scala
+++ engine-base/src/main/scala/com/sos/scheduler/engine/base/sprayjson/TypedJsonFormat.scala
@@ -2,36 +2,36 @@ package com.sos.scheduler.engine.base.sprayjson
 
 import com.sos.scheduler.engine.base.sprayjson.TypedJsonFormat._
 import com.sos.scheduler.engine.base.utils.ScalaUtils.implicitClass
-import com.sos.scheduler.engine.base.utils.ScalaUtils.implicits.toJavaFunction
-import java.util.concurrent.ConcurrentHashMap
+import scala.collection.immutable
+import scala.collection.immutable.Iterable
 import scala.reflect.ClassTag
 import spray.json._
 
 /**
  * @author Joacim Zschimmer
  */
-final class TypedJsonFormat[Super, Sub <: Super] private(
-  commonSuperClass: Class[Super],
-  classToLazyWriter: Map[Class[_ <: Sub], LazyWriteEntry],
-  typeToLazyReader: Map[String, () ⇒ RootJsonReader[_]],
+final class TypedJsonFormat[A] private(
+  private[TypedJsonFormat] val commonSuperClass: Class[A],
+  private[TypedJsonFormat] val classToWriteEntry: Map[Class[_ <: A], WriteEntry],
+  private[TypedJsonFormat] val typeToReader: Map[String, RootJsonReader[_]],
   typeFieldName: String,
   shortenTypeOnlyValue: Boolean)
-extends RootJsonFormat[Sub] {
+extends RootJsonFormat[A] {
 
-  private val classToWriter = new ConcurrentHashMap[Class[_ <: Sub], WriteEntry]
-  private val typeToReader = new ConcurrentHashMap[String, RootJsonReader[_]]
+  private val serializableClasses: Set[Class[_ <: A]] = classToWriteEntry.keySet
 
-  def write(o: Sub) = {
-    val w: WriteEntry = classToWriter.computeIfAbsent(o.getClass, { clazz: Class[_ <: Sub] ⇒
-      val e = classToLazyWriter.getOrElse(clazz,
-        throw new NoSuchElementException(s"${o.getClass} is not serializable as subclass of ${commonSuperClass.getName}"))
-      WriteEntry(e.getJsonWriter(), typeFieldName → JsString(e.typeName))
-    })
-    val serializedFields = w.jsonWriter.write(o).asJsObject.fields
+  def canSerialize(a: A) = canSerializeClass(a.getClass)
+
+  def canSerializeClass(c: Class[_ <: A]): Boolean = serializableClasses contains c
+
+  def write(o: A) = {
+    val WriteEntry(jsonWriter, typeField) = classToWriteEntry.getOrElse(o.getClass,
+      sys.error(s"${o.getClass} is not registered as subclass in TypedJsonFormat[${commonSuperClass.getName}]"))
+    val serializedFields = jsonWriter.write(o).asJsObject.fields
     if (shortenTypeOnlyValue && serializedFields.isEmpty)
-      w.typeField._2   // To be short, typename-only values are serialized as simple string "typename"
+      typeField._2   // To be short and more readable, typename-only values are serialized as simple string "typename" instead of { "TYPE": "typename" }
     else
-      JsObject(serializedFields + w.typeField)
+      JsObject(serializedFields + typeField)
   }
 
   def read(jsValue: JsValue) = {
@@ -40,12 +40,9 @@ extends RootJsonFormat[Sub] {
       case o: JsObject ⇒ (o.fields(typeFieldName).asInstanceOf[JsString].value, o.fields - typeFieldName)
       case o ⇒ throw new IllegalArgumentException(s"Expected JSON string or object for type ${commonSuperClass.getSimpleName}")
     }
-    val reader = typeToReader.computeIfAbsent(typeName, { typeName: String ⇒
-      val getReader = typeToLazyReader.getOrElse(typeName,
-        throw new NoSuchElementException(s"""JSON $typeFieldName="$typeName" denotes an unknown subtype of ${commonSuperClass.getSimpleName}"""))
-      getReader()
-    })
-    reader.read(JsObject(fields)).asInstanceOf[Sub]
+    val reader = typeToReader.getOrElse(typeName,
+      sys.error(s"""JSON $typeFieldName="$typeName" denotes an unknown subtype of ${commonSuperClass.getSimpleName}"""))
+    reader.read(JsObject(fields)).asInstanceOf[A]
   }
 }
 
@@ -53,50 +50,93 @@ object TypedJsonFormat {
   private val DefaultTypeFieldName = "TYPE"
   private val DefaultShortenTypeOnlyValue = true
 
-  // On compile error using TypedJsonFormat(), check if every Subtype really denotes a subtype of A.
-  def apply[A: ClassTag](subtypes: Subtype[_ <: A]*): TypedJsonFormat[A, A] =
-    _apply[A](subtypes = subtypes)
+/**
+  * A RootJsonType for polymorphic types.
+  * <p><b>On compile error using TypedJsonFormat(Subtype ...):</b> check if every Subtype really denotes a subtype of A.
+  *
+  */
+  def apply[A: ClassTag](subtype: Subtype[_ <: A], subtypes: Subtype[_ <: A]*): TypedJsonFormat[A] =
+    _apply[A](subtypes = subtype +: subtypes)
 
-  // On compile error using TypedJsonFormat(), check if every Subtype really denotes a subtype of A.
+  /**
+    * A RootJsonType for polymorphic types.
+    * <p><b>On compile error using TypedJsonFormat(Subtype ...):</b> check if every Subtype really denotes a subtype of A.
+    *
+    * @param typeField            Name of the subtype discriminating field
+    * @param shortenTypeOnlyValue To be short and more readable,
+    *                             typename-only values are serialized as simple string "typename"
+    *                             instead of { "TYPE": "typename" }
+    * @param subtypes
+    * @tparam A
+    * @return
+    */
   def apply[A: ClassTag](
     typeField: String = DefaultTypeFieldName,
     shortenTypeOnlyValue: Boolean = DefaultShortenTypeOnlyValue)
-    (subtypes: Subtype[_ <: A]*): TypedJsonFormat[A, A]
+    (subtypes: Subtype[_ <: A]*): TypedJsonFormat[A]
   =
     _apply[A](typeFieldName = typeField, shortenTypeOnlyValue = shortenTypeOnlyValue, subtypes = subtypes)
 
   private def _apply[A: ClassTag](
     typeFieldName: String = DefaultTypeFieldName,
     shortenTypeOnlyValue: Boolean = DefaultShortenTypeOnlyValue,
-    subtypes: Seq[Subtype[_ <: A]]): TypedJsonFormat[A, A]
+    subtypes: Seq[Subtype[_ <: A]]): TypedJsonFormat[A]
   =
-    new TypedJsonFormat[A, A](
+    new TypedJsonFormat[A](
       implicitClass[A],
-      (subtypes map { o ⇒ o.clazz → LazyWriteEntry(() ⇒ o.lazyJsonFormat().asInstanceOf[RootJsonWriter[Any]], o.typeName) }).toMap,
-      (subtypes map { o ⇒ o.typeName → (() ⇒ o.lazyJsonFormat()) }).toMap,
+      (subtypes flatMap { _.toClassToWriteEntry(typeFieldName) }).toMap,
+      (subtypes flatMap { _.toTypeToReader(typeFieldName) }).toMap,
       typeFieldName = typeFieldName,
       shortenTypeOnlyValue = shortenTypeOnlyValue)
 
-  /**
-    * @param lazyJsonFormat lazy to allow recursive tree structure with not yet initialized implicit commonSuperClass jsonFormat.
-    */
-  final case class Subtype[A](clazz: Class[A], lazyJsonFormat: () ⇒ RootJsonFormat[A], typeName: String)
+  final case class Subtype[A](clazz: Class[A], jsonFormat: RootJsonFormat[A], typeName: String) {
+
+    private[TypedJsonFormat] def toClassToWriteEntry(typeFieldName: String): immutable.Iterable[(Class[_ <: A], WriteEntry)] =
+      jsonFormat match {
+        case o: TypedJsonFormat[A @unchecked] ⇒
+          require(o.commonSuperClass isAssignableFrom clazz)
+          o.classToWriteEntry
+        case _ ⇒
+          Vector(clazz → WriteEntry(jsonFormat.asInstanceOf[RootJsonWriter[Any]], typeFieldName → JsString(typeName)))
+      }
+
+    private[TypedJsonFormat] def toTypeToReader(typeFieldName: String): Iterable[(String, RootJsonReader[_])] =
+      jsonFormat match {
+        case o: TypedJsonFormat[A @unchecked] ⇒
+          require(o.commonSuperClass isAssignableFrom clazz)
+          o.typeToReader
+        case _ ⇒
+          Vector(typeName → jsonFormat)
+      }
+  }
 
   object Subtype {
     def apply[A: ClassTag: RootJsonFormat]: Subtype[A] =
       Subtype[A](implicitly[RootJsonFormat[A]])
 
     def apply[A: ClassTag: RootJsonFormat](typeName: String): Subtype[A] =
-      new Subtype(implicitClass[A], () ⇒ implicitly[RootJsonFormat[A]], typeName)
+      new Subtype(implicitClass[A], implicitly[RootJsonFormat[A]], typeName)
 
     def apply[A: ClassTag](jsonFormat: ⇒ RootJsonFormat[A]): Subtype[A] =
-      new Subtype(implicitClass[A], () ⇒ jsonFormat, implicitClass[A].getSimpleName stripSuffix "$")
+      new Subtype(implicitClass[A], jsonFormat, implicitClass[A].getSimpleName stripSuffix "$")
 
     def apply[A: ClassTag](jsonFormat: ⇒ RootJsonFormat[A], typeName: String): Subtype[A] =
-      new Subtype(implicitClass[A], () ⇒ jsonFormat, typeName)
+      new Subtype(implicitClass[A], jsonFormat, typeName)
   }
 
-  final case class LazyWriteEntry(getJsonWriter: () ⇒ RootJsonWriter[Any], typeName: String)
+  /**
+    * For recursive structures.
+    */
+  def asLazy[A](lazyTypedJsonFormat: ⇒ TypedJsonFormat[A]) = new AsLazy[A] {
+    lazy val delegate = lazyTypedJsonFormat
+  }
 
-  final case class WriteEntry(jsonWriter: RootJsonWriter[Any], typeField: (String, JsString))
+  sealed trait AsLazy[A] extends RootJsonFormat[A] {
+    def delegate: TypedJsonFormat[A]
+    final def canSerialize(a: A) = delegate canSerialize a
+    final def write(x: A) = delegate.write(x)
+    final def read(value: JsValue) = delegate.read(value)
+  }
+
+  private[TypedJsonFormat] case class WriteEntry(jsonWriter: RootJsonWriter[Any], typeField: (String, JsString))
 }
--- engine-base/src/test/scala/com/sos/scheduler/engine/base/sprayjson/TypedJsonFormatTest.scala
+++ engine-base/src/test/scala/com/sos/scheduler/engine/base/sprayjson/TypedJsonFormatTest.scala
@@ -1,5 +1,6 @@
 package com.sos.scheduler.engine.base.sprayjson
 
+import com.sos.scheduler.engine.base.sprayjson.SprayJson.lazyRootFormat
 import com.sos.scheduler.engine.base.sprayjson.TypedJsonFormat._
 import com.sos.scheduler.engine.base.sprayjson.TypedJsonFormatTest._
 import org.junit.runner.RunWith
@@ -14,7 +15,7 @@ import spray.json._
 @RunWith(classOf[JUnitRunner])
 final class TypedJsonFormatTest extends FreeSpec {
 
-  private implicit val typedJsonFormat = TypedJsonFormat[A](
+  private implicit val aJsonFormat = TypedJsonFormat[A](
     Subtype(jsonFormat0(() ⇒ A0)),
     Subtype(jsonFormat1(A1)),
     //Subtype(jsonFormat0(Alien)),  // Must not not compile
@@ -40,12 +41,81 @@ final class TypedJsonFormatTest extends FreeSpec {
     assert(a.toJson == json)
     assert(a == json.convertTo[A])
   }
+
+  "Nested TypedJsonFormat" - {
+    implicit lazy val xJsonFormat: RootJsonFormat[X] =
+      lazyRootFormat(
+        TypedJsonFormat[X](
+          Subtype[A],
+          Subtype(jsonFormat2(B1))))
+
+    "case object" in {
+      val a: X = A0
+      val json = """"A0"""".parseJson
+      assert(a.toJson == json)
+      assert(a == json.convertTo[X])
+    }
+
+    "case class 1" in {
+      val a: X = A1("one")
+      val json = """{ "TYPE": "A1", "string": "one" } """.parseJson
+      assert(a.toJson == json)
+      assert(a == json.convertTo[X])
+    }
+
+    "case class 2" in {
+      val a: X = A2(2, "two")
+      val json = """{ "TYPE": "A2", "int": 2, "string": "two" }""".parseJson
+      assert(a.toJson == json)
+      assert(a == json.convertTo[X])
+    }
+    "case class 1b" in {
+      val b: X = B1("b one", Some(A0))
+      val json = """{ "TYPE": "B1", "b1String": "b one", "recursive": "A0" }""".parseJson
+      assert(b.toJson == json)
+      assert(b == json.convertTo[X])
+    }
+  }
+
+  "fallbackJsonWriter and recursive data structure" - {
+    implicit lazy val xJsonFormat: TypedJsonFormat.AsLazy[X] =
+      TypedJsonFormat.asLazy(
+        TypedJsonFormat[X](
+          typeField = "TYPETYPE")(
+            Subtype[A],
+            Subtype(jsonFormat2(B1))))
+
+    "case object" in {
+      val x: X = A0
+      val json = """"A0"""".parseJson
+      assert(x.toJson == json)
+      assert(x == json.convertTo[X])
+    }
+
+    "case class 1b" in {
+      val x: X = B1("b one", Some(A0))
+      val json = """{ "TYPETYPE": "B1", "b1String": "b one", "recursive": "A0" }""".parseJson
+      assert(x.toJson == json)
+      assert(x == json.convertTo[X])
+    }
+
+    "canSerialize" in {
+      assert(!xJsonFormat.canSerialize(B0))
+      assert(xJsonFormat.canSerialize(B1("", None)))
+    }
+  }
 }
 
 object TypedJsonFormatTest {
-  private sealed trait A
+  private trait X
+
+  private trait A extends X
   private case object A0 extends A
   private case class A1(string: String) extends A
   private case class A2(int: Int, string: String) extends A
   private case class Alien()
+
+  private trait B extends X
+  private case object B0 extends B
+  private case class B1(b1String: String, recursive: Option[X]) extends B
 }
