4f6b01ad84fc1642c42f6ed84c8df2e699ac0203
Joacim Zschimmer <jogit@zschimmer.com>
2016-10-06 14:04:41 +0200

    Renamed internal configuration resources master.conf and agent.conf

--- engine-taskserver/src/main/scala/com/sos/scheduler/engine/taskserver/configuration/inject/TaskServerMainModule.scala
+++ engine-taskserver/src/main/scala/com/sos/scheduler/engine/taskserver/configuration/inject/TaskServerMainModule.scala
@@ -60,5 +60,5 @@ final class TaskServerMainModule(dotnet: DotnetConfiguration) extends AbstractMo
 }
 
 object TaskServerMainModule {
-  private val ConfigurationResource = JavaResource("com/sos/scheduler/engine/taskserver/configuration/TaskServerMain-defaults.conf")
+  private val ConfigurationResource = JavaResource("com/sos/scheduler/engine/taskserver/configuration/TaskServerMain.conf")
 }
