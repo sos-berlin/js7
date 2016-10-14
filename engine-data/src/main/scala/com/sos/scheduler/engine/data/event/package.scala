a45689caec4b8ca1cebb2a0d6ed4627e1b18aaa9
Joacim Zschimmer <jogit@zschimmer.com>
2016-10-14 07:29:42 +0200

    JS-1659 EventCollector refactored

--- engine-data/src/main/scala/com/sos/scheduler/engine/data/event/package.scala
+++ engine-data/src/main/scala/com/sos/scheduler/engine/data/event/package.scala
@@ -13,6 +13,9 @@ package object event {
   object EventId {
     val BeforeFirst: EventId = 0
     val IdsPerMillisecond = 1000
+    // JavaScript uses floating point for all numbers, so it have 11 bits for a precise integer to
+    // represent all integers between 0 and 2**53 (9.007.199.254.740.992).
+    // 2 ** 53 = 9.007.199.254.740.992µs = 285 years. This is good until year 2255, for a million events/s.
     private[event] val JsonMaxValue = EventId(0x20000000000000L)  // =2^53 == 9007199254740992L
 
     def apply(eventId: String) = eventId.toLong
