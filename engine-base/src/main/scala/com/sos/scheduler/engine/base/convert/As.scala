6f88c77877cc49d037a1c10bfe760de8ef2648bd
Joacim Zschimmer <jogit@zschimmer.com>
2016-10-18 12:24:41 +0200

    Class As refactored

--- engine-base/src/main/scala/com/sos/scheduler/engine/base/convert/As.scala
+++ engine-base/src/main/scala/com/sos/scheduler/engine/base/convert/As.scala
@@ -9,28 +9,42 @@ import java.nio.file.{Path, Paths}
 trait As[V, W] extends (V ⇒ W)
 
 object As {
-  def apply[V, W](asW: V ⇒ W): As[V, W] = new As[V, W] { def apply(v: V) = asW(v) }
 
-  def convert[V, W](from: V)(implicit to: As[V, W]): W = to(from)
+  def apply[V, W](asW: V ⇒ W): As[V, W] =
+    new As[V, W] {
+      def apply(v: V) = asW(v)
+    }
+
+  def convert[V, W](from: V)(implicit to: As[V, W]): W =
+    to(from)
 
   implicit val StringAsString: As[String, String] = As(identity)
 
   implicit object StringAsBoolean extends As[String, Boolean] {
     val StringToBooleanMap = Map(
-      "true" → true, "on" → true, "yes" → true,
-      "false" → false, "off" → false, "no" → false)
+      "true"  → true , "on"  → true , "yes" → true,
+      "false" → false, "off" → false, "no"  → false)
 
     def apply(o: String) = StringToBooleanMap.getOrElse(o,
       throw new IllegalArgumentException(s"Boolean value true or false expected, not: $o"))
   }
 
-  implicit val StringAsInt: As[String, Int] = As(Integer.parseInt)
+  //
+  // Some default implementations
+  //
 
-  implicit val StringAsLong: As[String, Long] = As(java.lang.Long.parseLong)
+  implicit val StringAsInt: As[String, Int] =
+    As(Integer.parseInt)
 
-  def asAbsolutePath: As[String, Path] = As(o ⇒ StringAsPath(o).toAbsolutePath)
+  implicit val StringAsLong: As[String, Long] =
+    As(java.lang.Long.parseLong)
 
-  implicit val StringAsPath: As[String, Path] = As(Paths.get(_))
+  def asAbsolutePath: As[String, Path] =
+    As(o ⇒ StringAsPath(o).toAbsolutePath)
 
-  implicit val StringAsSecretString: As[String, SecretString] = As(SecretString.apply)
+  implicit val StringAsPath: As[String, Path] =
+    As(Paths.get(_))
+
+  implicit val StringAsSecretString: As[String, SecretString] =
+    As(SecretString.apply)
 }
