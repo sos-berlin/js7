96fa0d744fa6ebb332189c017e8e7217670c3dfc
Joacim Zschimmer <jogit@zschimmer.com>
2016-12-06 16:03:14 +0100

    As[String, GenericInt]

--- engine-base/src/main/scala/com/sos/scheduler/engine/base/generic/GenericInt.scala
+++ engine-base/src/main/scala/com/sos/scheduler/engine/base/generic/GenericInt.scala
@@ -1,5 +1,6 @@
 package com.sos.scheduler.engine.base.generic
 
+import com.sos.scheduler.engine.base.convert.As
 import spray.json._
 
 /**
@@ -14,6 +15,9 @@ object GenericInt {
     def apply(number: Int): A
 
     implicit val ordering: Ordering[A] = Ordering by { _.number }
+
+    implicit def stringAsGenericInt: As[String, A] =
+      As(o ⇒ apply(o.toInt))
 
     implicit object genericIntJsonFormat extends JsonFormat[A] {
       def write(o: A) = JsNumber(o.number)   // JavaScript uses only 53 bits due to conversion to Double !!!
