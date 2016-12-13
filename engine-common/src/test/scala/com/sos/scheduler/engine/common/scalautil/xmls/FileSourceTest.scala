b64ec7c4a9fd372fcd610e14bdfe3e9b8d87f296
Joacim Zschimmer <jogit@zschimmer.com>
2016-12-13 13:17:59 +0100

    XML FileSource

---
+++ engine-common/src/test/scala/com/sos/scheduler/engine/common/scalautil/xmls/FileSourceTest.scala
@@ -0,0 +1,24 @@
+package com.sos.scheduler.engine.common.scalautil.xmls
+
+import com.sos.scheduler.engine.common.scalautil.FileUtils.implicits.RichPath
+import java.nio.charset.StandardCharsets.UTF_8
+import java.nio.file.Files.{createTempFile, delete}
+import org.junit.runner.RunWith
+import org.scalatest.FreeSpec
+import org.scalatest.junit.JUnitRunner
+
+/**
+  * @author Joacim Zschimmer
+  */
+@RunWith(classOf[JUnitRunner])
+final class FileSourceTest extends FreeSpec {
+
+  "FileSource" in {
+    val file = createTempFile("test-", ".tmp")
+    file.contentString = "<test/>"
+    val source = new FileSource(file)
+    assert(io.Source.fromInputStream(source.getInputStream)(UTF_8).getLines().mkString == "<test/>")
+    source.close()
+    delete(file)  // Under Windows, file must be closed now
+  }
+}
