ef7504729621ee0c3416e0c0c4c7e8f31fda6879
Joacim Zschimmer <jogit@zschimmer.com>
2016-12-28 13:38:33 +0100

    GuiceImplicits

--- engine-common/src/main/scala/com/sos/scheduler/engine/common/guice/GuiceImplicits.scala
+++ engine-common/src/main/scala/com/sos/scheduler/engine/common/guice/GuiceImplicits.scala
@@ -1,11 +1,13 @@
 package com.sos.scheduler.engine.common.guice
 
 import com.google.inject.Injector
+import com.sos.scheduler.engine.base.utils.ScalaUtils.implicitClass
 import scala.reflect.ClassTag
 
 object GuiceImplicits {
   implicit class RichInjector(val delegate: Injector) extends AnyVal {
 
-    def instance[A](implicit c: ClassTag[A]) = delegate.getInstance(c.runtimeClass).asInstanceOf[A]
+    def instance[A: ClassTag]: A =
+      delegate.getInstance(implicitClass[A])
   }
 }
