2105d72d000481f7663cde1cc8eb8fe45f871233
Joacim Zschimmer <jogit@zschimmer.com>
2017-01-05 09:56:19 +0100

    SessionRegister

---
+++ engine-data/src/main/scala/com/sos/scheduler/engine/data/session/SessionToken.scala
@@ -0,0 +1,15 @@
+package com.sos.scheduler.engine.data.session
+
+import com.sos.scheduler.engine.base.generic.SecretString
+import spray.json.DefaultJsonProtocol._
+
+/**
+  * @author Joacim Zschimmer
+  */
+final case class SessionToken(secret: SecretString)
+
+object SessionToken {
+  private implicit val x = SecretString.implicits.jsonFormat
+  implicit val jsonFormat = jsonFormat1(apply)
+  val HeaderName = "X-JobScheduler-Session"
+}
