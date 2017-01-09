6680e5a1a6e23a3f78559159048da03ea8bfe682
Joacim Zschimmer <jogit@zschimmer.com>
2017-01-09 15:25:25 +0100

    DeadLetterActor logs UnhandledMessage, too

--- engine-common/src/main/scala/com/sos/scheduler/engine/common/akkautils/DeadLetterActor.scala
+++ engine-common/src/main/scala/com/sos/scheduler/engine/common/akkautils/DeadLetterActor.scala
@@ -1,6 +1,6 @@
 package com.sos.scheduler.engine.common.akkautils
 
-import akka.actor.{Actor, ActorSystem, DeadLetter, Props}
+import akka.actor.{Actor, ActorSystem, DeadLetter, Props, UnhandledMessage}
 import com.sos.scheduler.engine.common.scalautil.Logger
 
 /**
@@ -8,15 +8,22 @@ import com.sos.scheduler.engine.common.scalautil.Logger
   */
 private class DeadLetterActor(output: (⇒ String) ⇒ Unit) extends Actor {
   def receive = {
-    case o: DeadLetter ⇒ output(s"DeadLetter ${o.message.getClass.getName} from ${o.sender} to ${o.recipient}")
+    case o: DeadLetter ⇒
+      output(s"DeadLetter ${o.message.getClass.getName} from ${o.sender} to ${o.recipient}")
+
+    case o: UnhandledMessage ⇒
+      output(s"UnhandledMessage ${o.message.getClass.getName} from ${o.sender} to ${o.recipient}")
   }
 }
 
 object DeadLetterActor {
   private val logger = Logger(getClass)
 
-  def subscribe(actorSystem: ActorSystem, output: (⇒ String) ⇒ Unit = logDeadLetter): Unit =
-    actorSystem.eventStream.subscribe(actorSystem.actorOf(props(output), "DeadLetter"), classOf[DeadLetter])
+  def subscribe(actorSystem: ActorSystem, output: (⇒ String) ⇒ Unit = logDeadLetter): Unit = {
+    val actor = actorSystem.actorOf(props(output), "DeadLetter")
+    actorSystem.eventStream.subscribe(actor, classOf[DeadLetter])
+    actorSystem.eventStream.subscribe(actor, classOf[UnhandledMessage])
+  }
 
   private def logDeadLetter(message: ⇒ String): Unit = logger.debug(message)
 
