e891ead47729b5bc53efef0b06243d07da2bf0af
Joacim Zschimmer <jogit@zschimmer.com>
2017-01-09 15:25:44 +0100

    Agent logs DeadLetter and UnhandledMessage

--- engine-agent/src/main/scala/com/sos/scheduler/engine/agent/configuration/Akkas.scala
+++ engine-agent/src/main/scala/com/sos/scheduler/engine/agent/configuration/Akkas.scala
@@ -2,6 +2,7 @@ package com.sos.scheduler.engine.agent.configuration
 
 import akka.actor.ActorSystem
 import com.google.common.io.Closer
+import com.sos.scheduler.engine.common.akkautils.DeadLetterActor
 import com.sos.scheduler.engine.common.scalautil.Closers.implicits._
 import com.sos.scheduler.engine.common.scalautil.SideEffect._
 import com.typesafe.config.{Config, ConfigFactory}
@@ -15,6 +16,7 @@ object Akkas {
 
   def newActorSystem(name: String, config: Config = ConfigFactory.empty)(implicit closer: Closer): ActorSystem =
     ActorSystem(name, config withFallback AgentConfiguration.DefaultsConfig) sideEffect { o ⇒
+      DeadLetterActor.subscribe(o)
       closer.onClose {
         o.shutdown()
         o.awaitTermination(ShutdownDuration)
