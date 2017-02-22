cd1b122cbe4600bbcaf546252876659711cabbbf
Joacim Zschimmer <jogit@zschimmer.com>
2017-02-22 12:50:40 +0100

    OrderId.toString with prefix "Order:", like other TypedPath

--- sbt/data/src/main/scala/com/sos/scheduler/engine/data/order/OrderId.scala
+++ sbt/data/src/main/scala/com/sos/scheduler/engine/data/order/OrderId.scala
@@ -2,6 +2,8 @@ package com.sos.scheduler.engine.data.order
 
 import com.sos.scheduler.engine.base.generic.IsString
 
-final case class OrderId(string: String) extends IsString
+final case class OrderId(string: String) extends IsString {
+  override def toString = s"Order:$string"
+}
 
 object OrderId extends IsString.Companion[OrderId]
