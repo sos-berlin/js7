d478d48044a8ebf6957faf97328fad6e7c5237de
Joacim Zschimmer <jogit@zschimmer.com>
2017-02-22 17:59:36 +0100

    sbt

--- sbt/project/BuildUtils.scala
+++ sbt/project/BuildUtils.scala
@@ -1,8 +1,27 @@
+import java.nio.file.{FileVisitOption, Files, Path}
 import sbt.File
+import scala.collection.JavaConversions._
+import scala.collection.immutable.Seq
 
 object BuildUtils {
   lazy val isWindows = sys.props("os.name") startsWith "Windows"
 
+  def recursiveFileMapping(directory: File, to: String = ""): Seq[(File, String)] = {
+    val to_ = (to stripSuffix "/") + "/"
+    for (file ← listFilesRecursively(directory)) yield
+      file → (to_ + (file.toString stripPrefix directory.toString))
+  }
+
   def listFiles(directory: File): Vector[File] =
     (Option(directory.listFiles) getOrElse sys.error(s"Missing directory $directory")).toVector
+
+  def listFilesRecursively(directory: File, options: FileVisitOption*): Vector[File] = {
+    val javaStream = Files.walk(directory.toPath, options: _*)
+    try {
+      val builder = Vector.newBuilder[File]
+      for (file ← javaStream.iterator) builder += file.toFile
+      builder.result.sortBy { _.toString }
+    }
+    finally javaStream.close()
+  }
 }
