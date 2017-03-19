f1baa665e1610b53a93c3aeb236661c5458abb86
Joacim Zschimmer <jogit@zschimmer.com>
2017-03-19 14:30:36 +0100

    Better TypedJsonFormat error message

--- base/src/main/scala/com/sos/jobscheduler/base/sprayjson/typed/WithSubtypeRegister.scala
+++ base/src/main/scala/com/sos/jobscheduler/base/sprayjson/typed/WithSubtypeRegister.scala
@@ -53,8 +53,7 @@ extends TypedJsonFormat[A] {
     typeNameToJsonReader contains o.fields(typeFieldName).asString
 
   def write(a: A) = {
-    val jsonWriter = classToJsonWriter.getOrElse(a.getClass,
-        sys.error(s"${a.getClass} is not registered as subclass in TypedJsonFormat[${superclass.getName}]"))
+    val jsonWriter = classToJsonWriter.getOrElse(a.getClass, throwUnknownClass(a.getClass.toString))
       .asInstanceOf[JsonWriter[A]]
     val result = jsonWriter.write(a).asJsObject
     if (shortenTypeOnlyValue && result.fields.keySet == Set(typeFieldName))
@@ -67,10 +66,18 @@ extends TypedJsonFormat[A] {
     val (typeName, fields: Map[String, JsValue]) = jsValue match {
       case o: JsString ⇒ (o.value, Map.empty)
       case o: JsObject ⇒ (o.fields(typeFieldName).asInstanceOf[JsString].value, o.fields /*- typeFieldName*/)
-      case o ⇒ sys.error(s"Expected JSON string or object for type ${superclass.getSimpleName}")
+      case _ ⇒ sys.error(s"Expected JSON string or object for $toString")
     }
-    val reader = typeNameToJsonReader.getOrElse(typeName,
-      sys.error(s"""JSON $typeFieldName="$typeName" does not denote a subtype of ${superclass.getSimpleName}"""))
+    val reader = typeNameToJsonReader.getOrElse(typeName, throwUnknownClass(s"""JSON "$typeFieldName": "$typeName""""))
     reader.read(JsObject(fields)).asInstanceOf[A]
+  }
+
+  private def throwUnknownClass(name: String) =
+    throw new RuntimeException(s"$name is not registered as subclass in $toString")
+
+  override def toString = {
+    var s = subtypeNames take 3 mkString ", "
+    if (subtypeNames.size > 3) s = s"$s, ..."
+    s"TypedJsonFormat[${superclass.getName}]($s)"
   }
 }
