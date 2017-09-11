d39fb3158f97408893d565b5296114ec0db7fc0f
Joacim Zschimmer <jogit@zschimmer.com>
2017-09-11 13:30:08 +0200

    Java 9

--- common/src/main/scala/com/sos/jobscheduler/common/process/ProcessesJava8pid.scala
+++ common/src/main/scala/com/sos/jobscheduler/common/process/ProcessesJava8pid.scala
@@ -12,43 +12,50 @@ import javax.lang.model.SourceVersion
  * @author Joacim Zschimmer
  */
 private[process] object ProcessesJava8pid {
-  private[process] val processToPid = SourceVersion.latest.toString match {
-    case "RELEASE_9" ⇒ Jdk9ProcessToPid
-    case _ ⇒ if (isWindows) WindowsBeforeJava9ProcessToPid else UnixBeforeJava9ProcessToPid
+
+  private val hasJava9 = SourceVersion.values map { _.toString } contains "RELEASE_9"
+
+  private[process] val processToPid = {
+    if (hasJava9)
+      Java9ProcessToPid
+    else if (isWindows)
+      WindowsBeforeJava9ProcessToPid
+    else
+      UnixBeforeJava9ProcessToPid
   }
 
-  private object Jdk9ProcessToPid extends (Process ⇒ Option[Pid]) {
+  private object Java9ProcessToPid extends (Process ⇒ Option[Pid]) {
     private lazy val getPid = classOf[Process].getMethod("getPid")   // Needs Java 9
 
     def apply(process: Process) =
       try Some(Pid(getPid.invoke(process).asInstanceOf[java.lang.Long]))
-      catch {
-        case t: Throwable ⇒ None
+      catch { case _: Throwable ⇒
+        None
       }
   }
 
   private object UnixBeforeJava9ProcessToPid extends (Process ⇒ Option[Pid]) {
-    def apply(process: Process) = try {
-      val pidField = process.getClass.getDeclaredField("pid")
-      pidField.setAccessible(true)
-      Some(Pid(pidField.get(process).asInstanceOf[java.lang.Integer].longValue))
-    }
-    catch {
-      case t: Throwable ⇒ None
-    }
+    def apply(process: Process) =
+      try {
+        val pidField = process.getClass.getDeclaredField("pid")
+        pidField.setAccessible(true)
+        Some(Pid(pidField.get(process).asInstanceOf[java.lang.Integer].longValue))
+      } catch { case _: Throwable ⇒
+        None
+      }
   }
 
   private object WindowsBeforeJava9ProcessToPid extends (Process ⇒ Option[Pid]) {
-    def apply(process: Process) = try {
-      None // May be implemented with JNA https://github.com/java-native-access/jna !!!
-      //val handleField = process.getClass.getDeclaredField("handle")
-      //handleField.setAccessible(true)
-      //val handle = new WinNT.HANDLE
-      //handle.setPointer(Pointer.createConstant(handleField.getLong(process)))
-      //Kernel32.INSTANCE.GetProcessId(handle)
+    def apply(process: Process) =
+      try {
+        None // May be implemented with JNA https://github.com/java-native-access/jna !!!
+        //val handleField = process.getClass.getDeclaredField("handle")
+        //handleField.setAccessible(true)
+        //val handle = new WinNT.HANDLE
+        //handle.setPointer(Pointer.createConstant(handleField.getLong(process)))
+        //Kernel32.INSTANCE.GetProcessId(handle)
+      } catch { case _: Throwable ⇒
+        None
+      }
     }
-    catch {
-      case t: Throwable ⇒ None
-    }
-  }
 }
