5ecb78a62907b678db607334b0afc0175360229f
Joacim Zschimmer <jogit@zschimmer.com>
2017-09-24 12:34:36 +0200

    log4j

--- common/src/main/scala/com/sos/jobscheduler/common/log/Log4j.scala
+++ common/src/main/scala/com/sos/jobscheduler/common/log/Log4j.scala
@@ -1,16 +1,21 @@
 package com.sos.jobscheduler.common.log
 
+import com.sos.jobscheduler.common.scalautil.Logger
+
 /**
   * @author Joacim Zschimmer
   */
 object Log4j {
 
+  private val logger = Logger(getClass)
+
   /**
     * Call in case the shutdown hook is disabled in log4j2.xml: &gt;configuration shutdownHook="disable">.
     */
   def shutdown(): Unit = {
-    for (logManager ← Option(Class.forName("org.apache.logging.log4j.LogManager"))) {
-      logManager.getMethod("shutdown").invoke(null)
+    for (logManager ← Option(Class forName "org.apache.logging.log4j.LogManager")) {
+      logger.debug("log4j.LogManager.shutdown")
+      logManager.getMethod("shutdown", classOf[Boolean]).invoke(null, Boolean.box(true))
     }
   }
 }
