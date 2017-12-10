2c19c39cf7b3067f4ee1ad111232932c20009201
Joacim Zschimmer <jogit@zschimmer.com>
2017-12-10 07:50:34 +0100

    Webservice /master/api: response is not cachable

--- master/src/main/scala/com/sos/jobscheduler/master/web/api/ApiRoute.scala
+++ master/src/main/scala/com/sos/jobscheduler/master/web/api/ApiRoute.scala
@@ -1,6 +1,7 @@
 package com.sos.jobscheduler.master.web.api
 
-import akka.http.scaladsl.model.headers.RawHeader
+import akka.http.scaladsl.model.headers.CacheDirectives.{`max-age`, `no-cache`, `no-store`}
+import akka.http.scaladsl.model.headers.{RawHeader, `Cache-Control`}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import com.sos.jobscheduler.common.BuildInfo
@@ -15,11 +16,13 @@ trait ApiRoute extends RootRoute with OrderRoute with FrontEndRoute with GuiRout
 
   val apiRoute: Route =
     respondWithHeader(RawHeader("X-JobScheduler-Build-ID", BuildInfo.buildId)) {
-      pathEnd {
-        rootRoute
-      } ~
-      pathSegments("order") {
-        orderRoute
+      respondWithHeader(`Cache-Control`(`max-age`(0), `no-store`, `no-cache`)) {
+        pathEnd {
+          rootRoute
+        } ~
+        pathSegments("order") {
+          orderRoute
+        }
       } ~
       pathSegments("frontend") {
         frontEndRoute
