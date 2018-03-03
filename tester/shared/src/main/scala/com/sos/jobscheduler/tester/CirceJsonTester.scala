9229ebd5f86db006260b89f28ad2c8bc2f8cff4d
Joacim Zschimmer <jogit@zschimmer.com>
2018-03-03 12:51:55 +0100

    Refactored

--- tester/shared/src/main/scala/com/sos/jobscheduler/tester/CirceJsonTester.scala
+++ tester/shared/src/main/scala/com/sos/jobscheduler/tester/CirceJsonTester.scala
@@ -19,15 +19,15 @@ object CirceJsonTester {
     // Do a.asJson first to get the JSON string, then evaluate lazy json (which may have syntax errors during development).
     val asJson: Json = removeJNull(a.asJson)  // Circe converts None to JNull which we remove here (like Printer dropNullValues = true)
     if (asJson != json) fail(s"${prettyPrinter.pretty(normalize(asJson))} did not equal ${prettyPrinter.pretty(normalize(json))}")
-    assert(forceLeft(json.as[A]) == a)
+    assert(forceRight(json.as[A]) == a)
     assert(json == parseJson(printer.pretty(asJson)))
     assert(parseJson(printer.pretty(asJson)) == asJson)
   }
 
   private def parseJson(string: String): Json =
-    forceLeft(parse(string))
+    forceRight(parse(string))
 
-  private def forceLeft[R](either: Either[Throwable, R]): R =
+  private def forceRight[R](either: Either[Throwable, R]): R =
     either match {
       case Right(o) ⇒ o
       case Left(t) ⇒ throw new RuntimeException(t.toString, t)   // Add stacktrace
