e9fb9dfc84434be874529da2e63351f881d55750
Joacim Zschimmer <jogit@zschimmer.com>
2018-03-17 10:14:26 +0100

    MonixForCats

---
+++ base/shared/src/test/scala/com/sos/jobscheduler/base/monix/MonixForCatsLawTest.scala
@@ -0,0 +1,23 @@
+//package com.sos.jobscheduler.base.monix
+//
+//import cats.laws.discipline.ApplicativeTests
+//import cats.laws.discipline.SemigroupalTests.Isomorphisms
+//import com.sos.jobscheduler.base.monix.MonixForCats._
+//import monix.eval.Task
+//import org.scalacheck.Arbitrary
+//import org.scalacheck.Arbitrary.arbitrary
+//import org.scalatest.FunSuite
+//import org.typelevel.discipline.scalatest.Discipline
+//
+///**
+//  * @author Joacim Zschimmer
+//  */
+//class MonixForCatsLawTest extends FunSuite with Discipline {
+//
+//  private implicit def arbitraryTask[A: Arbitrary]: Arbitrary[Task[A]] =
+//    Arbitrary(arbitrary[A] map Task.pure)
+//
+//  private implicit val isomorphisms: Isomorphisms[Task] = Isomorphisms.invariant(taskApplicative)
+//
+//  checkAll("Task[Int]", ApplicativeTests[Task].applicative[Int, Int, Int])
+//}
