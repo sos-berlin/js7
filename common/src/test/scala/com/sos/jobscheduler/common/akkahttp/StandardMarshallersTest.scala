798304554fc5205d2bd951988233902c64f8ddce
Joacim Zschimmer <jogit@zschimmer.com>
2018-03-30 08:54:32 +0200

    Test timing

--- common/src/test/scala/com/sos/jobscheduler/common/akkahttp/StandardMarshallersTest.scala
+++ common/src/test/scala/com/sos/jobscheduler/common/akkahttp/StandardMarshallersTest.scala
@@ -15,8 +15,9 @@ import com.sos.jobscheduler.common.akkahttp.StandardMarshallers._
 import com.sos.jobscheduler.common.akkautils.Akkas.newActorSystem
 import com.sos.jobscheduler.common.http.AkkaHttpUtils._
 import com.sos.jobscheduler.common.http.CirceJsonSupport._
+import com.sos.jobscheduler.common.scalautil.Futures.implicits._
+import com.sos.jobscheduler.common.time.ScalaTime._
 import io.circe.generic.semiauto.deriveEncoder
-import org.scalatest.concurrent.ScalaFutures._
 import org.scalatest.{BeforeAndAfterAll, FreeSpec}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
@@ -35,24 +36,24 @@ final class StandardMarshallersTest extends FreeSpec with BeforeAndAfterAll {
   }
 
   "ToEntityMarshaller[Problem], default is text/plain" in {
-    val response = Marshal(Problem("PROBLEM")).toResponseFor(HttpRequest()).futureValue
+    val response = Marshal(Problem("PROBLEM")).toResponseFor(HttpRequest()) await 99.s
     assert(response.status == BadRequest)
     assert(response.entity.contentType == `text/plain(UTF-8)`)
-    assert(response.entity.toStrict(9.seconds).futureValue.data == ByteString("PROBLEM\n"))
+    assert(response.entity.toStrict(9.seconds).await(99.s).data == ByteString("PROBLEM\n"))
   }
 
   "ToEntityMarshaller[Problem], application/json" in {
-    val response = Marshal(Problem("PROBLEM")).toResponseFor(HttpRequest(headers = List(Accept(`application/json`)))).futureValue
+    val response = Marshal(Problem("PROBLEM")).toResponseFor(HttpRequest(headers = List(Accept(`application/json`)))) await 99.s
     assert(response.status == BadRequest)
     assert(response.entity.contentType == ContentTypes.`application/json`)
-    assert(response.entity.toStrict(9.seconds).futureValue.data.utf8String.parseJson ==
+    assert(response.entity.toStrict(9.seconds).await(99.s).data.utf8String.parseJson ==
       json"""{ "TYPE": "Problem", "message": "PROBLEM" }""")
   }
 
   "problemToEntityMarshaller" in {
-    val entity = Marshal(Problem("PROBLEM")).to[MessageEntity].futureValue
+    val entity = Marshal(Problem("PROBLEM")).to[MessageEntity] await 99.s
     assert(entity.contentType == `text/plain(UTF-8)`)
-    assert(entity.utf8StringFuture.futureValue == "PROBLEM\n")
+    assert(entity.utf8StringFuture.await(99.s) == "PROBLEM\n")
   }
 
   "checkedToResponseMarshaller" - {
@@ -60,17 +61,17 @@ final class StandardMarshallersTest extends FreeSpec with BeforeAndAfterAll {
     implicit val encoder = deriveEncoder[A]
 
     "Valid" in {
-      val response = Marshal(Valid(A(7)): Checked[A]).to[HttpResponse].futureValue
+      val response = Marshal(Valid(A(7)): Checked[A]).to[HttpResponse] await 99.s
       assert(response.status == OK)
       assert(response.entity.contentType == ContentTypes.`application/json`)
-      assert(response.entity.toStrict(9.seconds).futureValue.data.utf8String.parseJson == json""" { "number": 7 } """)
+      assert(response.entity.toStrict(9.seconds).await(99.s).data.utf8String.parseJson == json""" { "number": 7 } """)
     }
 
     "Invalid" in {
-      val response = Marshal(Invalid(Problem("PROBLEM")): Checked[A]).to[HttpResponse].futureValue
+      val response = Marshal(Invalid(Problem("PROBLEM")): Checked[A]).to[HttpResponse] await 99.s
       assert(response.status == BadRequest)
       assert(response.entity.contentType == `text/plain(UTF-8)`)
-      assert(response.entity.toStrict(9.seconds).futureValue.data == ByteString("PROBLEM\n"))
+      assert(response.entity.toStrict(9.seconds).await(99.s).data == ByteString("PROBLEM\n"))
     }
   }
 }
