c62312b993cb6c42c0ceeb00b26f1dfd48f571e8
Joacim Zschimmer <jogit@zschimmer.com>
2018-05-11 08:08:29 +0200

    Clean-up

--- master-gui-browser/src/main/scala/com/sos/jobscheduler/master/gui/browser/services/MasterApi.scala
+++ master-gui-browser/src/main/scala/com/sos/jobscheduler/master/gui/browser/services/MasterApi.scala
@@ -3,7 +3,6 @@ package com.sos.jobscheduler.master.gui.browser.services
 import com.sos.jobscheduler.common.http.JsHttpClient
 import com.sos.jobscheduler.master.client.HttpMasterApi
 import com.sos.jobscheduler.master.gui.browser.services.JsBridge.jobschedulerBuildId
-import scala.concurrent.ExecutionContext
 
 /**
   * @author Joacim Zschimmer
@@ -11,5 +10,4 @@ import scala.concurrent.ExecutionContext
 object MasterApi extends HttpMasterApi {
   protected val uri = "/"
   protected val httpClient = new JsHttpClient(jobschedulerBuildId.ensuring(_.nonEmpty, "GUI error: Variable jobschedulerBuildId is empty"))
-  protected def executionContext = ExecutionContext.global
 }
