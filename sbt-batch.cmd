072886e79c8d52dd3b755d478afc28ad7ac19a95
Joacim Zschimmer <jogit@zschimmer.com>
2018-05-18 20:25:11 +0200

    sbt 1.1.5

--- sbt-batch.cmd
+++ sbt-batch.cmd
@@ -5,6 +5,6 @@ if "%WINDOWS_NET_SDK_HOME%" == "" (
   set WINDOWS_NET_SDK_HOME=%windir%\Microsoft.NET\Framework\v4.0.30319
 )
 
-set JAVA_OPTS=-Xms500M -Xmx3G -XX:MetaspaceSize=256M -XX:MaxMetaspaceSize=1G -XX:+CMSClassUnloadingEnabled -Dlog4j.configurationFile=project/log4j2.xml -Dsbt.log.noformat=true
+set JAVA_OPTS=-Xms500M -Xmx2500M -XX:MetaspaceSize=256M -XX:MaxMetaspaceSize=1500M -XX:+CMSClassUnloadingEnabled -Dlog4j.configurationFile=project/log4j2.xml -Dsbt.log.noformat=true
 sbt %*
 endlocal
