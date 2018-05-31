dd55aeb529da0836536444adc49c74d8b5fe8420
Joacim Zschimmer <jogit@zschimmer.com>
2018-05-31 18:59:03 +0200

    sbt: more heap for sbt-batch than default in .jvmopts

--- sbt-batch.cmd
+++ sbt-batch.cmd
@@ -5,6 +5,7 @@ if "%WINDOWS_NET_SDK_HOME%" == "" (
   set WINDOWS_NET_SDK_HOME=%windir%\Microsoft.NET\Framework\v4.0.30319
 )
 
-set JAVA_OPTS=-Xms500M -Xmx2500M -XX:MetaspaceSize=256M -XX:MaxMetaspaceSize=1500M -XX:+CMSClassUnloadingEnabled -Dlog4j.configurationFile=project/log4j2.xml -Dsbt.log.noformat=true
-sbt %*
+rem set SBT_OPTS=
+set JAVA_OPTS=-Xmx2500M -XX:MaxMetaspaceSize=1500M -XX:+CMSClassUnloadingEnabled -Dlog4j.configurationFile=project/log4j2.xml -Dsbt.log.noformat=true %JAVA_OPTS%
+sbt.bat %*
 endlocal
