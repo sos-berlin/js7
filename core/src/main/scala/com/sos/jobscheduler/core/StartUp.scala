86651ce57732dde257d640cc7ef7a29da30904c6
Joacim Zschimmer <jogit@zschimmer.com>
2018-07-03 17:28:21 +0200

    Log class path only once

--- core/src/main/scala/com/sos/jobscheduler/core/StartUp.scala
+++ core/src/main/scala/com/sos/jobscheduler/core/StartUp.scala
@@ -6,26 +6,30 @@ import com.sos.jobscheduler.common.system.OperatingSystem.operatingSystem.{cpuMo
 import com.sos.jobscheduler.common.utils.ByteUnits.toMB
 import java.io.File
 import java.nio.file.Path
+import monix.execution.atomic.AtomicBoolean
 
 /**
   * @author Joacim Zschimmer
   */
 object StartUp {
   private val logger = Logger(getClass)
+  private val classPathLogged = AtomicBoolean(false)
 
   /** Log Java version, config and data directory, and classpath. */
   def logStartUp(configDir: Path, dataDir: Path): Unit = {
     logger.info(
       s"Java " + JavaInformations.implementationVersion + " " +
       "(" + toMB(sys.runtime.maxMemory) + ") · " +
-      sys.props("os.name") + distributionNameAndVersionOption.map(o ⇒ s" ($o)").getOrElse("") + " · " +
-      cpuModel.map(o ⇒ s"$o ").getOrElse("") + "(" + sys.runtime.availableProcessors + " threads) · " +
+      sys.props("os.name") + distributionNameAndVersionOption.fold("")(o ⇒ s" ($o)") + " · " +
+      cpuModel.fold("")(o ⇒ s"$o ") + "(" + sys.runtime.availableProcessors + " threads) · " +
       (if (hostname.nonEmpty) s"host=$hostname " else "") +
       s"config=$configDir " +
       s"data=$dataDir")
 
-    for (o ← sys.props("java.class.path") split File.pathSeparator) {
-      logger.debug(Logger.Java, s"Classpath $o")
+    if (!classPathLogged.getAndSet(true)) {  // Log only once (for tests running master and agents in same JVM)
+      for (o ← sys.props("java.class.path") split File.pathSeparator) {
+        logger.debug(Logger.Java, s"Classpath $o")
+      }
     }
   }
 }
