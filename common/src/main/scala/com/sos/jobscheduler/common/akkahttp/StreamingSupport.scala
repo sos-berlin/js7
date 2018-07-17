3e0ca39901ed8d8396284bce591d1376f27d38ce
Joacim Zschimmer <jogit@zschimmer.com>
2018-07-17 15:26:43 +0200

    Web service StreamingSupport logs exception

--- common/src/main/scala/com/sos/jobscheduler/common/akkahttp/StreamingSupport.scala
+++ common/src/main/scala/com/sos/jobscheduler/common/akkahttp/StreamingSupport.scala
@@ -8,6 +8,8 @@ import akka.stream.scaladsl.{Flow, Source}
 import akka.util.ByteString
 import com.google.common.base.Ascii
 import com.sos.jobscheduler.base.utils.CloseableIterator
+import com.sos.jobscheduler.base.utils.ScalaUtils.RichThrowable
+import com.sos.jobscheduler.common.scalautil.Logger
 import monix.eval.Task
 import monix.execution.Scheduler
 import monix.reactive.Observable
@@ -19,6 +21,7 @@ object StreamingSupport
 {
   val `application/json-seq` = MediaType.customWithFixedCharset("application", "json-seq", `UTF-8`)
   private val JsonObjectMaxSize = 100*1000  // TODO Sollte etwas größer als ein stdout-Batzen oder eine Variablenmenge sein
+  private val logger = Logger(getClass)
 
   val JsonSeqStreamSupport: JsonEntityStreamingSupport = {
     val rs = ByteString(Ascii.RS)
@@ -42,6 +45,10 @@ object StreamingSupport
   implicit final class AkkaObservable[A](private val underlying: Observable[A]) extends AnyVal {
     def toAkkaSource(implicit scheduler: Scheduler): Source[A, NotUsed] =
       Source.fromPublisher(underlying.toReactivePublisher(scheduler))
+        .mapError { case throwable ⇒
+          logger.debug(s"Exception in HTTP stream: ${throwable.toStringWithCauses}", throwable)
+          throwable
+        }
   }
 
   def closeableIteratorToAkkaSource[A](iterator: CloseableIterator[A])(implicit scheduler: Scheduler): Source[A, NotUsed] =
