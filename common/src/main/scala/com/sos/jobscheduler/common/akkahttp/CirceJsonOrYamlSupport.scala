b1827c26b726510bb6a6b2db8e79a0e66f13e96e
Joacim Zschimmer <jogit@zschimmer.com>
2018-07-23 09:37:22 +0200

    JSON HTTP marshalling logs exception as warning

--- common/src/main/scala/com/sos/jobscheduler/common/akkahttp/CirceJsonOrYamlSupport.scala
+++ common/src/main/scala/com/sos/jobscheduler/common/akkahttp/CirceJsonOrYamlSupport.scala
@@ -7,12 +7,14 @@ import akka.http.scaladsl.model.{ContentType, HttpEntity}
 import akka.http.scaladsl.unmarshalling.FromEntityUnmarshaller
 import akka.util.ByteString
 import com.sos.jobscheduler.base.circeutils.CirceUtils.implicits.CompactPrinter
+import com.sos.jobscheduler.base.utils.ScalaUtils.RichThrowable
 import com.sos.jobscheduler.common.http.CirceToYaml._
 import com.sos.jobscheduler.common.http.{CirceJsonSupport, CirceToYaml}
 import com.sos.jobscheduler.common.scalautil.Logger
 import io.circe.syntax.EncoderOps
 import io.circe.{Decoder, Encoder}
 import scala.language.implicitConversions
+import scala.util.control.NonFatal
 
 /**
  * Like CirceJsonSupport, but marshals as pretty JSON when text/plain or compact JSON when application/json is requested.
@@ -29,21 +31,32 @@ object CirceJsonOrYamlSupport {
   implicit def jsonOrYamlMarshaller[A: Encoder]: ToEntityMarshaller[A] =
     Marshaller.oneOf(yamlMarshaller, jsonMarshaller)  // Accept: */* should return YAML (for command line curl)
 
-  private def jsonMarshaller[A: Encoder]: ToEntityMarshaller[A] =
+  private def jsonMarshaller[A](implicit encoder: Encoder[A]): ToEntityMarshaller[A] =
     Marshaller.withFixedContentType(`application/json`) { value ⇒
-      val string = CompactPrinter.pretty(value.asJson)
+      val string = logException(s"jsonMarhaller(${encoder.getClass.getName})") {
+        CompactPrinter.pretty(value.asJson)
+      }
       HttpEntity.Strict(`application/json`, ByteString(string))
     }
 
-  private def yamlMarshaller[A: Encoder]: ToEntityMarshaller[A] =
+  private def yamlMarshaller[A](implicit encoder: Encoder[A]): ToEntityMarshaller[A] =
     Marshaller.withOpenCharset(`text/plain`) { (value, charset) ⇒
       try {
-        val string = CirceToYaml.yaml.dump(jsonToYaml(value.asJson))  // OutOfMemoryError possible: two big objects in memory
+        val string = logException(s"ysonMarhaller(${encoder.getClass.getName})") {
+          CirceToYaml.yaml.dump(jsonToYaml(value.asJson))   // OutOfMemoryError possible: two big objects in memory
+        }
         HttpEntity.Strict(ContentType(`text/plain`, charset), ByteString(string.getBytes(charset.nioCharset)))
       } catch {
         case t: OutOfMemoryError ⇒
           logger.error(t.toString)
           throw new RuntimeException(s"While converting to YAML: $t", t)  // To avoid termination of Akka
       }
+    }
+
+  private def logException[A](what: ⇒ String)(body: ⇒ A): A =
+    try body
+    catch { case NonFatal(t) ⇒
+      logger.warn(s"jsonMarhaller($what: ${t.toStringWithCauses}", t)
+      throw t
     }
 }
