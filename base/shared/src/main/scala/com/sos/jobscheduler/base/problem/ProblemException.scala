f4364ee9601b154af96051ced6bef84a53fcc9b6
Joacim Zschimmer <jogit@zschimmer.com>
2018-08-24 05:31:20 +0200

    Logging

--- base/shared/src/main/scala/com/sos/jobscheduler/base/problem/ProblemException.scala
+++ base/shared/src/main/scala/com/sos/jobscheduler/base/problem/ProblemException.scala
@@ -4,6 +4,9 @@ package com.sos.jobscheduler.base.problem
   * @author Joacim Zschimmer
   */
 class ProblemException protected[problem](message: String, cause: Throwable = null)
-extends RuntimeException(message, cause) {
+extends RuntimeException(message, cause)
+{
   def this(cause: Throwable) = this(cause.toString, cause)
+
+  override def toString = s"ProblemException: $getMessage"
 }
