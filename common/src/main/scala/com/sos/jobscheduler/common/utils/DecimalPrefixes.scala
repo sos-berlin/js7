5ff4020d6148d2f6712cbf1722bcdfa20265b4a8
Joacim Zschimmer <jogit@zschimmer.com>
2018-10-20 07:05:58 +0200

    DecimalPrefixes

---
+++ common/src/main/scala/com/sos/jobscheduler/common/utils/DecimalPrefixes.scala
@@ -0,0 +1,22 @@
+package com.sos.jobscheduler.common.utils
+
+/**
+  * @author Joacim Zschimmer
+  */
+object DecimalPrefixes
+{
+  private val PrefixToFactor = Map(
+    'k' → 1000,
+    'M' → (1000*1000),
+    'G' → (1000*1000*1000))
+
+  def toInt(string: String): Int =
+    if (string.nonEmpty && string.last.isLetter) {
+      val prefix = string.last
+      val factor = PrefixToFactor.getOrElse(prefix,
+        throw new IllegalArgumentException(s"Unknown SI-Prefix: '$prefix', expected: ${PrefixToFactor.keys.mkString(", ")}"))
+      val number = string.take(string.length - 1).toInt
+      Math.multiplyExact(number, factor)
+    } else
+      string.toInt
+}
---
+++ common/src/test/scala/com/sos/jobscheduler/common/utils/DecimalPrefixesTest.scala
@@ -0,0 +1,22 @@
+package com.sos.jobscheduler.common.utils
+
+import com.sos.jobscheduler.common.utils.DecimalPrefixes.toInt
+import org.scalatest.FreeSpec
+
+/**
+  * @author Joacim Zschimmer
+  */
+final class DecimalPrefixesTest extends FreeSpec
+{
+  "toInt" in {
+    assert(intercept[Exception] { toInt("") }.toString == "java.lang.NumberFormatException: For input string: \"\"")
+    assert(intercept[Exception] { toInt("k") }.toString == "java.lang.NumberFormatException: For input string: \"\"")
+    assert(intercept[Exception] { toInt("1K") }.getMessage == "Unknown SI-Prefix: 'K', expected: k, M, G")
+    assert(toInt("1") == 1)
+    assert(toInt("1k") == 1000)
+    assert(toInt("-1k") == -1000)
+    assert(toInt("12k") == 12000)
+    assert(toInt("1M") == 1000*1000)
+    assert(toInt("1G") == 1000*1000*1000)
+  }
+}
