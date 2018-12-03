068f4e67431339e79c2f6eb100dcf8dea4c4dccf
Joacim Zschimmer <jogit@zschimmer.com>
2018-12-03 06:53:07 +0100

    sbt -enableassertions

--- master-data/jvm/src/test/java/com/sos/jobscheduler/master/data/javaapi/MasterScriptConverterTester.java
+++ master-data/jvm/src/test/java/com/sos/jobscheduler/master/data/javaapi/MasterScriptConverterTester.java
@@ -17,31 +17,31 @@ final class MasterScriptConverterTester
     private static final String WorkflowJson =
         "{" +
             "\"instructions\":[{\"TYPE\":\"Execute.Anonymous\",\"job\":{\"agentPath\":\"/AGENT\",\"executablePath\":\"/EXECUTABLE\",\"taskLimit\":1}}]," +
-            "\"source\":\"define workflow {\\n  execute \\\"/EXECUTABLE\\\", agent=\\\"/AGENT\\\";\\n}\\n\"" +
+            "\"source\":\"define workflow {\\n  execute executable=\\\"/EXECUTABLE\\\", agent=\\\"/AGENT\\\";\\n}\\n\"" +
         "}";
 
     private MasterScriptConverterTester() {}
 
     static void testWorkflowJsonToScript() {
         JavaChecked<String> checkedScript = MasterScriptConverter.workflowJsonToScript(WorkflowJson);
-        assert checkedScript.isValid(): checkedScript.checked().toString();
+        assert checkedScript.isValid(): checkedScript.checked();
         assert !checkedScript.problem().isPresent();
         assertEqual(checkedScript.get(), WorkflowScript);
     }
 
     static void testWorkflowScriptToJson() {
         JavaChecked<String> checkedJson = MasterScriptConverter.workflowScriptToJson(WorkflowScript);
-        assert checkedJson.isValid() : checkedJson.toString();
+        assert checkedJson.isValid() : checkedJson;
         assert !checkedJson.problem().isPresent();
         assertEqual(checkedJson.get(), WorkflowJson);
     }
 
     static void testWorkflowScriptSyntaxError() {
         JavaChecked<String> checkedJson = MasterScriptConverter.workflowScriptToJson("ERROR");
-        assert checkedJson.isInvalid() ;
+        assert checkedJson.isInvalid();
         Optional<Problem> optionalProblem = checkedJson.problem();
         assert optionalProblem.isPresent();
-        assertEqual(optionalProblem.get().toString(), "\"workflow\":1:1 ...\"ERROR\"");
+        assertEqual(optionalProblem.get().toString(), "\"define\":1:1 ...\"ERROR\"");
     }
 
     private static void assertEqual(String string, String expected) {
--- sbt-batch.cmd
+++ sbt-batch.cmd
@@ -1,11 +1,7 @@
 @echo off
 setlocal
 
-if "%WINDOWS_NET_SDK_HOME%" == "" (
-  set WINDOWS_NET_SDK_HOME=%windir%\Microsoft.NET\Framework\v4.0.30319
-)
-
 rem set SBT_OPTS=
-set JAVA_OPTS=-Xmx2500M -XX:MaxMetaspaceSize=1500M -XX:+CMSClassUnloadingEnabled -Dlog4j.configurationFile=project/log4j2.xml -Dsbt.log.noformat=true %JAVA_OPTS%
+set JAVA_OPTS=-Xmx2500M -XX:MaxMetaspaceSize=1500M -XX:+CMSClassUnloadingEnabled -Dlog4j.configurationFile=project/log4j2.xml -Dsbt.log.noformat=true -enableassertions %JAVA_OPTS%
 sbt.bat %*
 endlocal
