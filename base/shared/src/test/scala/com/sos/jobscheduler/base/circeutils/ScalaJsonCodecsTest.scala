718f98e954ede5324b53d4723d2cdd78ffeb40f8
Joacim Zschimmer <jogit@zschimmer.com>
2019-03-07 10:07:43 +0100

    ScalaJsonCodecsTest

--- base/shared/src/test/scala/com/sos/jobscheduler/base/circeutils/ScalaJsonCodecsTest.scala
+++ base/shared/src/test/scala/com/sos/jobscheduler/base/circeutils/ScalaJsonCodecsTest.scala
@@ -9,11 +9,35 @@ import scala.concurrent.duration._
 /**
   * @author Joacim Zschimmer
   */
-final class ScalaJsonCodecsTest extends FreeSpec {
+final class ScalaJsonCodecsTest extends FreeSpec
+{
+  "FiniteDuration" - {
+    "1s" in {
+      testJson(1.second, json"1")
+    }
 
-  "FinitedDuration" in {
-    testJson(Duration(Long.MaxValue, NANOSECONDS), json"9223372036.854775807")
-    assert(""" "1x" """.parseJsonOrThrow.as[FiniteDuration].isLeft)
-    //assert(""" "PT1m2.123S" """.parseJsonOrThrow.as[Duration].orThrow == Duration.ofSeconds(62, 123456789))
+    "1ms" in {
+      testJson(1.millisecond, json"0.001")
+    }
+
+    "1ns" in {
+      testJson(1.nanosecond, json"0.000000001")
+    }
+
+    "negative" in {
+      testJson(-1234.milliseconds, json"-1.234")
+    }
+
+    "Long.MaxValue" in {
+      testJson(Duration(Long.MaxValue, NANOSECONDS), json"9223372036.854775807")
+    }
+
+    "Invalid" in {
+      assert(""" "1x" """.parseJsonOrThrow.as[FiniteDuration].isLeft)
+    }
+
+    //"ISO-8601" in {
+    //  assert(""" "PT1m2.123S" """.parseJsonOrThrow.as[Duration].orThrow == Duration.ofSeconds(62, 123456789))
+    //}
   }
 }
