04288a5dee03a7577194636c2fad667cef0c6511
Joacim Zschimmer <jogit@zschimmer.com>
2019-05-27 13:29:50 +0200

    ConcurrentRequestLimiter logs some debug messages

--- common/src/main/scala/com/sos/jobscheduler/common/akkahttp/ConcurrentRequestLimiter.scala
+++ common/src/main/scala/com/sos/jobscheduler/common/akkahttp/ConcurrentRequestLimiter.scala
@@ -4,11 +4,13 @@ import akka.http.scaladsl.model.StatusCodes.TooManyRequests
 import akka.http.scaladsl.server.{Directive0, RequestContext, Route, RouteResult}
 import cats.syntax.show._
 import com.sos.jobscheduler.base.problem.Problem
+import com.sos.jobscheduler.base.time.ScalaTime._
 import com.sos.jobscheduler.common.akkahttp.AkkaHttpServerUtils.whenResponseTerminated
 import com.sos.jobscheduler.common.akkahttp.ConcurrentRequestLimiter._
 import com.sos.jobscheduler.common.akkahttp.StandardMarshallers._
-import com.sos.jobscheduler.base.time.ScalaTime._
+import com.sos.jobscheduler.common.scalautil.Logger
 import java.util.concurrent.ConcurrentLinkedQueue
+import monix.execution.atomic.AtomicInt
 import monix.execution.{Cancelable, Scheduler}
 import scala.concurrent.duration._
 import scala.concurrent.{Future, Promise}
@@ -21,10 +23,12 @@ final class ConcurrentRequestLimiter(limit: Int, rejectWithProblem: Problem, tim
 extends Directive0
 {
   @volatile private var busy = 0
-  private val queue = new ConcurrentLinkedQueue[Entry]()
+  private val queue = new ConcurrentLinkedQueue[Entry]
+  private val counter = AtomicInt(0)
 
   def tapply(inner: Unit => Route) = requestContext => {
     def execute() = whenResponseTerminated(_ => onExecuted()).apply(inner(()))(requestContext)
+    val number = counter.incrementAndGet()
     queue.synchronized {
       if (busy < limit) {
         busy += 1
@@ -33,19 +37,22 @@ extends Directive0
         Some(completeWithTooManyRequests(requestContext))
       } else {
         busy += 1
+        logger.debug(s"limit=$limit busy=$busy Request #$number enqueued: ${requestContext.request.uri}")
         val promise = Promise[RouteResult]()
-        val timer = scheduler.scheduleOnce(timeout)(onTimeout(requestContext, promise))
-        queue.add(Entry(() => execute(), promise, timer))
+        val timer = scheduler.scheduleOnce(timeout)(onTimeout(number, requestContext, promise))
+        queue.add(Entry(number, () => execute(), promise, timer))
         Some(promise.future)
       }
     }
     .getOrElse(execute())
   }
 
-  private def onTimeout(requestContext: RequestContext, promise: Promise[RouteResult]): Unit = {
+  private def onTimeout(number: Int, requestContext: RequestContext, promise: Promise[RouteResult]): Unit = {
     queue.synchronized {
       busy -= 1
-      queue.removeIf(_.promise == promise)
+      if (queue.removeIf(_.promise == promise)) {
+        logger.debug(s"limit=$limit busy=$busy Request #$number timed out")
+      }
     }
     promise.tryCompleteWith(completeWithTooManyRequests(requestContext))
   }
@@ -53,16 +60,19 @@ extends Directive0
   private def completeWithTooManyRequests(requestContext: RequestContext) =
     requestContext.complete(TooManyRequests -> rejectWithProblem)
 
-  private def onExecuted(): Unit =
-    queue.synchronized {
+  private def onExecuted(): Unit = {
+    val maybeEntry = queue.synchronized {
       busy -= 1
       for (entry <- Option(queue.poll())) yield {
         entry.timer.cancel()
+        logger.debug(s"limit=$limit busy=$busy Request #${entry.number} dequeued, executes now")
         entry
       }
-    } foreach { e =>
-      e.promise.completeWith(e.execute())
     }
+    for (entry <- maybeEntry) {
+      entry.promise.completeWith(entry.execute())
+    }
+  }
 
   def isBusy = busy >= limit
 
@@ -71,7 +81,10 @@ extends Directive0
 
 object ConcurrentRequestLimiter
 {
+  private val logger = Logger(getClass)
+
   private case class Entry(
+    number: Int,
     execute: () => Future[RouteResult],
     promise: Promise[RouteResult],
     timer: Cancelable)
