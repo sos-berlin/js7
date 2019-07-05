4d0a71ad59c127616d9bcba0fbaff8c3bdc09c6b
Joacim Zschimmer <jogit@zschimmer.com>
2019-07-05 08:10:00 +0200

    OrderCanceledFat, OrderStoppedFat

--- tests/src/test/scala/com/sos/jobscheduler/tests/history/InMemoryHistory.scala
+++ tests/src/test/scala/com/sos/jobscheduler/tests/history/InMemoryHistory.scala
@@ -1,7 +1,7 @@
 package com.sos.jobscheduler.tests.history
 
 import com.sos.jobscheduler.data.event.{<-:, KeyedEvent, Stamped}
-import com.sos.jobscheduler.data.fatevent.OrderFatEvent.{OrderAddedFat, OrderFinishedFat, OrderForkedFat, OrderJoinedFat, OrderProcessedFat, OrderProcessingStartedFat, OrderStdWrittenFat}
+import com.sos.jobscheduler.data.fatevent.OrderFatEvent.{OrderAddedFat, OrderCanceledFat, OrderFinishedFat, OrderForkedFat, OrderJoinedFat, OrderProcessedFat, OrderProcessingStartedFat, OrderStdWrittenFat, OrderStoppedFat}
 import com.sos.jobscheduler.data.fatevent.{FatEvent, OrderFatEvent}
 import com.sos.jobscheduler.data.order.OrderId
 import scala.collection.immutable.Seq
@@ -59,6 +59,16 @@ final class InMemoryHistory {
         }
 
       case OrderFinishedFat(workflowPosition) =>
+        idToOrderEntry(orderId) = idToOrderEntry(orderId).copy(
+          finishedAt = Some(timestamp),
+          endWorkflowPosition = Some(workflowPosition))
+
+      case OrderCanceledFat(workflowPosition) =>
+        idToOrderEntry(orderId) = idToOrderEntry(orderId).copy(
+          finishedAt = Some(timestamp),
+          endWorkflowPosition = Some(workflowPosition))
+
+      case OrderStoppedFat(workflowPosition, _) =>
         idToOrderEntry(orderId) = idToOrderEntry(orderId).copy(
           finishedAt = Some(timestamp),
           endWorkflowPosition = Some(workflowPosition))
