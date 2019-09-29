22238efe25704363c0b37abc4779fc00cd223168
Joacim Zschimmer <jogit@zschimmer.com>
2019-09-29 17:41:03 +0200

    Remove unused code

--- base/shared/src/main/scala/com/sos/jobscheduler/base/utils/Strings.scala
+++ base/shared/src/main/scala/com/sos/jobscheduler/base/utils/Strings.scala
@@ -5,15 +5,11 @@ import scala.math.max
 /**
   * @author Joacim Zschimmer
   */
-object Strings {
-
+object Strings
+{
   private val Ellipsis = "..."
 
   implicit final class RichString(private val underlying: String) extends AnyVal {
-    ///** Truncate to `n`, replacing the tail with ellipsis and, if the string is long, the total character count. */
-    def truncateWithEllipsis(n: Int): String =
-      truncateWithEllipsis(n, showLength = false)
-
     /** Truncate to `n`, replacing the tail with ellipsis and, if the string is long, the total character count. */
     def truncateWithEllipsis(n: Int, showLength: Boolean = false): String = {
       val suffix = if (showLength) s"$Ellipsis(length ${underlying.length})" else Ellipsis
