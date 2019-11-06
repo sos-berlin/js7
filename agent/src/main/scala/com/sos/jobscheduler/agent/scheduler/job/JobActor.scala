a2686b08e491d9afce0b69dd28911042dfb63b0e
Joacim Zschimmer <jogit@zschimmer.com>
2019-11-06 11:58:57 +0100

    logging

--- agent/src/main/scala/com/sos/jobscheduler/agent/scheduler/job/JobActor.scala
+++ agent/src/main/scala/com/sos/jobscheduler/agent/scheduler/job/JobActor.scala
@@ -116,7 +116,7 @@ extends Actor with Stash {
       handleIfReadyForOrder()
 
     case Input.Terminate(sigtermProcesses, sigkillProcessesAfter) =>
-      logger.debug("ShutDown")
+      logger.debug("Terminate")
       terminating = true
       if (sigtermProcesses) {
         killAll(SIGTERM)
