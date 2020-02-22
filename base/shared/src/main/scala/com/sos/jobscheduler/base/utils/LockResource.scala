954a1736cdf32d599b8c10a70f56f3dd3385cccb
Joacim Zschimmer <jogit@zschimmer.com>
2020-02-22 09:26:03 +0100

    LockResource

---
+++ base/shared/src/main/scala/com/sos/jobscheduler/base/utils/LockResource.scala
@@ -0,0 +1,13 @@
+package com.sos.jobscheduler.base.utils
+
+import cats.effect.Resource
+import monix.catnap.MVar
+import monix.eval.Task
+
+object LockResource
+{
+  def apply(): Resource[Task, Unit] = {
+    val lock = MVar[Task].of(()).memoize
+    Resource.make(lock.flatMap(_.take))(_ => lock.flatMap(_.put(())))
+  }
+}
---
+++ base/shared/src/test/scala/com/sos/jobscheduler/base/utils/LockResourceTest.scala
@@ -0,0 +1,48 @@
+package com.sos.jobscheduler.base.utils
+
+import cats.effect.Resource
+import com.sos.jobscheduler.base.time.ScalaTime._
+import monix.eval.Task
+import monix.execution.Scheduler.Implicits.global
+import org.scalatest.AsyncFreeSpec
+import scala.collection.immutable.Seq
+
+/**
+  * @author Joacim Zschimmer
+  */
+final class LockResourceTest extends AsyncFreeSpec
+{
+  private val n = 1000
+  private val initial = 1
+
+  "LockResource" in {
+    doTest(LockResource())
+      .runToFuture
+      .map(o => assert(o == Vector.fill(n)(initial)))
+  }
+
+  "Other Resource does not lock" in {
+    val nonLockingResource = Resource.make(Task.unit)(_ => Task.unit)
+    doTest(nonLockingResource)
+      .runToFuture
+      .map(o => assert(o != Vector.fill(n)(initial)))
+  }
+
+  private def doTest(lockResource: Resource[Task, Unit]): Task[Seq[Int]] = {
+    @volatile var variable = initial
+    val tasks = (1 to n).map(i => lockResource.use { _ =>
+      Task {
+        val found = variable
+        variable += 1
+        found
+      }.flatMap { found =>
+        Task.sleep(100.µs) >>
+        Task {
+          variable = initial
+          found
+        }
+      }
+    })
+    Task.gather(tasks)
+  }
+}
