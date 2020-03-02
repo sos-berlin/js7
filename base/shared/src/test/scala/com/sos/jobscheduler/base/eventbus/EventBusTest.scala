2d604403dd371ae4b009287204a6eeea856b4ebf
Joacim Zschimmer <jogit@zschimmer.com>
2020-03-02 07:43:04 +0100

    EventBus

---
+++ base/shared/src/test/scala/com/sos/jobscheduler/base/eventbus/EventBusTest.scala
@@ -0,0 +1,63 @@
+package com.sos.jobscheduler.base.eventbus
+
+import com.sos.jobscheduler.base.eventbus.EventBusTest._
+import org.scalatest.FreeSpec
+import scala.collection.mutable
+import scala.util.Success
+
+/**
+  * @author Joacim Zschimmer
+  */
+final class EventBusTest extends FreeSpec
+{
+  "subscribe" in {
+    val events = mutable.Buffer[AnyRef]()
+    val eventBus = new EventBus
+    eventBus.publish("ignored")
+
+    val stringSubscription = eventBus.subscribeToClass(classOf[String])(events += _)
+    val bStringSubscription = eventBus.subscribe[String] { events += _ + "-B" }
+    val aSubscription = eventBus.subscribe { a: A => events += a }
+    eventBus.publish("1")
+    eventBus.publish(A("2"))
+    bStringSubscription.close()
+    eventBus.publish("3")
+    bStringSubscription.close()
+    eventBus.publish("4")
+    eventBus.publish(A("5"))
+
+    assert(events.toList == List("1", "1-B", A("2"), ("3"), "4", A("5")))
+
+    assert(!eventBus.isEmpty)
+    stringSubscription.close()
+    aSubscription.close()
+    assert(eventBus.isEmpty)
+  }
+
+  "oneShot" in {
+    val events = mutable.Buffer[AnyRef]()
+    val eventBus = new EventBus
+    eventBus.oneShot[String](events += _)
+    assert(!eventBus.isEmpty)
+    eventBus.publish("ONE")
+    assert(eventBus.isEmpty)
+    eventBus.publish("TWO")
+    assert(events.toList == List("ONE"))
+  }
+
+  "when" in {
+    val eventBus = new EventBus
+    val future = eventBus.when[String]
+    assert(!future.isCompleted)
+    assert(!eventBus.isEmpty)
+    eventBus.publish("ONE")
+    assert(future.isCompleted)
+    assert(future.value == Some(Success("ONE")))
+    assert(eventBus.isEmpty)
+  }
+}
+
+private object EventBusTest
+{
+  private case class A(string: String)
+}
