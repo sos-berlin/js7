3668a17ee82bd5eb8c7773a1e96cf82d92f9ea80
Joacim Zschimmer <jogit@zschimmer.com>
2020-04-26 21:27:50 +0200

    Scala 2.13.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import scala.language.implicitConversions
 //noinspection TypeAnnotation
 object Dependencies
 {
-  val scalaVersion = "2.13.1"
+  val scalaVersion = "2.13.2"
 
   val akkaVersion = "2.5.30"
   val akkaHttpVersion = "10.1.11"
