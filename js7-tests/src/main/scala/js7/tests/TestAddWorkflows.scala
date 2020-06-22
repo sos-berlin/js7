3250931c1207be1b7017561a7da71925d76bedd3
Joacim Zschimmer <jogit@zschimmer.com>
2020-06-22 16:22:15 +0200

    TestAddWorkflows as a main program

---
+++ js7-tests/src/main/scala/js7/tests/TestAddWorkflows.scala
@@ -0,0 +1,126 @@
+package js7.tests
+
+import cats.syntax.option._
+import com.typesafe.config.ConfigFactory
+import java.nio.file.Path
+import js7.base.auth.UserAndPassword
+import js7.base.convert.AsJava.StringAsPath
+import js7.base.time.ScalaTime._
+import js7.base.time.Stopwatch
+import js7.base.utils.AutoClosing.autoClosing
+import js7.base.web.Uri
+import js7.common.commandline.CommandLineArguments
+import js7.common.scalautil.MonixUtils.syntax._
+import js7.controller.client.AkkaHttpControllerApi
+import js7.controller.data.ControllerCommand.UpdateRepo
+import js7.core.startup.JavaMain.runMain
+import js7.data.agent.AgentRefPath
+import js7.data.filebased.{FileBased, FileBasedSigner, VersionId}
+import js7.data.job.ExecutablePath
+import js7.data.workflow.instructions.Execute
+import js7.data.workflow.instructions.executable.WorkflowJob
+import js7.data.workflow.{Workflow, WorkflowPath}
+import js7.tests.TestAddWorkflows._
+import js7.tests.testenv.DirectoryProvider
+import monix.eval.Task
+import monix.execution.Scheduler.Implicits.global
+import monix.execution.atomic.AtomicInt
+import monix.reactive.Observable
+import scala.util.{Failure, Success, Try}
+
+final class TestAddWorkflows(settings: Settings)
+{
+  def run(): Unit = {
+    val directoryProvider = new DirectoryProvider(
+      agentRefPath :: Nil,
+      controllerConfig = ConfigFactory.parseString(
+       """js7.web.server.auth.public = true
+         |akka.stdout-loglevel = "OFF"""".stripMargin),
+      useDirectory = settings.directory)
+    autoClosing(directoryProvider) { _ =>
+      directoryProvider.run { (controller, _)  =>
+        val commands = measureTime(settings.workflowCount, "workflows signed") {
+          generateCommands(directoryProvider.fileBasedSigner)
+        }
+        measureTime(settings.workflowCount, "workflows") {
+          executeCommands(controller.localUri, commands)
+        }
+      }
+    }
+  }
+
+  private def measureTime[A](n: Int, ops: String)(body: => A): A = {
+    val stopwatch = new Stopwatch
+    val a = body
+    println(stopwatch.itemsPerSecondString(n, ops))
+    a
+  }
+
+  private def generateCommands(signer: FileBasedSigner[FileBased]): Seq[UpdateRepo] = {
+    val workflow0 = Workflow.of(Execute(WorkflowJob(agentRefPath, ExecutablePath(s"/EXECUTABLE"))))
+    val versionCounter = AtomicInt(0)
+    Observable.fromIterable(1 to settings.workflowCount)
+      .bufferTumbling(settings.bundleSize)
+      .mapParallelUnordered(sys.runtime.availableProcessors)(is => Task {
+        val v = VersionId(s"SPEED-${versionCounter.incrementAndGet()}")
+        val workflows = for (i <- is) yield workflow0.withId(WorkflowPath(s"/WORKFLOW-$i") ~ v)
+        UpdateRepo(v, workflows map signer.sign)
+      }
+    ).toListL.await(99.s)
+  }
+
+  private def executeCommands(uri: Uri, commands: Seq[UpdateRepo]): Unit =
+    AkkaHttpControllerApi.separateAkkaResource(uri, credentials)
+      .use(controller =>
+        controller.login() >>
+          Observable
+            .fromIterable(commands)
+            .mapParallelUnordered(settings.parallelism)(
+              controller.executeCommand(_))
+            .completedL)
+      .await(1.h)
+}
+
+object TestAddWorkflows
+{
+  private val agentRefPath = AgentRefPath("/AGENT")
+  private val credentials = none[UserAndPassword]
+
+  def main(args: Array[String]): Unit =
+    runMain {
+      Try(Settings.fromArgs(args.toSeq)) match {
+        case Failure(t) =>
+          println(
+            """usage: testAddWorkflows
+              |  --workflow-count=10000
+              |  --bundle-size=100
+              |  --parallelism=2""".stripMargin)
+          println(t.toString)
+          System.exit(1)
+
+        case Success(settings) =>
+          new TestAddWorkflows(settings)
+            .run()
+      }
+    }
+
+  private final case class Settings(
+    directory: Option[Path],
+    workflowCount: Int,
+    bundleSize: Int,
+    parallelism: Int,
+    useCluster: Boolean)
+
+  private object Settings
+  {
+    private[TestAddWorkflows] def fromArgs(args: Seq[String]): Settings =
+      CommandLineArguments.parse(args) { a =>
+        Settings(
+          directory = a.optionAs[Path]("--directory="),
+          workflowCount = a.as[Int]("--workflow-count=", 10000),
+          bundleSize = a.as[Int]("--bundle-size=", 100),
+          parallelism = a.as[Int]("--parallelism=", 2),
+          useCluster = false) //a.boolean("--cluster"))
+      }
+  }
+}
---
+++ js7-tests/src/test/scala/js7/tests/TestAddWorkflowsTest.scala
@@ -0,0 +1,9 @@
+package js7.tests
+
+/**
+  * @author Joacim Zschimmer
+  */
+object TestAddWorkflowsTest extends org.scalatest.FreeSpec
+{
+  def main(args: Array[String]) = TestAddWorkflows.main(args)
+}
