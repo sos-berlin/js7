ebb6c8468768df925457a7312677ae117532474d
Joacim Zschimmer <jogit@zschimmer.com>
2020-08-03 09:22:57 +0200

    Parallelize SnapshotRoute

--- js7-controller/src/main/scala/js7/controller/web/controller/api/SnapshotRoute.scala
+++ js7-controller/src/main/scala/js7/controller/web/controller/api/SnapshotRoute.scala
@@ -1,15 +1,21 @@
 package js7.controller.web.controller.api
 
+import akka.http.scaladsl.model.HttpEntity
 import akka.http.scaladsl.server.Directives.get
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.PathDirectives.pathSingleSlash
+import akka.util.ByteString
+import io.circe.syntax._
 import js7.base.auth.ValidUserPermission
+import js7.base.circeutils.CirceUtils.RichJson
+import js7.base.monixutils.MonixBase.syntax.RichMonixObservable
 import js7.base.problem.Checked
 import js7.base.utils.FutureCompletion
 import js7.base.utils.FutureCompletion.syntax._
 import js7.common.akkahttp.AkkaHttpServerUtils.completeTask
 import js7.common.akkahttp.StandardMarshallers._
-import js7.common.http.JsonStreamingSupport.{NdJsonStreamingSupport, jsonSeqMarshaller}
+import js7.common.http.JsonStreamingSupport.`application/x-ndjson`
+import js7.common.http.StreamingSupport.AkkaObservable
 import js7.common.scalautil.Logger
 import js7.controller.data.ControllerSnapshots.SnapshotJsonCodec
 import js7.controller.data.ControllerState
@@ -33,13 +39,13 @@ trait SnapshotRoute extends ControllerRouteProvider
           completeTask(
             for (checkedState <- controllerState) yield
               for (state <- checkedState) yield {
-                implicit val x = NdJsonStreamingSupport
-                implicit val y = SnapshotJsonCodec
-                implicit val z = jsonSeqMarshaller[Any]
-                monixObservableToMarshallable(
-                  state.toSnapshotObservable
-                    .takeUntilCompletedAndDo(whenShuttingDownCompletion)(_ =>
-                      Task { logger.debug("whenShuttingDown completed") }))
+                  HttpEntity(
+                    `application/x-ndjson`,
+                    state.toSnapshotObservable
+                      .takeUntilCompletedAndDo(whenShuttingDownCompletion)(_ =>
+                        Task { logger.debug("whenShuttingDown completed") })
+                      .mapParallelOrderedBatch()(o => ByteString(o.asJson(SnapshotJsonCodec).compactPrint) ++ LF)
+                      .toAkkaSourceForHttpResponse)
               })
         }
       }
@@ -49,4 +55,5 @@ trait SnapshotRoute extends ControllerRouteProvider
 object SnapshotRoute
 {
   private val logger = Logger(getClass)
+  private val LF = ByteString("\n")
 }
