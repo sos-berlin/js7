762f00e712383c175962858ba5994c499d054751
Joacim Zschimmer <jogit@zschimmer.com>
2020-12-30 20:45:32 +0100

    toKBGB shows two digits

--- js7-common/src/main/scala/js7/common/utils/ByteUnits.scala
+++ js7-common/src/main/scala/js7/common/utils/ByteUnits.scala
@@ -3,17 +3,17 @@ package js7.common.utils
 /**
   * @author Joacim Zschimmer
   */
-object ByteUnits {
-
+object ByteUnits
+{
   /** Converts to bytes, kB, MB or GB */
   def toKBGB(size: Long): String =
     size match {
       case _ if size < 0 => s"${size}bytes"
       case _ if size == 0  => "0kB"
       case _ if size < 1000  => "<1kB"
-      case _ if size < 1000000 => s"${size / 1000}kB"
-      case _ if size < 1000000000 => s"${size / 1000000}MB"
-      case _ => s"${size / 1000000000}GB"
+      case _ if size < 1000000 => formatNumber(size, 1000) + "kB"
+      case _ if size < 1000000000 => formatNumber(size, 1000000) + "MB"
+      case _ => formatNumber(size, 1000000000) + "GB"
     }
 
   def toMB(size: Long): String =
@@ -30,8 +30,20 @@ object ByteUnits {
       case _ if size < 0 => s"${size}bytes"
       case _ if size == 0  => "0KiB"
       case _ if size < 1024  => "<1KiB"
-      case _ if size < 1024*1024 => s"${size / 1024}KiB"
-      case _ if size < 1024*1024*1024 => s"${size / (1024*1024)}MiB"
-      case _ => s"${size / (1024*1024*1024)}GiB"
+      case _ if size < 2*1024-1 => formatNumber(size, 1024) + "KiB"
+      case _ if size < 1024*1024 => formatNumber(size, 1024) + "KiB"
+      case _ if size < 1024*1024*1024 => formatNumber(size, 1024*1024) + "MiB"
+      case _ => formatNumber(size, 1024*1024*1024) + "GiB"
     }
+
+  private[utils] def formatNumber(number: Long, divisor: Int): String = {
+    val n = number / divisor
+    if (n >= 10)
+      n.toString
+    else {
+      val m = 10 * number / divisor % 10
+      if (m == 0) n.toString
+      else s"$n.$m"
+    }
+  }
 }
--- js7-common/src/test/scala/js7/common/utils/ByteUnitsTest.scala
+++ js7-common/src/test/scala/js7/common/utils/ByteUnitsTest.scala
@@ -1,12 +1,50 @@
 package js7.common.utils
 
-import js7.common.utils.ByteUnits.{toKBGB, toKiBGiB, toMB}
+import js7.common.utils.ByteUnits.{toKBGB, toKiBGiB, toMB, formatNumber}
 import org.scalatest.freespec.AnyFreeSpec
 
 /**
   * @author Joacim Zschimmer
   */
-final class ByteUnitsTest extends AnyFreeSpec {
+final class ByteUnitsTest extends AnyFreeSpec
+{
+  "formatNumber" in {
+    for (i <- 0 to 9) {
+      assert(formatNumber(i, 1) == i.toString)
+    }
+    assert(formatNumber(10, 1) == "10")
+
+    // Base 1000
+    assert(formatNumber(1000, 1000) == "1")
+    assert(formatNumber(1099, 1000) == "1")
+    assert(formatNumber(1100, 1000) == "1.1")
+    assert(formatNumber(1199, 1000) == "1.1")
+    assert(formatNumber(1900, 1000) == "1.9")
+    assert(formatNumber(1999, 1000) == "1.9")
+    assert(formatNumber(2000, 1000) == "2")
+    assert(formatNumber(2099, 1000) == "2")
+    assert(formatNumber(2900, 1000) == "2.9")
+    assert(formatNumber(2999, 1000) == "2.9")
+    assert(formatNumber(9999, 1000) == "9.9")
+    assert(formatNumber(10000, 1000) == "10")
+    assert(formatNumber(10100, 1000) == "10")
+    assert(formatNumber(11000, 1000) == "11")
+    assert(formatNumber(99999, 1000) == "99")
+
+    // Base 1024
+    assert(formatNumber(1023, 1024) == "0.9")
+    assert(formatNumber(1024, 1024) == "1")
+    assert(formatNumber(1024+102, 1024) == "1")
+    assert(formatNumber(1024+103, 1024) == "1.1")
+    assert(formatNumber(1024+204, 1024) == "1.1")
+    assert(formatNumber(1024+205, 1024) == "1.2")
+    assert(formatNumber(2048-1, 1024) == "1.9")
+    assert(formatNumber(2048, 1024) == "2")
+    assert(formatNumber(10*1024-1, 1024) == "9.9")
+    assert(formatNumber(10*1024, 1024) == "10")
+    assert(formatNumber(11*1024-1, 1024) == "10")
+    assert(formatNumber(11*1024, 1024) == "11")
+  }
 
   "toKBGB" in {
     assert(toKBGB(-1) == "-1bytes")
@@ -14,10 +52,18 @@ final class ByteUnitsTest extends AnyFreeSpec {
     assert(toKBGB(1) == "<1kB")
     assert(toKBGB(999) == "<1kB")
     assert(toKBGB(1000) == "1kB")
+    assert(toKBGB(1100) == "1.1kB")
+    assert(toKBGB(1199) == "1.1kB")
     assert(toKBGB(999999) == "999kB")
     assert(toKBGB(1000000) == "1MB")
+    assert(toKBGB(1100000) == "1.1MB")
+    assert(toKBGB(1199999) == "1.1MB")
+    assert(toKBGB(10999999) == "10MB")
     assert(toKBGB(999999999) == "999MB")
     assert(toKBGB(1000000000) == "1GB")
+    assert(toKBGB(1100000000) == "1.1GB")
+    assert(toKBGB(1199999999) == "1.1GB")
+    assert(toKBGB(10000000000L) == "10GB")
   }
 
   "toMB" in {
@@ -31,15 +77,21 @@ final class ByteUnitsTest extends AnyFreeSpec {
   }
 
   "toKiBGiB" in {
+    val K = 1024L
+    val M = 1024*K
+    val G = 1024*M
     assert(toKiBGiB(-1) == "-1bytes")
     assert(toKiBGiB(0) == "0KiB")
     assert(toKiBGiB(1) == "<1KiB")
-    assert(toKiBGiB(1024-1) == "<1KiB")
-    assert(toKiBGiB(1024) == "1KiB")
-    assert(toKiBGiB(1024*1024-1) == "1023KiB")
-    assert(toKiBGiB(1024*1024) == "1MiB")
-    assert(toKiBGiB(1024*1024*1024-1) == "1023MiB")
-    assert(toKiBGiB(1024*1024*1024) == "1GiB")
-    assert(toKiBGiB(1024L*1024*1024*1024) == "1024GiB")
+    assert(toKiBGiB(K-1) == "<1KiB")
+    assert(toKiBGiB(K) == "1KiB")
+    assert(toKiBGiB(K+K/2) == "1.5KiB")
+    assert(toKiBGiB(M-1) == "1023KiB")
+    assert(toKiBGiB(M) == "1MiB")
+    assert(toKiBGiB(M+M/2) == "1.5MiB")
+    assert(toKiBGiB(M*K-1) == "1023MiB")
+    assert(toKiBGiB(G+G/2) == "1.5GiB")
+    assert(toKiBGiB(10*G+G/2) == "10GiB")
+    assert(toKiBGiB(1024*G) == "1024GiB")
   }
 }
