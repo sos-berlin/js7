a603d54c8f7ea3d38566aceccf952659f550589b
Joacim Zschimmer <jogit@zschimmer.com>
2021-02-08 11:48:47 +0100

    PlayTest

---
+++ js7-tests/src/test/scala/js7/tests/PlayTest.scala
@@ -0,0 +1,95 @@
+package js7.tests
+
+import io.circe.Json
+import js7.base.circeutils.CirceUtils.{JsonStringInterpolator, RichCirceEither}
+import js7.base.configutils.Configs._
+import js7.base.problem.Checked._
+import js7.base.utils.ScalaUtils.syntax.RichPartialFunction
+import js7.common.scalautil.Logger
+import js7.data.agent.AgentId
+import js7.data.item.VersionId
+import js7.data.order.OrderEvent.{OrderFailed, OrderFinished, OrderProcessed, OrderStdoutWritten}
+import js7.data.order.{FreshOrder, OrderEvent, OrderId, Outcome}
+import js7.data.value.{NamedValues, NumberValue, Value}
+import js7.data.workflow.{Workflow, WorkflowPath}
+import js7.executor.internal.InternalJob
+import js7.executor.internal.InternalJob.{OrderContext, OrderProcess, Result}
+import js7.tests.testenv.ControllerAgentForScalaTest
+import monix.eval.Task
+import org.scalatest.freespec.AnyFreeSpec
+
+/* A test to play with. */
+final class PlayTest extends AnyFreeSpec with ControllerAgentForScalaTest
+{
+  protected val agentIds = PlayTest.agentIds
+  protected val versionedItems = Nil
+  override protected val controllerConfig = config"""
+    js7.web.server.auth.public = on
+    js7.journal.remove-obsolete-files = false
+    js7.controller.agent-driver.command-batch-delay = 0ms
+    js7.controller.agent-driver.event-buffer-delay = 10ms"""
+  override protected def agentConfig = config"""
+    js7.job.execution.signed-script-injection-allowed = on
+    """
+  private val versionIdIterator = Iterator.from(1).map(i => VersionId(s"v$i"))
+  private val workflowPathIterator = Iterator.from(1).map(i => WorkflowPath(s"WORKFLOW-$i"))
+  private val orderIdIterator = Iterator.from(1).map(i => OrderId(s"🔵-$i"))
+
+  "Test" in {
+    val workflowJson = json"""{
+      "instructions": []
+    }"""
+
+    testWithWorkflow(toWorkflow(workflowJson),
+      Map.empty,
+      expectedOutput = "",
+      expectedOutcomes = Nil)
+  }
+
+  private def toWorkflow(json: Json): Workflow =
+    json.as(Workflow.topJsonDecoder).toChecked.orThrow
+
+  private def testWithWorkflow(
+    anonymousWorkflow: Workflow,
+    orderArguments: Map[String, Value] = Map.empty,
+    expectedOutput: String,
+    expectedOutcomes: Seq[Outcome])
+  : Unit = {
+    val events = runWithWorkflow(anonymousWorkflow, orderArguments)
+    val output = events.collect { case OrderStdoutWritten(chunk) => chunk }.fold("")(_ + _)
+    val outcomes = events.collect { case OrderProcessed(outcome) => outcome }
+    assert(output == expectedOutput && outcomes == expectedOutcomes)
+    if (expectedOutcomes.lastOption.forall(_.isSucceeded))
+      assert(events.last.isInstanceOf[OrderFinished])
+    else
+      assert(events.last.isInstanceOf[OrderFailed])
+  }
+
+  private def runWithWorkflow(
+    anonymousWorkflow: Workflow,
+    orderArguments: Map[String, Value] = Map.empty)
+  : Seq[OrderEvent] = {
+    val versionId = versionIdIterator.next()
+    val workflow = anonymousWorkflow.withId(workflowPathIterator.next() ~ versionId)
+    val order = FreshOrder(orderIdIterator.next(), workflow.path, arguments = orderArguments)
+    directoryProvider.updateVersionedItems(controller, versionId, Seq(workflow))
+
+    controller.runOrder(order).map(_.value)
+  }
+}
+
+object PlayTest
+{
+  private val logger = Logger(getClass)
+  private val agentIds = Seq(AgentId("primaryAgent"), AgentId("secondaryAgent"))  // Names for Andreas' test cases
+
+  private final class TestInternalJob extends InternalJob
+  {
+    def processOrder(context: OrderContext) =
+      OrderProcess(
+        Task {
+          for (number <- context.arguments.checked("ARG").flatMap(_.toNumber).map(_.number)) yield
+            Result(NamedValues("RESULT" -> NumberValue(number + 1)))
+        })
+  }
+}
