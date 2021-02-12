451f2a4c420ceed2afb6e42cf99b5ebdb5a1fd9b
Joacim Zschimmer <jogit@zschimmer.com>
2021-02-12 11:32:05 +0100

    UnbufferedReaderToObservable

---
+++ js7-base/jvm/src/test/scala/js7/base/monixutils/UnbufferedReaderObservableTest.scala
@@ -0,0 +1,31 @@
+package js7.base.monixutils
+
+import java.io.{PipedReader, PipedWriter}
+import monix.eval.Task
+import monix.execution.Scheduler.Implicits.global
+import monix.execution.schedulers.TestScheduler
+import org.scalatest.freespec.AnyFreeSpec
+
+final class UnbufferedReaderObservableTest extends AnyFreeSpec
+{
+  "UnbufferedReaderObservable" in {
+    val scheduler = TestScheduler()
+    val w = new PipedWriter
+    val r = new PipedReader(w)
+
+    val whenCompleted = UnbufferedReaderObservable(Task.pure(r))
+      .toListL.runToFuture
+
+    val expected = List("EINS", "ZWEI", "DREI")
+    for (s <- expected) {
+      w.write(s)
+      w.flush()
+      scheduler.tick()
+    }
+    w.close()
+
+    for (list <- whenCompleted) yield {
+      assert(list == expected)
+    }
+  }
+}
