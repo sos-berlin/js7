5b9e65d2a7516fc7b9037e2ee2b17c5fcb24aa87
Joacim Zschimmer <jogit@zschimmer.com>
2021-02-16 23:11:49 +0100

    Test toReleaseEventId

--- js7-data/shared/src/test/scala/js7/data/event/JournalStateTest.scala
+++ js7-data/shared/src/test/scala/js7/data/event/JournalStateTest.scala
@@ -19,4 +19,31 @@ final class JournalStateTest extends AnyFreeSpec
       }
     }""")
   }
+
+  "toReleaseEventId" in {
+    val a = UserId("A")
+    val b = UserId("B")
+    val x = UserId("X")
+
+    assert(JournalState.empty.toReleaseEventId(10, Nil) == 10)
+    assert(JournalState.empty.toReleaseEventId(10, Seq(a)) == 0)
+
+    val journalState = JournalState(Map(a -> 10, b -> 20))
+
+    assert(journalState.toReleaseEventId(5, Nil) == 5)
+    assert(journalState.toReleaseEventId(15, Nil) == 10)
+    assert(journalState.toReleaseEventId(25, Nil) == 10)
+
+    assert(journalState.toReleaseEventId(5, Seq(a)) == 5)
+    assert(journalState.toReleaseEventId(15, Seq(a)) == 10)
+    assert(journalState.toReleaseEventId(25, Seq(a)) == 10)
+
+    assert(journalState.toReleaseEventId(5, Seq(a, b)) == 5)
+    assert(journalState.toReleaseEventId(15, Seq(a, b)) == 10)
+    assert(journalState.toReleaseEventId(25, Seq(a, b)) == 10)
+
+    assert(journalState.toReleaseEventId(5, Seq(x)) == 0)
+    assert(journalState.toReleaseEventId(15, Seq(x)) == 0)
+    assert(journalState.toReleaseEventId(25, Seq(x)) == 0)
+  }
 }
