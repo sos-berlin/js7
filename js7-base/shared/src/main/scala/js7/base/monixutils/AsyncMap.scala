c4ed5ca5e9f248d8aeddecdc335ff3ed4a722e07
Joacim Zschimmer <jogit@zschimmer.com>
2021-03-11 10:02:40 +0100

    AsyncMap

---
+++ js7-base/shared/src/main/scala/js7/base/monixutils/AsyncMap.scala
@@ -0,0 +1,60 @@
+package js7.base.monixutils
+
+import cats.syntax.flatMap._
+import js7.base.problem.Checked
+import js7.base.utils.ScalaUtils.syntax._
+import monix.catnap.MVar
+import monix.eval.Task
+import scala.reflect.ClassTag
+
+final class AsyncMap[K: ClassTag, V](initial: Map[K, V])
+{
+  private val mvarTask = MVar.of[Task, Map[K, V]](initial).memoize
+
+  def all: Task[Map[K, V]] =
+    mvarTask.flatMap(_.read)
+
+  def removeAll: Task[Map[K, V]] =
+    mvarTask.flatMap(mvar => mvar
+      .take
+      .flatMap(result => mvar.put(Map.empty).as(result)))
+
+  def get(key: K): Task[Checked[V]] =
+    mvarTask.flatMap(_.read)
+      .map(_.checked(key))
+
+  def update(key: K, update: Option[V] => Task[V]): Task[V] =
+    getAndUpdate(key, update)
+      .map(_._2)
+
+  def getAndUpdate(key: K, update: Option[V] => Task[V]): Task[(Option[V], V)] =
+    mvarTask
+      .flatMap(mvar =>
+        mvar.take.flatMap { map =>
+          val previous = map.get(key)
+          update(previous)
+            .flatMap(value =>
+              mvar.put(map + (key -> value))
+            .map(_ => previous -> value))
+          // TODO Handle failure and cancellation
+        })
+      .uncancelable
+
+  def updateChecked(key: K, update: Option[V] => Task[Checked[V]]): Task[Checked[V]] =
+    mvarTask
+      .flatMap(mvar =>
+        mvar.take.flatMap(map =>
+          update(map.get(key))
+            .flatTap(checked => mvar.put(checked match {
+              case Left(_) => map
+              case Right(value) => map + (key -> value)
+              // TODO Handle failure and cancellation
+            }))))
+      .uncancelable
+}
+
+object AsyncMap
+{
+  def apply[K: ClassTag, V](initial: Map[K, V] = Map.empty) =
+    new AsyncMap(initial)
+}
---
+++ js7-base/shared/src/test/scala/js7/base/monixutils/AsyncMapTest.scala
@@ -0,0 +1,79 @@
+package js7.base.monixutils
+
+import js7.base.problem.Problems.UnknownKeyProblem
+import js7.base.problem.{Checked, Problem}
+import monix.eval.Task
+import monix.execution.Scheduler.Implicits.global
+import org.scalatest.freespec.AsyncFreeSpec
+
+final class AsyncMapTest extends AsyncFreeSpec
+{
+  private val asyncMap = AsyncMap(Map(1 -> "EINS"))
+
+  private def update(maybe: Option[String]): Task[String] =
+    maybe.fold(Task("FIRST"))(o => Task.pure(o + "-UPDATED"))
+
+  "get existing" in {
+    asyncMap.get(1)
+      .map(checked => assert(checked == Right("EINS")))
+      .runToFuture
+  }
+
+  "get non-existing" in {
+    asyncMap.get(0)
+      .map(checked => assert(checked == Left(UnknownKeyProblem("Int", "0"))))
+      .runToFuture
+  }
+
+  "getAndUpdate" in {
+    asyncMap.getAndUpdate(0, update)
+      .map(pair => assert(pair == (None, "FIRST")))
+      .flatMap(_ =>
+        asyncMap.getAndUpdate(0, update))
+      .map(pair => assert(pair == (Some("FIRST"), "FIRST-UPDATED")))
+      .runToFuture
+  }
+
+  "update" in {
+    asyncMap.update(2, update)
+      .map(o => assert(o == "FIRST"))
+      .runToFuture
+  }
+
+  "updateChecked" in {
+    def updateChecked(maybe: Option[String]): Task[Checked[String]] =
+      maybe match {
+        case None => Task(Right("FIRST"))
+        case Some(_) => Task(Left(Problem("EXISTING")))
+      }
+
+    asyncMap.updateChecked(3, updateChecked)
+      .map(o => assert(o == Right("FIRST")))
+      .flatMap(_ =>
+        asyncMap.updateChecked(3, updateChecked))
+      .map(o => assert(o == Left(Problem("EXISTING"))))
+      .runToFuture
+  }
+
+  "all" in {
+    asyncMap.all.map(all =>
+      assert(all == Map(
+        0 -> "FIRST-UPDATED",
+        1 -> "EINS",
+        2 -> "FIRST",
+        3 -> "FIRST")))
+      .runToFuture
+  }
+
+  "removeAll" in {
+    asyncMap.removeAll
+      .map(all => assert(all == Map(
+        0 -> "FIRST-UPDATED",
+        1 -> "EINS",
+        2 -> "FIRST",
+        3 -> "FIRST")))
+      .flatMap(_ => asyncMap.all)
+      .map(all => assert(all.isEmpty))
+      .runToFuture
+  }
+}
