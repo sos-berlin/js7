e0589f3636094f5df4287073261f4b70e4a08fc1
Joacim Zschimmer <jogit@zschimmer.com>
2021-03-11 09:15:58 +0100

    Java Pattern JSON codecs

---
+++ js7-base/jvm/src/test/scala/js7/base/circeutils/StandardJsonCodecsTest.scala
@@ -0,0 +1,31 @@
+package js7.base.circeutils
+
+import io.circe.syntax.EncoderOps
+import java.util.regex.Pattern
+import js7.base.circeutils.CirceUtils.{JsonStringInterpolator, RichCirceEither}
+import js7.base.circeutils.StandardJsonCodecs.{PatternJsonCodec, RegexJsonCodec}
+import js7.base.problem.Problem
+import js7.base.utils.ScalaUtils.syntax.RichEither
+import org.scalatest.freespec.AnyFreeSpec
+import scala.util.matching.Regex
+
+final class StandardJsonCodecsTest extends AnyFreeSpec
+{
+  "Pattern" in {
+    // Pattern does not implement `equals`
+    val json = json"""".*""""
+    assert(".*".r.pattern.asJson == json)
+    assert(json.as[Pattern].toChecked.orThrow.pattern == ".*".r.pattern.pattern)
+    assert("\\".asJson.as[Pattern].toChecked == Left(Problem(
+      "JSON DecodingFailure at : java.util.regex.PatternSyntaxException: Unexpected internal error near index 1\\n\\")))
+  }
+
+  "Regex" in {
+    // Regex does not implement `equals`
+    val json = json"""".*""""
+    assert(".*".r.asJson == json)
+    assert(json.as[Regex].toChecked.orThrow.pattern.pattern == ".*".r.pattern.pattern)
+    assert("\\".asJson.as[Regex].toChecked == Left(Problem(
+      "JSON DecodingFailure at : java.util.regex.PatternSyntaxException: Unexpected internal error near index 1\\n\\")))
+  }
+}
---
+++ js7-base/shared/src/main/scala/js7/base/circeutils/StandardJsonCodecs.scala
@@ -0,0 +1,21 @@
+package js7.base.circeutils
+
+import io.circe.Codec
+import java.util.regex.Pattern
+import js7.base.circeutils.CirceUtils.{stringJsonCodec, toStringJsonCodec}
+import scala.util.matching.Regex
+
+object StandardJsonCodecs
+{
+  implicit val PatternJsonCodec: Codec[Pattern] =
+    stringJsonCodec(
+      pattern =>
+        if (pattern.flags() != 0)
+          throw new IllegalArgumentException(s"Pattern with flags cannot be JSON-encoded: $pattern")
+        else
+          pattern.pattern,
+      Pattern.compile(_))
+
+  implicit val RegexJsonCodec: Codec[Regex] =
+    toStringJsonCodec(_.r)
+}
