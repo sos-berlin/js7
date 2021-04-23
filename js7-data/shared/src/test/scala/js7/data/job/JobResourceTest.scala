14d2837687acb655a15cf36a8730411451302848
Joacim Zschimmer <jogit@zschimmer.com>
2021-04-23 08:10:14 +0200

    JobResourceTest

--- js7-data/shared/src/test/scala/js7/data/job/JobResourceTest.scala
+++ js7-data/shared/src/test/scala/js7/data/job/JobResourceTest.scala
@@ -3,7 +3,7 @@ package js7.data.job
 import js7.base.circeutils.CirceUtils.JsonStringInterpolator
 import js7.data.controller.ControllerState.signableSimpleItemJsonCodec
 import js7.data.item.{ItemRevision, SignableSimpleItem}
-import js7.data.value.expression.Expression.ObjectExpression
+import js7.data.value.expression.Expression.{Argument, Concat, FunctionCall, ObjectExpression, StringConstant}
 import js7.tester.CirceJsonTester.{testJson, testJsonDecoder}
 import org.scalatest.freespec.AnyFreeSpec
 
@@ -20,13 +20,20 @@ final class JobResourceTest extends AnyFreeSpec
     testJson[SignableSimpleItem](
       JobResource(
         JobResourcePath("JOB-RESOURCE"),
-        ObjectExpression.empty,
+        ObjectExpression(Map(
+          "NAME" -> StringConstant("VALUE"),
+          "MYPATH" -> Concat(
+            StringConstant("/bin:"),
+            FunctionCall("env", Seq(Argument(StringConstant("PATH"))))))),
         //Seq(JobResourcePath("DEPENDS-ON")),
         Some(ItemRevision(1))),
       json"""{
-        "TYPE": "JobResource",
         "path": "JOB-RESOURCE",
-        "env": {},
+        "TYPE": "JobResource",
+        "env": {
+          "NAME": "'VALUE'",
+          "MYPATH": "'/bin:' ++ env('PATH')"
+        },
         "itemRevision": 1
       }""")
   }
