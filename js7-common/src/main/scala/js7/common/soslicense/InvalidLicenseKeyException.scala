72964bedd7bab938a7296011b2fcab3760b7a8ca
Joacim Zschimmer <jogit@zschimmer.com>
2021-05-12 10:30:04 +0200

    Delete legacy license code

--- js7-common/src/main/scala/js7/common/soslicense/InvalidLicenseKeyException.scala
+++
@@ -1,8 +0,0 @@
-package js7.common.soslicense
-
-/**
- * @author Joacim Zschimmer
- */
-final class InvalidLicenseKeyException(val key: LicenseKeyString) extends RuntimeException {
-  override def getMessage = s"Invalid license key: $key"
-}
--- js7-common/src/main/scala/js7/common/soslicense/LicenseKeyRequiredException.scala
+++
@@ -1,8 +0,0 @@
-package js7.common.soslicense
-
-import js7.base.exceptions.StandardPublicException
-
-/**
- * @author Joacim Zschimmer
- */
-class LicenseKeyRequiredException extends StandardPublicException("License key is required")
