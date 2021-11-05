23f04f69bb0d5500194f21a79426b72ee81f6aff
Joacim Zschimmer <jogit@zschimmer.com>
2021-11-05 07:38:08 +0100

    binarySearch

---
+++ js7-base/shared/src/main/scala/js7/base/utils/BinarySearch.scala
@@ -0,0 +1,27 @@
+package js7.base.utils
+
+import scala.annotation.tailrec
+
+object BinarySearch
+{
+  def binarySearch(indexedSeq: IndexedSeq[Int], value: Int): (Int, Boolean) =
+    binarySearch(0, indexedSeq.length, indexedSeq(_).compare(value))
+
+  def binarySearch(start: Int, end: Int, compareTo: Int => Int): (Int, Boolean) =
+  {
+    @tailrec def loop(low: Int, high: Int): (Int, Boolean) =
+      if (low <= high) {
+        val middle = (low + high) >>> 1
+        val cmp = compareTo(middle)
+        if (cmp < 0)
+          loop(low + 1, high)
+        else if (cmp > 0)
+          loop(low, high - 1)
+        else
+          middle -> true
+      } else
+        low -> false
+
+    loop(start, end - 1)
+  }
+}
---
+++ js7-base/shared/src/test/scala/js7/base/utils/BinarySearchTest.scala
@@ -0,0 +1,23 @@
+package js7.base.utils
+
+import js7.base.utils.BinarySearch.binarySearch
+import org.scalatest.freespec.AnyFreeSpec
+
+final class BinarySearchTest extends AnyFreeSpec
+{
+  "binarySearch" in {
+    assert(binarySearch(Vector.empty, 7) == (0, false))
+
+    assert(binarySearch(Vector(30), 20) == (0, false))
+    assert(binarySearch(Vector(30), 30) == (0, true))
+    assert(binarySearch(Vector(30), 40) == (1, false))
+
+    assert(binarySearch(Vector(20, 30, 40), 15) == (0, false))
+    assert(binarySearch(Vector(20, 30, 40), 20) == (0, true))
+    assert(binarySearch(Vector(20, 30, 40), 25) == (1, false))
+    assert(binarySearch(Vector(20, 30, 40), 30) == (1, true))
+    assert(binarySearch(Vector(20, 30, 40), 35) == (2, false))
+    assert(binarySearch(Vector(20, 30, 40), 40) == (2, true))
+    assert(binarySearch(Vector(20, 30, 40), 56) == (3, false))
+  }
+}
