1fb7f907ec3e3e428187c683a0018e23ee16081f
Joacim Zschimmer <jogit@zschimmer.com>
2021-11-12 20:16:54 +0100

    AsyncVariable.update

--- js7-base/shared/src/main/scala/js7/base/monixutils/AsyncVariable.scala
+++ js7-base/shared/src/main/scala/js7/base/monixutils/AsyncVariable.scala
@@ -12,6 +12,20 @@ final class AsyncVariable[V](initial: V)
   def get: V =
     _value
 
+  def update(update: V => Task[V]): Task[V] =
+    lock.lock(
+      for (v <- update(_value)) yield {
+        _value = v
+        v
+      })
+
+  def updateChecked(update: V => Task[Checked[V]]): Task[Checked[V]] =
+    lock.lock(
+      for (checked <- update(_value)) yield {
+        for (v <- checked) _value = v
+        checked
+      })
+
   def updateCheckedWithResult[R](update: V => Task[Checked[(V, R)]]): Task[Checked[R]] =
     lock.lock(
       update(_value)
--- js7-base/shared/src/test/scala/js7/base/monixutils/AsyncVariableTest.scala
+++ js7-base/shared/src/test/scala/js7/base/monixutils/AsyncVariableTest.scala
@@ -13,20 +13,40 @@ final class AsyncVariableTest extends AsyncFreeSpec
     assert(asyncVariable.get == 1)
   }
 
+  "updated" in {
+    asyncVariable.update(i => Task(i + 1))
+      .map(v => assert(v == 2))
+      .runToFuture
+  }
+
+  "updatedChecked" - {
+    "Left" in {
+      asyncVariable.updateChecked(i => Task(Left(Problem("PROBLEM"))))
+        .map(checked => assert(checked == Left(Problem("PROBLEM"))))
+        .runToFuture
+    }
+
+    "Right" in {
+      asyncVariable.updateChecked(i => Task(Right(i + 1)))
+        .map(v => assert(v == Right(3)))
+        .runToFuture
+    }
+  }
+
   "updateCheckedWithResult" - {
     "Left" in {
       asyncVariable
-        .updateCheckedWithResult[Nothing](i => Task(Left(Problem("PROBLEM"))))
+        .updateCheckedWithResult[Nothing](_ => Task(Left(Problem("PROBLEM"))))
         .map(checked =>
-          assert(checked == Left(Problem("PROBLEM")) && asyncVariable.get == 1))
+          assert(checked == Left(Problem("PROBLEM")) && asyncVariable.get == 3))
         .runToFuture
     }
 
     "Right" in {
       asyncVariable
-        .updateCheckedWithResult(i => Task(Right(i + 3 -> "hej")))
+        .updateCheckedWithResult(i => Task(Right(i + 4 -> "hej")))
         .map(checked =>
-          assert(checked == Right("hej") && asyncVariable.get == 4))
+          assert(checked == Right("hej") && asyncVariable.get == 7))
         .runToFuture
     }
   }
