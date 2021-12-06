726aac5f65713ce4cd3020e2919be1807595a1ee
Joacim Zschimmer <jogit@zschimmer.com>
2021-12-06 11:05:27 +0100

    Uri.port

---
+++ js7-base/jvm/src/test/java/js7/base/web/UriJvmTest.scala
@@ -0,0 +1,13 @@
+package js7.base.web
+
+import js7.base.problem.Problem
+import org.scalatest.freespec.AnyFreeSpec
+
+final class UriJvmTest extends AnyFreeSpec
+{
+  "port" in {
+    assert(Uri("http://").port.left.toOption.get.toString.contains("URI"))
+    assert(Uri("http://example.com/test").port == Left(Problem("URI has no port number")))
+    assert(Uri("http://example.com:1234/test").port == Right(1234))
+  }
+}
--- js7-base/shared/src/main/scala/js7/base/web/Uri.scala
+++ js7-base/shared/src/main/scala/js7/base/web/Uri.scala
@@ -1,10 +1,24 @@
 package js7.base.web
 
+import java.net.{URI, URISyntaxException}
 import js7.base.annotation.javaApi
 import js7.base.generic.GenericString
+import js7.base.problem.{Checked, Problem}
 
 final case class Uri(string: String) extends GenericString
 {
+  private lazy val javaUri: Checked[URI] =
+    try Right(new URI(string))
+    catch { case t: URISyntaxException =>
+      Left(Problem.pure("Invalid URI: " + t.getMessage))
+    }
+
+  def port: Checked[Int] =
+    javaUri.flatMap(_.getPort match {
+      case -1 => Left(Problem.pure("URI has no port number"))
+      case n => Right(n)
+    })
+
   /** Concats with exactly one slash between the parts. */
   def /(tail: String): Uri =
     if (string.endsWith("/") != tail.startsWith("/"))
