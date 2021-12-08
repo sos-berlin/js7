776926bf77eebfed58d4cd126c8c6eb920181065
Joacim Zschimmer <jogit@zschimmer.com>
2021-12-08 20:56:34 +0100

    fix Akka ClassLoader problem for testing

--- js7-agent/src/main/scala/js7/agent/configuration/Akkas.scala
+++ js7-agent/src/main/scala/js7/agent/configuration/Akkas.scala
@@ -4,10 +4,10 @@ import akka.actor.ActorSystem
 import com.typesafe.config.{Config, ConfigFactory}
 import js7.base.utils.Closer
 import js7.base.utils.ScalaUtils.syntax._
-import js7.base.utils.SideEffect._
 import js7.common.akkautils.Akkas.terminateAndWait
 import js7.common.akkautils.DeadLetterActor
 import scala.concurrent.ExecutionContext
+import scala.util.chaining.scalaUtilChainingOps
 
 /**
  * @author Joacim Zschimmer
@@ -21,11 +21,12 @@ object Akkas
       .withFallback(AgentConfiguration.DefaultConfig)
       .resolve
     val ec = myConfig.getBoolean("js7.akka.use-js7-thread-pool") ? defaultExecutionContext
-    ActorSystem(name, config = Some(myConfig), defaultExecutionContext = ec) sideEffect { o =>
-      DeadLetterActor.subscribe(o)
-      closer.onClose {
-        terminateAndWait(o)
+    ActorSystem(name, Some(myConfig), Some(Akkas.getClass.getClassLoader), ec)
+      .tap { o =>
+        DeadLetterActor.subscribe(o)
+        closer.onClose {
+          terminateAndWait(o)
+        }
       }
-    }
   }
 }
