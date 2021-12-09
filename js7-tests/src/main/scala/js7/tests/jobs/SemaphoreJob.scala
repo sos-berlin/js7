b29b91a40eea3201ad3fd28279261cfd0ec6c5f8
Joacim Zschimmer <jogit@zschimmer.com>
2021-12-09 04:36:46 +0100

    SemaphoreJobTest

--- js7-tests/src/main/scala/js7/tests/jobs/SemaphoreJob.scala
+++ js7-tests/src/main/scala/js7/tests/jobs/SemaphoreJob.scala
@@ -1,8 +1,10 @@
 package js7.tests.jobs
 
+import js7.base.log.Logger
 import js7.data.order.Outcome
 import js7.launcher.OrderProcess
 import js7.launcher.internal.InternalJob
+import js7.tests.jobs.SemaphoreJob._
 import monix.catnap.Semaphore
 import monix.eval.Task
 import monix.execution.Scheduler
@@ -17,22 +19,49 @@ extends InternalJob
   final def toOrderProcess(step: Step) = {
     OrderProcess(
       semaphore
+        .tapEval(sema =>
+          sema.count.flatMap(count =>
+            Task(logger.debug(s"${step.order.id} acquire ... (count=$count)"))))
         .flatMap(_.acquire)
+        .tapEval(_ => Task(logger.debug(s"${step.order.id} acquired")))
         .as(Outcome.succeeded))
   }
 }
 
 object SemaphoreJob
 {
+  private val logger = Logger[this.type]
+
   abstract class Companion[I <: SemaphoreJob](implicit classTag: ClassTag[I])
   extends InternalJob.Companion[I]
   {
     val semaphore = Semaphore[Task](0).memoize
 
-    def continue(n: Int = 1)(implicit s: Scheduler): Unit =
-      semaphore.flatMap(_.releaseN(n)).runSyncUnsafe()
+    def reset()(implicit s: Scheduler): Unit = {
+      logger.debug("reset")
+      (for {
+        sema <- semaphore
+        count <- sema.count
+        _ <-
+          if (count > 0) sema.acquireN(count)
+          else if (count < 0) sema.releaseN(-count)
+          else Task.pure(sema)
+      } yield ())
+        .runSyncUnsafe()
+    }
 
-    def continueN(n: Int)(implicit s: Scheduler): Unit =
-      semaphore.flatMap(_.releaseN(n)).runSyncUnsafe()
+    def continue(n: Int = 1)(implicit s: Scheduler): Unit = {
+      logger.debug(s"continue($n)")
+      semaphore
+        .flatMap(_.releaseN(n))
+        .runSyncUnsafe()
+    }
+
+    def continueN(n: Int)(implicit s: Scheduler): Unit = {
+      logger.debug(s"continue($n)")
+      semaphore
+        .flatMap(_.releaseN(n))
+        .runSyncUnsafe()
+    }
   }
 }
---
+++ js7-tests/src/test/scala/js7/tests/jobs/SemaphoreJobTest.scala
@@ -0,0 +1,72 @@
+package js7.tests.jobs
+
+import js7.base.configutils.Configs.HoconStringInterpolator
+import js7.base.thread.MonixBlocking.syntax.RichTask
+import js7.base.time.ScalaTime._
+import js7.base.utils.ScalaUtils.syntax.RichEither
+import js7.data.agent.AgentPath
+import js7.data.order.OrderEvent.{OrderProcessed, OrderProcessingStarted}
+import js7.data.order.{FreshOrder, OrderId}
+import js7.data.workflow.{Workflow, WorkflowPath}
+import js7.tests.jobs.SemaphoreJobTest._
+import js7.tests.testenv.ControllerAgentForScalaTest
+import monix.execution.Scheduler.Implicits.global
+import org.scalatest.freespec.AnyFreeSpec
+import scala.concurrent.TimeoutException
+
+final class SemaphoreJobTest extends AnyFreeSpec with ControllerAgentForScalaTest
+{
+  override protected def agentConfig = config"""
+    js7.job.execution.signed-script-injection-allowed = true
+    """
+  protected val agentPaths = Seq(agentPath)
+  protected lazy val items = Seq(workflow)
+
+  "continue and reset" in {
+    assert(TestSemaphoreJob.semaphore.flatMap(_.count).await(99.s) == 0)
+    val orderId = OrderId("ORDER")
+    controllerApi.addOrder(FreshOrder(orderId, workflow.path)).await(99.s).orThrow
+
+    controller.eventWatch.await[OrderProcessingStarted](_.key == orderId)
+    intercept[TimeoutException](
+      controller.eventWatch.await[OrderProcessed](_.key == orderId, timeout = 1.s))
+    assert(TestSemaphoreJob.semaphore.flatMap(_.count).await(99.s) == -1)
+
+    TestSemaphoreJob.continue(1)
+    controller.eventWatch.await[OrderProcessed](_.key == orderId)
+
+    assert(TestSemaphoreJob.semaphore.flatMap(_.count).await(99.s) == 0)
+
+    TestSemaphoreJob.continue(3)
+    assert(TestSemaphoreJob.semaphore.flatMap(_.count).await(99.s) == +3)
+
+    TestSemaphoreJob.reset()
+    assert(TestSemaphoreJob.semaphore.flatMap(_.count).await(99.s) == 0)
+  }
+
+  "reset while job is waiting" in {
+    val orderId = OrderId("ORDER-2")
+    controllerApi.addOrder(FreshOrder(orderId, workflow.path)).await(99.s).orThrow
+
+    controller.eventWatch.await[OrderProcessingStarted](_.key == orderId)
+    intercept[TimeoutException](
+      controller.eventWatch.await[OrderProcessed](_.key == orderId, timeout = 1.s))
+    assert(TestSemaphoreJob.semaphore.flatMap(_.count).await(99.s) == -1)
+
+    TestSemaphoreJob.reset()
+    controller.eventWatch.await[OrderProcessed](_.key == orderId)
+
+    assert(TestSemaphoreJob.semaphore.flatMap(_.count).await(99.s) == 0)
+  }
+}
+
+object SemaphoreJobTest {
+  private val agentPath = AgentPath("AGENT")
+  private val workflow = Workflow(
+    WorkflowPath("WORKFLOW") ~ "INITIAL",
+    Seq(
+      TestSemaphoreJob.execute(agentPath)))
+
+  final class TestSemaphoreJob extends SemaphoreJob(TestSemaphoreJob)
+  object TestSemaphoreJob extends SemaphoreJob.Companion[TestSemaphoreJob]
+}
