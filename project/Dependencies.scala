a340943431dc43923a6f0a2f4ee2a6e4c8881b9b
Joacim Zschimmer <jogit@zschimmer.com>
2022-01-05 07:43:54 +0100

    Log4j 2.17.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies
   val akkaVersion = "2.6.17"
   val akkaHttpVersion = "10.2.7"
   val slf4jVersion = "1.7.32"  // See also plugins.sbt
-  val log4jVersion = "2.17.0"  // See also plugins.sbt
+  val log4jVersion = "2.17.1"  // See also plugins.sbt
   val catsVersion = "2.7.0"
   val catsEffectVersion = "2.5.4"
   val izumiReflectVersion = "2.0.7"
