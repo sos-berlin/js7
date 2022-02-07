8b859ec93589ba5590d23abf78567c207564329c
Joacim Zschimmer <jogit@zschimmer.com>
2022-02-07 09:41:02 +0100

    JS-1972 AtomicUpdater

---
+++ js7-base/shared/src/main/scala/js7/base/utils/AtomicUpdater.scala
@@ -0,0 +1,22 @@
+package js7.base.utils
+
+import monix.execution.atomic.AtomicAny
+import scala.annotation.tailrec
+
+final class AtomicUpdater[A <: AnyRef](initial: A)
+{
+  private val value = AtomicAny(initial)
+
+  def get: A =
+    value.get()
+
+  @tailrec
+  def update(f: A => A): Unit = {
+    val a = value.get()
+    if (!value.compareAndSet(a, f(a)))
+      update(f)
+  }
+
+  def getAndSet(a: A): A =
+    value.getAndSet(a)
+}
