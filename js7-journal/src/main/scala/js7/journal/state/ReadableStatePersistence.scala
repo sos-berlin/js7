ed1f7170fb18c298f8169dbc6f554fec52ca7f2f
Joacim Zschimmer <jogit@zschimmer.com>
2022-02-18 16:13:24 +0100

    ReadableStatePersistence

---
+++ js7-journal/src/main/scala/js7/journal/state/ReadableStatePersistence.scala
@@ -0,0 +1,25 @@
+package js7.journal.state
+
+import js7.data.event.JournaledState
+import js7.journal.watch.EventWatch
+import monix.eval.Task
+
+trait ReadableStatePersistence[S <: JournaledState[S]]
+{
+  protected type State <: JournaledState[State]
+
+  protected val S: JournaledState.Companion[S]
+
+  //TODO def currentState(): S
+  def currentState: S
+
+  val state: Task[S] =
+    Task(currentState)
+
+  final def awaitCurrentState: Task[S] =
+    waitUntilStarted *> Task(currentState)
+
+  def waitUntilStarted: Task[Unit]
+
+  def eventWatch: EventWatch
+}
--- js7-journal/src/main/scala/js7/journal/state/StatePersistence.scala
+++ js7-journal/src/main/scala/js7/journal/state/StatePersistence.scala
@@ -1,9 +1,56 @@
 package js7.journal.state
 
-import js7.data.event.JournaledState
+import js7.base.problem.Checked
+import js7.base.utils.LockKeeper
+import js7.base.utils.ScalaUtils.syntax._
+import js7.data.event.{Event, JournaledState, KeyedEvent, Stamped}
+import js7.journal.CommitOptions
+import monix.eval.Task
 
 trait StatePersistence[S <: JournaledState[S]]
-extends AnyStatePersistence
+extends ReadableStatePersistence[S]
 {
   protected type State = S
+
+  def persistKeyedEvent[E <: Event](
+    keyedEvent: KeyedEvent[E],
+    options: CommitOptions = CommitOptions.default)
+    (implicit enclosing: sourcecode.Enclosing)
+  : Task[Checked[(Stamped[KeyedEvent[E]], S)]]
+
+  final def persistKeyedEvent[E <: Event](
+    keyedEvent: KeyedEvent[E],
+    options: CommitOptions,
+    commitLater: Boolean)
+    (implicit enclosing: sourcecode.Enclosing)
+  : Task[Checked[Unit]] =
+    persistKeyedEvents(keyedEvent :: Nil, options, commitLater = commitLater)
+
+  def persistKeyedEvents[E <: Event](
+    keyedEvents: Seq[KeyedEvent[E]],
+    options: CommitOptions = CommitOptions.default)
+  : Task[Checked[(Seq[Stamped[KeyedEvent[E]]], S)]]
+
+  final def persistKeyedEvents[E <: Event](
+    keyedEvents: Seq[KeyedEvent[E]],
+    options: CommitOptions,
+    commitLater: Boolean)
+  : Task[Checked[Unit]] =
+    if (commitLater)
+      persistKeyedEventsLater(keyedEvents, options)
+    else
+      persistKeyedEvents(keyedEvents, options).rightAs(())
+
+  def persistKeyedEventsLater[E <: Event](
+    keyedEvents: Seq[KeyedEvent[E]],
+    options: CommitOptions = CommitOptions.default)
+  : Task[Checked[Unit]]
+
+  def persist[E <: Event](stateToEvents: S => Checked[Seq[KeyedEvent[E]]])
+  : Task[Checked[(Seq[Stamped[KeyedEvent[E]]], S)]]
+
+  private val keyLockKeeper = new LockKeeper[Any]
+
+  def lock[A](key: Any)(body: Task[A])(implicit enclosing: sourcecode.Enclosing): Task[A] =
+    keyLockKeeper.lock(key)(body)
 }
