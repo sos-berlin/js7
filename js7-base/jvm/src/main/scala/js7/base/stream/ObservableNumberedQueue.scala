b5da3cdc29bc1ea871ca368473da59e0ac7a0414
Joacim Zschimmer <jogit@zschimmer.com>
2022-03-25 07:00:32 +0100

    JS-1954 Refactor ObservableNumberedQueue

--- js7-base/jvm/src/main/scala/js7/base/stream/ObservableNumberedQueue.scala
+++ js7-base/jvm/src/main/scala/js7/base/stream/ObservableNumberedQueue.scala
@@ -21,27 +21,33 @@ import scala.util.{Failure, Success}
 final class ObservableNumberedQueue[V: Tag]
 {
   private val vName = implicitly[Tag[V]].tag.toString
-  private var torn = 0L
-  private var nextNumber = 1L
-  @volatile private var queue = Vector.empty[Numbered[V]]
   private val sync = new IncreasingNumberSync(initial = 0, i => s"#$i")
   private val lock = AsyncLock("ObservableNumberedQueue", suppressLog = true)
+  @volatile private var _state = State()
 
   def enqueue(commands: Iterable[V]): Task[Unit] =
-    lock.lock(
-      Task {
-        var lastNumber = -1L
-        for (command <- commands) {
-          queue :+= Numbered(nextNumber, command)
-          lastNumber = nextNumber
-          nextNumber += 1
-        }
-        lastNumber
-      }.flatMap(syncNumber)) // This separate Task should work as a write barrier for queue (?)
+    lock.lock(Task {
+      val s = _state
+      var queue = s.queue
+      var nextNumber = s.nextNumber
+      var lastNumber = -1L
+      for (command <- commands) {
+        queue :+= Numbered(nextNumber, command)
+        lastNumber = nextNumber
+        nextNumber += 1
+      }
+      _state = s.copy(queue = queue, nextNumber = nextNumber)
+      if (lastNumber != -1) {
+        sync.onAdded(lastNumber)
+      }
+    })
 
   def enqueueNumbered(commands: Iterable[Numbered[V]]): Task[Unit] =
     Task.when(commands.nonEmpty)(
       lock.lock(Task {
+        val s = _state
+        var queue = s.queue
+        var nextNumber = s.nextNumber
         var lastNumber = -1L
         for (command <- commands) {
           assertThat(command.number >= nextNumber)
@@ -49,16 +55,14 @@ final class ObservableNumberedQueue[V: Tag]
           lastNumber = command.number
           nextNumber = command.number + 1
         }
-        lastNumber
-      }.flatMap(syncNumber))) // This separate Task should work as a write barrier for queue (?)
-
-  private def syncNumber(lastNumber: Long): Task[Unit] =
-    Task {
-      if (lastNumber != -1) sync.onAdded(lastNumber)
-    }
+        _state = s.copy(queue = queue, nextNumber = nextNumber)
+        if (lastNumber != -1) {
+          sync.onAdded(lastNumber)
+        }
+      }))
 
   def observable: Observable[Seq[Numbered[V]]] =
-    observable(torn)
+    observable(after = _state.torn)
 
   def observable(after: Long): Observable[Seq[Numbered[V]]] =
     Observable.deferAction { implicit s =>
@@ -66,10 +70,10 @@ final class ObservableNumberedQueue[V: Tag]
 
       def loop(ack: Future[Ack], after: Long): Unit =
         ack.syncTryFlatten.syncOnContinue {
-          checkNumber(after)
+          Task.defer(_state.checkNumber(after))
             // Should we delay some milliseconds to reduce number of command batches ???
             .*>(sync.whenAvailable(after = after, until = None, delay = 0.s))
-            .*>(readQueue(after))
+            .*>(Task.defer(_state.readQueue(after)))
             .runToFuture  // Be sure to leave the `loop` recursion stack
             .onComplete {
               case Failure(t) =>
@@ -78,7 +82,7 @@ final class ObservableNumberedQueue[V: Tag]
               case Success(values) =>
                 if (values.isEmpty) {
                   // Race condition ???
-                  logger.error(
+                  logger.warn(
                     s"Internal: sync.whenAvailable($after) triggered but no command available - delay 1s")
                   Task(loop(Continue, after)).delayExecution(1.s).runAsyncAndForget
                 } else {
@@ -93,49 +97,56 @@ final class ObservableNumberedQueue[V: Tag]
       subject
     }
 
-  private def readQueue(after: Long): Task[Vector[Numbered[V]]] =
-    lock.lock(Task.defer {
-      val q = queue
-      val (index, found) = binarySearch(0, q.length, q(_).number.compare(after))
-      if (!found && after != torn)
-        Task.raiseError(unknownNumberProblem(after).throwable)
-      else
-        Task.pure(q.drop(index + found.toInt))
-    })
-
-  private def checkNumber(after: Long): Task[Unit] =
-    Task.defer {
-      Task.when(after < torn || queue.lastOption.map(_.number).exists(_ < after))(
-        Task.raiseError(unknownNumberProblem(after).throwable))
-    }
-
   private def unknownNumberProblem(after: Long) =
     Problem.pure(s"Unknown Numbered[$vName]: #$after")
 
   def releaseUntil(after: Long): Task[Checked[Unit]] =
     lock.lock(Task {
-      val q = queue
-      if (after < torn)
+      val s = _state
+      val q = s.queue
+      val torn = s.torn
+      if (after < s.torn)
         Left(Problem.pure(s"releaseUntil($after) < torn=$torn ?"))
       else if (after > torn && q.lastOption.exists(_.number < after))
         Left(Problem.pure(
           s"releaseUntil($after) > ${q.lastOption.map(_.number).getOrElse("None")} ?"))
       else {
-        torn = after
         val index = binarySearch(0, q.length, q(_).number.compare(after))._1
-        queue = q.drop(index)
+        _state = s.copy(torn = after, queue = q.drop(index))
         Checked.unit
       }
     })
 
   def dequeueAll: Task[Vector[Numbered[V]]] =
     lock.lock(Task {
-      val result = queue
-      queue = Vector.empty
+      val s = _state
+      val result = s.queue
+      _state = s.copy(queue = Vector.empty)
       result
     })
 
   override def toString = s"ObservableNumberedQueue[$vName]"
+
+  private final case class State(
+    torn: Long = 0L,
+    nextNumber: Long = 1L,
+    queue: Vector[Numbered[V]] = Vector.empty[Numbered[V]])
+  {
+    def readQueue(after: Long): Task[Vector[Numbered[V]]] = {
+      val q = queue
+      val (index, found) = binarySearch(0, q.length, q(_).number.compare(after))
+      if (!found && after != torn)
+        Task.raiseError(unknownNumberProblem(after).throwable)
+      else
+        Task.pure(q.drop(index + found.toInt))
+    }
+
+    def checkNumber(after: Long): Task[Unit] =
+      if (after < torn || queue.lastOption.map(_.number).exists(_ < after))
+        Task.raiseError(unknownNumberProblem(after).throwable)
+      else
+        Task.unit
+  }
 }
 
 object ObservableNumberedQueue
