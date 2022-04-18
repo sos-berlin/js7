327285b297f348c41df5e7a717353c6276aac73e
Joacim Zschimmer <jogit@zschimmer.com>
2022-04-18 20:22:17 +0200

    Latch

---
+++ js7-base/shared/src/main/scala/js7/base/monixutils/Latch.scala
@@ -0,0 +1,44 @@
+package js7.base.monixutils
+
+import monix.catnap.MVar
+import monix.eval.Task
+
+/** A single-use latch, after being closed it can never opened again. */
+final class Latch extends Latch.ReadOnly
+{
+  // Initially open
+  private val latch = MVar[Task].empty[Unit]().memoize
+
+  val is: Task[Boolean] =
+    latch.flatMap(_.isEmpty)
+
+  val isNot: Task[Boolean] =
+    is.map(!_)
+
+  /** Close the latch and return true if not was already closed. */
+  val switch: Task[Boolean] =
+    latch.flatMap(_.tryPut(()))
+
+  /** Close and return `task` iff switch was previously off. */
+  def switchThen[A](task: => Task[A]): Task[Option[A]] =
+    switch.flatMap(hasSwitched =>
+      if (hasSwitched)
+        task.map(Some(_))
+      else
+        Task.none)
+
+  val when: Task[Unit] =
+    latch.flatMap(_.read).void
+}
+
+object Latch
+{
+  trait ReadOnly
+  {
+    def is: Task[Boolean]
+
+    def isNot: Task[Boolean]
+
+    def when: Task[Unit]
+  }
+}
---
+++ js7-base/shared/src/test/scala/js7/base/monixutils/LatchTest.scala
@@ -0,0 +1,64 @@
+package js7.base.monixutils
+
+import js7.base.time.ScalaTime._
+import monix.eval.Task
+import monix.execution.Scheduler.Implicits.global
+import org.scalatest.freespec.AsyncFreeSpec
+import scala.concurrent.Promise
+
+final class LatchTest extends AsyncFreeSpec
+{
+  "Initially isNot" in {
+    val latch = new Latch
+    latch.isNot
+      .map(o => assert(!o))
+      .*>(latch.is)
+      .map(assert(_))
+      .runToFuture
+  }
+
+  "switch" in {
+    val latch = new Latch
+    latch
+      .switch
+      .map(assert(_)) // first call
+      .*>(latch.isNot)
+      .map(assert(_))
+      .*>(latch.is)
+      .map(o => assert(!o))
+      .*>(latch.switch)
+      .map(o => assert(!o)) // second call
+      .*>(latch.isNot)
+      .map(assert(_))
+      .runToFuture
+  }
+
+  "when" in {
+    val latch = new Latch
+    val promise = Promise[Unit]()
+
+    val whenClosed = latch
+      .when
+      .*>(Task { promise.success(()) })
+      .runToFuture
+
+    Task.sleep(100.ms)
+      .map(_ => assert(!promise.isCompleted))
+      .*>(latch.switch)
+      .map(assert(_))
+      .*>(Task.fromFuture(promise.future))
+      .*>(Task.fromFuture(whenClosed).as(succeed))
+    .runToFuture
+  }
+
+  "switchThen" in {
+    val latch = new Latch
+    val promise = Promise[Unit]()
+    latch
+      .switchThen(Task { promise.success(()); 7 })
+      .map(maybe => assert(maybe.contains(7) && promise.isCompleted))
+      .*>(latch.switchThen(Task { promise.success(()) }))
+      .map(maybe => assert(maybe.isEmpty))
+      .runToFuture
+  }
+}
