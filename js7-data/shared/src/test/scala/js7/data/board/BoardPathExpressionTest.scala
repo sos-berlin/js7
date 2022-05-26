cf66025742a090ec77025a03482b4414a363a621
Joacim Zschimmer <jogit@zschimmer.com>
2022-05-26 17:49:03 +0200

    JS-1948 Test for BoardPathExpression.eval

--- js7-data/shared/src/test/scala/js7/data/board/BoardPathExpressionTest.scala
+++ js7-data/shared/src/test/scala/js7/data/board/BoardPathExpressionTest.scala
@@ -1,22 +1,37 @@
 package js7.data.board
 
+import js7.base.utils.ScalaUtils.syntax.RichEither
 import js7.data.board.BoardPathExpression.{And, ExpectNotice, Or}
 import js7.data.board.BoardPathExpressionParser.parse
 import org.scalatest.freespec.AnyFreeSpec
 
 final class BoardPathExpressionTest extends AnyFreeSpec
 {
+  private val A = BoardPath("A")
+  private val B = BoardPath("B")
+  private val C = BoardPath("C")
+
   "A" in {
     assert(parse("'A'") == Right(ExpectNotice(BoardPath("A"))))
   }
 
   "'A' || 'B' && 'C'" in {
-    assert(parse("'A' || 'B' && 'C'") == Right(
+    val expr = parse("'A' || 'B' && 'C'").orThrow
+    assert(expr ==
       Or(
         ExpectNotice(BoardPath("A")),
         And(
           ExpectNotice(BoardPath("B")),
-          ExpectNotice(BoardPath("C"))))))
+          ExpectNotice(BoardPath("C")))))
+
+    assert(!expr.eval(Set.empty))
+    assert(expr.eval(Set(A)))
+    assert(!expr.eval(Set(B)))
+    assert(expr.eval(Set(A, B)))
+    assert(!expr.eval(Set(C)))
+    assert(expr.eval(Set(C, A)))
+    assert(expr.eval(Set(C, B)))
+    assert(expr.eval(Set(C, A, B)))
   }
 
   "'A'||'B'&&'C'" in {
