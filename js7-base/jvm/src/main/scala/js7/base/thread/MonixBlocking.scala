cb2b0e1eea19d78b4eeaa70b9910c780f126a71a
Joacim Zschimmer <jogit@zschimmer.com>
2022-06-08 17:13:37 +0200

    .await shows callers stack trace

--- js7-base/jvm/src/main/scala/js7/base/thread/MonixBlocking.scala
+++ js7-base/jvm/src/main/scala/js7/base/thread/MonixBlocking.scala
@@ -1,11 +1,13 @@
 package js7.base.thread
 
 import js7.base.thread.Futures.implicits._
+import js7.base.utils.StackTraces.StackTraceThrowable
 import monix.eval.Task
 import monix.execution.Scheduler
 import scala.collection.BuildFrom
 import scala.concurrent.duration._
 import scala.reflect.runtime.universe._
+import scala.util.control.NonFatal
 
 /**
   * @author Joacim Zschimmer
@@ -15,13 +17,20 @@ object MonixBlocking
   object syntax {
     implicit class RichTask[A](private val underlying: Task[A]) extends AnyVal
     {
-      def await(duration: FiniteDuration)(implicit s: Scheduler): A =
-        underlying
-          .runSyncStep
-          .fold(_.runSyncUnsafe(duration), identity)
+      def await(duration: Duration)(implicit s: Scheduler): A =
+        try
+          underlying
+            .runSyncStep
+            .fold(_.runSyncUnsafe(duration), identity)
+        catch { case NonFatal(t) =>
+          if (t.getStackTrace.forall(_.getClassName != getClass.getName)) {
+            t.appendCurrentStackTrace
+          }
+          throw t
+        }
 
       def awaitInfinite(implicit s: Scheduler): A =
-        underlying.runToFuture.awaitInfinite
+        await(Duration.Inf)
     }
 
     implicit final class RichTaskTraversable[A, M[X] <: Iterable[X]](private val underlying: M[Task[A]]) extends AnyVal
