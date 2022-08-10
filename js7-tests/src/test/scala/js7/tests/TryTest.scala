c85ea60f6ea227309bd645cabcff70a00f2b17f5
Joacim Zschimmer <jogit@zschimmer.com>
2022-08-10 09:19:51 +0200

    JS-2005 Refactor TryTest

--- js7-tests/src/test/scala/js7/tests/TryTest.scala
+++ js7-tests/src/test/scala/js7/tests/TryTest.scala
@@ -1,104 +1,168 @@
 package js7.tests
 
+import js7.base.configutils.Configs.HoconStringInterpolator
 import js7.base.io.process.Processes.ShellFileExtension as sh
 import js7.base.problem.Checked.Ops
 import js7.base.system.OperatingSystem.isWindows
-import js7.base.utils.AutoClosing.autoClosing
 import js7.data.agent.AgentPath
 import js7.data.event.KeyedEvent
 import js7.data.job.RelativePathExecutable
 import js7.data.order.OrderEvent.{OrderAdded, OrderAttachable, OrderAttached, OrderCatched, OrderDetachable, OrderDetached, OrderFailed, OrderFailedInFork, OrderFinished, OrderForked, OrderJoined, OrderMoved, OrderProcessed, OrderProcessingStarted, OrderStarted, OrderTerminated}
 import js7.data.order.{FreshOrder, OrderEvent, OrderId, Outcome}
 import js7.data.value.NamedValues
+import js7.data.value.expression.ExpressionParser.expr
+import js7.data.workflow.instructions.{Fail, If, TryInstruction}
 import js7.data.workflow.position.BranchId.{Then, try_}
 import js7.data.workflow.position.{BranchId, Position}
-import js7.data.workflow.{WorkflowParser, WorkflowPath}
+import js7.data.workflow.{Workflow, WorkflowParser, WorkflowPath}
+import js7.tests.jobs.EmptyJob
 import js7.tests.TryTest.*
-import js7.tests.testenv.DirectoryProvider
 import js7.tests.testenv.DirectoryProvider.toLocalSubagentId
+import js7.tests.testenv.{ControllerAgentForScalaTest, TestItemUpdater}
 import monix.execution.Scheduler.Implicits.traced
+import org.scalactic.source
 import org.scalatest.freespec.AnyFreeSpec
 
-final class TryTest extends AnyFreeSpec
+final class TryTest
+extends AnyFreeSpec
+with ControllerAgentForScalaTest
+with TestItemUpdater
 {
-  "Nested try catch with outer non-failing catch, OrderFinished" in {
-    autoClosing(new DirectoryProvider(TestAgentPath :: Nil, FinishingWorkflow :: Nil, testName = Some("TryTest"))) { directoryProvider =>
-      for (a <- directoryProvider.agents) {
-        a.writeExecutable(RelativePathExecutable(s"OKAY$sh"), ":")
-        a.writeExecutable(RelativePathExecutable(s"FAIL-1$sh"), if (isWindows) "@exit 1" else "exit 1")
-        a.writeExecutable(RelativePathExecutable(s"FAIL-2$sh"), if (isWindows) "@exit 2" else "exit 2")
-      }
-      directoryProvider.run { (controller, _) =>
-        val orderId = OrderId("🔺")
-        controller.addOrderBlocking(FreshOrder(orderId, FinishingWorkflow.id.path))
-        controller.eventWatch.await[OrderFinished](_.key == orderId)
-        checkEventSeq(orderId, controller.eventWatch.allKeyedEvents[OrderEvent], ExpectedFinishedEvents)
-      }
+  override protected val controllerConfig = config"""
+    js7.auth.users.TEST-USER.permissions = [ UpdateItem ]
+    js7.journal.remove-obsolete-files = false
+    js7.controller.agent-driver.command-batch-delay = 0ms
+    js7.controller.agent-driver.event-buffer-delay = 0ms"""
+
+  override protected def agentConfig = config"""
+    js7.job.execution.signed-script-injection-allowed = on
+    js7.order.stdout-stderr.delay = 1ms"""
+
+  protected val agentPaths = Seq(agentPath)
+  protected val items = Nil
+
+  "(prepare)" in {
+    for (a <- directoryProvider.agents) {
+      a.writeExecutable(RelativePathExecutable(s"OKAY$sh"), ":")
+      a.writeExecutable(RelativePathExecutable(s"FAIL-1$sh"), if (isWindows) "@exit 1" else "exit 1")
+      a.writeExecutable(RelativePathExecutable(s"FAIL-2$sh"), if (isWindows) "@exit 2" else "exit 2")
+      a.writeExecutable(RelativePathExecutable(s"NEVER$sh"), if (isWindows) "@exit 3" else "exit 3")
     }
+  }
+
+  "Nested try catch with outer non-failing catch, OrderFinished" in {
+    val finishingScript =
+      s"""
+          |define workflow {
+          |  try {                                                // :0
+          |    try {                                              // :0/try:0
+          |      execute executable="FAIL-1$sh", agent="AGENT";   // :0/try:0/try:0   OrderCatched
+          |      execute executable="OKAY$sh", agent="AGENT";     // :0/try:0/try:1   skipped
+          |    } catch {
+          |      execute executable="FAIL-2$sh", agent="AGENT";   // :0/try:0/catch:0   OrderCatched
+          |    }
+          |    execute executable="OKAY$sh", agent="AGENT";       // :0/try:1
+          |  } catch {}
+          |  execute executable="OKAY$sh", agent="AGENT";         // :1
+          |}""".stripMargin
+    val workflow = WorkflowParser.parse(WorkflowPath("FINISHING"), finishingScript).orThrow
+    val Some(v) = updateItems(workflow)
+
+    val orderId = OrderId("🔺")
+    controller.addOrderBlocking(FreshOrder(orderId, workflow.path))
+    controller.eventWatch.await[OrderFinished](_.key == orderId)
+    checkEventSeq(orderId, controller.eventWatch.allKeyedEvents[OrderEvent], Vector(
+      OrderAdded(workflow.path ~ v),
+      OrderMoved(Position(0) / "try+0" % 0 / "try+0" % 0),
+      OrderAttachable(agentPath),
+      OrderAttached(agentPath),
+
+      OrderStarted,
+      OrderProcessingStarted(subagentId),
+      OrderProcessed(Outcome.Failed.rc(1)),
+      OrderCatched(Position(0) / "try+0" % 0 / "catch+0" % 0),
+
+      OrderProcessingStarted(subagentId),
+      OrderProcessed(Outcome.Failed.rc(2)),
+      OrderCatched(Position(0) / "catch+0" % 0),
+      OrderMoved(Position(1)),
+
+      OrderProcessingStarted(subagentId),
+      OrderProcessed(Outcome.Succeeded.rc(0)),
+      OrderMoved(Position(2)),
+
+      OrderDetachable,
+      OrderDetached,
+      OrderFinished))
+    assert(controllerState.idToOrder(orderId).lastOutcome == Outcome.succeededRC0)
   }
 
   "Nested try catch with failing catch, OrderFailed" in {
-    autoClosing(new DirectoryProvider(TestAgentPath :: Nil, StoppingWorkflow :: Nil, testName = Some("TryTest"))) { directoryProvider =>
-      for (a <- directoryProvider.agents) {
-        a.writeExecutable(RelativePathExecutable(s"FAIL-1$sh"), if (isWindows) "@exit 1" else "exit 1")
-        a.writeExecutable(RelativePathExecutable(s"FAIL-2$sh"), if (isWindows) "@exit 2" else "exit 2")
-      }
-      directoryProvider.run { (controller, _) =>
-        val orderId = OrderId("❌")
-        controller.addOrderBlocking(FreshOrder(orderId, StoppingWorkflow.id.path))
-        controller.eventWatch.await[OrderFailed](_.key == orderId)
-        checkEventSeq(orderId, controller.eventWatch.allKeyedEvents[OrderEvent], ExpectedStoppedEvent)
-      }
-    }
+    val workflowPath = WorkflowPath("STOPPING")
+    val Some(v) = updateItems(Workflow(
+      workflowPath,
+      Seq(
+        TryInstruction(
+          tryWorkflow = Workflow.of(Fail()),
+          catchWorkflow = Workflow.of(Fail())))))
+
+    val orderId = OrderId("❌")
+    controller.addOrderBlocking(FreshOrder(orderId, workflowPath))
+    controller.eventWatch.await[OrderFailed](_.key == orderId)
+    checkEventSeq(
+      orderId,
+      controller.eventWatch.allKeyedEvents[OrderEvent],
+      Vector(
+        OrderAdded(workflowPath ~ v),
+        OrderMoved(Position(0) / "try+0" % 0),
+        OrderStarted,
+        OrderCatched(Position(0) / "catch+0" % 0, Some(Outcome.failed)),
+        OrderFailed(Position(0) / "catch+0" % 0, Some(Outcome.failed))))
+    assert(controllerState.idToOrder(orderId).lastOutcome == Outcome.failed)
   }
 
   "try - if - fail" in {
-    val workflow = WorkflowParser.parse(WorkflowPath("TRY-IF") ~ "INITIAL",
-      s"""define workflow {
-         |  try {
-         |    execute executable="OKAY$sh", agent="AGENT";
-         |    if (true) {
-         |      fail;
-         |    }
-         |  } catch {
-         |    execute executable="OKAY$sh", agent="AGENT";
-         |  }
-         |  execute executable="OKAY$sh", agent="AGENT";
-         |}""".stripMargin).orThrow
-    autoClosing(new DirectoryProvider(TestAgentPath :: Nil, workflow :: Nil, testName = Some("TryTest"))) { directoryProvider =>
-      for (a <- directoryProvider.agents) {
-        a.writeExecutable(RelativePathExecutable(s"OKAY$sh"), ":")
-        a.writeExecutable(RelativePathExecutable(s"FAIL$sh"), if (isWindows) "@exit 1" else "exit 1")
-      }
-      directoryProvider.run { (controller, _) =>
-        val orderId = OrderId("⭕")
-        controller.addOrderBlocking(FreshOrder(orderId, workflow.id.path))
-        controller.eventWatch.await[OrderFinished](_.key == orderId)
-        checkEventSeq(orderId, controller.eventWatch.allKeyedEvents[OrderEvent], Vector(
-          OrderAdded(workflow.id),
-          OrderMoved(Position(0) / try_(0) % 0),
-          OrderAttachable(TestAgentPath),
-          OrderAttached(TestAgentPath),
-          OrderStarted,
-          OrderProcessingStarted(subagentId),
-          OrderProcessed(Outcome.Succeeded(NamedValues.rc(0))),
-          OrderMoved(Position(0) / try_(0) % 1 / Then % 0),
-          OrderCatched(Position(0) / "catch+0" % 0, Some(Outcome.failed)),
-          OrderProcessingStarted(subagentId),
-          OrderProcessed(Outcome.Succeeded(NamedValues.rc(0))),
-          OrderMoved(Position(1)),
-          OrderProcessingStarted(subagentId),
-          OrderProcessed(Outcome.Succeeded(NamedValues.rc(0))),
-          OrderMoved(Position(2)),
-          OrderDetachable,
-          OrderDetached,
-          OrderFinished))
-      }
-    }
+    val workflow = Workflow(
+      WorkflowPath("TRY-IF"),
+      Seq(
+        TryInstruction(
+          tryWorkflow = Workflow.of(
+            EmptyJob.execute(agentPath),
+            If(expr("true"),
+              Workflow.of(Fail()))),
+          catchWorkflow = Workflow.of(
+            EmptyJob.execute(agentPath))),
+        EmptyJob.execute(agentPath)))
+    val Some(v) = updateItems(workflow)
+
+    val orderId = OrderId("⭕")
+    controller.addOrderBlocking(FreshOrder(orderId, workflow.path))
+    controller.eventWatch.await[OrderFinished](_.key == orderId)
+
+    checkEventSeq(orderId, controller.eventWatch.allKeyedEvents[OrderEvent], Vector(
+      OrderAdded(workflow.path ~ v),
+      OrderMoved(Position(0) / try_(0) % 0),
+      OrderAttachable(agentPath),
+      OrderAttached(agentPath),
+      OrderStarted,
+      OrderProcessingStarted(subagentId),
+      OrderProcessed(Outcome.succeeded),
+      OrderMoved(Position(0) / try_(0) % 1 / Then % 0),
+      OrderCatched(Position(0) / "catch+0" % 0, Some(Outcome.failed)),
+      OrderProcessingStarted(subagentId),
+      OrderProcessed(Outcome.succeeded),
+      OrderMoved(Position(1)),
+      OrderProcessingStarted(subagentId),
+      OrderProcessed(Outcome.succeeded),
+      OrderMoved(Position(2)),
+      OrderDetachable,
+      OrderDetached,
+      OrderFinished))
+    assert(controllerState.idToOrder(orderId).lastOutcome == Outcome.succeeded)
   }
 
   "fork - fail" in {
-    val workflow = WorkflowParser.parse(WorkflowPath("TRY-IF") ~ "INITIAL",
+    val workflow = WorkflowParser.parse(WorkflowPath("FORK-FAIL"),
       s"""define workflow {
          |  try {
          |    fork (joinIfFailed=true) {
@@ -111,53 +175,47 @@ final class TryTest extends AnyFreeSpec
          |    execute executable="OKAY$sh", agent="AGENT";
          |  }
          |}""".stripMargin).orThrow
-    autoClosing(new DirectoryProvider(TestAgentPath :: Nil, workflow :: Nil, testName = Some("TryTest"))) { directoryProvider =>
-      for (a <- directoryProvider.agents) {
-        a.writeExecutable(RelativePathExecutable(s"OKAY$sh"), ":")
-        a.writeExecutable(RelativePathExecutable(s"FAIL-1$sh"), if (isWindows) "@exit 1" else "exit 1")
-        a.writeExecutable(RelativePathExecutable(s"FAIL-2$sh"), if (isWindows) "@exit 2" else "exit 2")
-        a.writeExecutable(RelativePathExecutable(s"NEVER$sh"), if (isWindows) "@exit 3" else "exit 3")
-      }
-      directoryProvider.run { (controller, _) =>
-        val orderId = OrderId("🔴")
-        controller.addOrderBlocking(FreshOrder(orderId, workflow.id.path))
-        controller.eventWatch.await[OrderTerminated](_.key == orderId)
-        checkEventSeq(orderId, controller.eventWatch.allKeyedEvents[OrderEvent], Vector(
-          OrderAdded(workflow.id),
-          OrderMoved(Position(0) / "try+0" % 0),
-          OrderStarted,
-          OrderAttachable(TestAgentPath),
-          OrderAttached(TestAgentPath),
-          OrderForked(Vector(
-            OrderForked.Child("🥕", OrderId("🔴|🥕")),
-            OrderForked.Child("🍋", OrderId("🔴|🍋")),
-            OrderForked.Child("🌶", OrderId("🔴|🌶")))),
-          OrderDetachable,
-          OrderDetached,
-          OrderJoined(Outcome.Failed(Some("Order:🔴|🍋 failed;\nOrder:🔴|🌶 failed"))),
-          OrderCatched(Position(0) / "catch+0" % 0),
-          OrderAttachable(TestAgentPath),
-          OrderAttached(TestAgentPath),
-          OrderProcessingStarted(subagentId),
-          OrderProcessed(Outcome.Succeeded(NamedValues.rc(0))),
-          OrderMoved(Position(1)),
-          OrderDetachable,
-          OrderDetached,
-          OrderFinished))
-        checkEventSeq(OrderId("🔴|🍋"), controller.eventWatch.allKeyedEvents[OrderEvent], Vector(
-          OrderProcessingStarted(subagentId),
-          OrderProcessed(Outcome.Failed(None, NamedValues.rc(1))),
-          OrderDetachable,
-          OrderDetached,
-          OrderFailedInFork(Position(0) / BranchId.try_(0) % 0 / BranchId.fork("🍋") % 0)))
-      }
-    }
+    val Some(v) = updateItems(workflow)
+
+    val orderId = OrderId("🔴")
+    controller.addOrderBlocking(FreshOrder(orderId, workflow.path))
+    controller.eventWatch.await[OrderTerminated](_.key == orderId)
+    checkEventSeq(orderId, controller.eventWatch.allKeyedEvents[OrderEvent], Vector(
+      OrderAdded(workflow.path ~ v),
+      OrderMoved(Position(0) / "try+0" % 0),
+      OrderStarted,
+      OrderAttachable(agentPath),
+      OrderAttached(agentPath),
+      OrderForked(Vector(
+        OrderForked.Child("🥕", OrderId("🔴|🥕")),
+        OrderForked.Child("🍋", OrderId("🔴|🍋")),
+        OrderForked.Child("🌶", OrderId("🔴|🌶")))),
+      OrderDetachable,
+      OrderDetached,
+      OrderJoined(Outcome.Failed(Some("Order:🔴|🍋 failed;\nOrder:🔴|🌶 failed"))),
+      OrderCatched(Position(0) / "catch+0" % 0),
+      OrderAttachable(agentPath),
+      OrderAttached(agentPath),
+      OrderProcessingStarted(subagentId),
+      OrderProcessed(Outcome.Succeeded(NamedValues.rc(0))),
+      OrderMoved(Position(1)),
+      OrderDetachable,
+      OrderDetached,
+      OrderFinished))
+    checkEventSeq(OrderId("🔴|🍋"), controller.eventWatch.allKeyedEvents[OrderEvent], Vector(
+      OrderProcessingStarted(subagentId),
+      OrderProcessed(Outcome.Failed(None, NamedValues.rc(1))),
+      OrderDetachable,
+      OrderDetached,
+      OrderFailedInFork(Position(0) / BranchId.try_(0) % 0 / BranchId.fork("🍋") % 0)))
+    assert(controllerState.idToOrder(orderId).lastOutcome == Outcome.succeededRC0)
   }
 
   private def checkEventSeq(
     orderId: OrderId,
     keyedEvents: IterableOnce[KeyedEvent[OrderEvent]],
     expected: Vector[OrderEvent])
+    (implicit pos: source.Position)
   : Unit = {
       val events = keyedEvents.iterator.filter(_.key == orderId).map(_.event).toVector
       assert(events == expected)
@@ -166,71 +224,6 @@ final class TryTest extends AnyFreeSpec
 
 object TryTest
 {
-  private val TestAgentPath = AgentPath("AGENT")
-  private val subagentId = toLocalSubagentId(TestAgentPath)
-  private val finishingScript = s"""
-     |define workflow {
-     |  try {                                                // :0
-     |    try {                                              // :0/try:0
-     |      execute executable="FAIL-1$sh", agent="AGENT";   // :0/try:0/try:0   OrderCatched
-     |      execute executable="OKAY$sh", agent="AGENT";     // :0/try:0/try:1   skipped
-     |    } catch {
-     |      execute executable="FAIL-2$sh", agent="AGENT";   // :0/try:0/catch:0   OrderCatched
-     |    }
-     |    execute executable="OKAY$sh", agent="AGENT";       // :0/try:1
-     |  } catch {}
-     |  execute executable="OKAY$sh", agent="AGENT";         // :1
-     |}""".stripMargin
-  private val FinishingWorkflow = WorkflowParser.parse(WorkflowPath("FINISHING") ~ "INITIAL", finishingScript).orThrow
-
-  private val ExpectedFinishedEvents = Vector(
-    OrderAdded(FinishingWorkflow.id),
-    OrderMoved(Position(0) / "try+0" % 0 / "try+0" % 0),
-    OrderAttachable(TestAgentPath),
-    OrderAttached(TestAgentPath),
-
-    OrderStarted,
-    OrderProcessingStarted(subagentId),
-    OrderProcessed(Outcome.Failed.rc(1)),
-    OrderCatched(Position(0) / "try+0" % 0 / "catch+0" % 0),
-
-    OrderProcessingStarted(subagentId),
-    OrderProcessed(Outcome.Failed.rc(2)),
-    OrderCatched(Position(0) / "catch+0" % 0),
-    OrderMoved(Position(1)),
-
-    OrderProcessingStarted(subagentId),
-    OrderProcessed(Outcome.Succeeded.rc(0)),
-    OrderMoved(Position(2)),
-
-    OrderDetachable,
-    OrderDetached,
-    OrderFinished)
-
-  private val stoppingScript = s"""
-     |define workflow {
-     |  try {                                              // :0
-     |    execute executable="FAIL-1$sh", agent="AGENT";   // :0/try:0  OrderCatched
-     |  } catch {
-     |    execute executable="FAIL-2$sh", agent="AGENT";   // :0/catch:0  OrderFailed
-     |  }
-     |}""".stripMargin
-  private val StoppingWorkflow = WorkflowParser.parse(WorkflowPath("STOPPING") ~ "INITIAL", stoppingScript).orThrow
-
-  private val ExpectedStoppedEvent = Vector(
-    OrderAdded(StoppingWorkflow.id),
-    OrderMoved(Position(0) / "try+0" % 0),
-    OrderAttachable(TestAgentPath),
-    OrderAttached(TestAgentPath),
-
-    OrderStarted,
-    OrderProcessingStarted(subagentId),
-    OrderProcessed(Outcome.Failed.rc(1)),
-    OrderCatched(Position(0) / "catch+0" % 0),
-
-    OrderProcessingStarted(subagentId),
-    OrderProcessed(Outcome.Failed.rc(2)),
-    OrderDetachable,
-    OrderDetached,
-    OrderFailed(Position(0) / "catch+0" % 0))
+  private val agentPath = AgentPath("AGENT")
+  private val subagentId = toLocalSubagentId(agentPath)
 }
