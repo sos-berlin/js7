171a60a74e7ba275a7e5ffe87d3d16ca39420ad4
Joacim Zschimmer <jogit@zschimmer.com>
2022-12-01 09:23:51 +0100

    VirtualThreads

--- js7-base/jvm/src/main/scala/js7/base/thread/VirtualThreads.scala
+++ js7-base/jvm/src/main/scala/js7/base/thread/VirtualThreads.scala
@@ -5,36 +5,19 @@ import js7.base.log.Logger
 import js7.base.system.Java8Polyfill.javaVersion
 import js7.base.utils.ScalaUtils.syntax.*
 import js7.base.utils.Tests.isTest
-import monix.execution.atomic.Atomic
 import org.jetbrains.annotations.TestOnly
 
 object VirtualThreads
 {
   private val logger = Logger(getClass)
   private var enabled = javaVersion >= 19 && (isTest || sys.props.contains("js7.VirtualThread"))
-  private val successLogged = Atomic(false)
 
   private lazy val maybeNewVirtualThreadPerTaskExecutor: Option[() => ExecutorService] =
-    if (!enabled) {
-      None
-    } else
-      try {
-        val method = classOf[Executors].getMethod("newVirtualThreadPerTaskExecutor")
-        def invoke() = method.invoke(null).asInstanceOf[ExecutorService]
-        proveUsability(invoke())
-        Some(invoke _)
-      } catch throwableToNone
-
-  // May throw
-  private def proveUsability(executor: ExecutorService): Unit = {
-    executor.execute { () =>
-      if (!successLogged.getAndSet(true)) {
-        logger.debug(
-          s"""Using Java 19 VirtualThreads for some operations "${Thread.currentThread}"""")
-      }
-    }
-    executor.shutdown()
-  }
+    for {
+      factory <- newVirtualThreadFactory
+      newThreadPerTaskExecutor <- newThreadPerTaskExecutor
+    } yield
+      () => newThreadPerTaskExecutor(factory)
 
   @TestOnly
   def isEnabled =
@@ -44,30 +27,63 @@ object VirtualThreads
     maybeNewVirtualThreadPerTaskExecutor.map(_())
 
   lazy val newMaybeVirtualThread: Runnable => Thread =
+    newVirtualThreadFactory match {
+      case Some(factory) => factory.newThread(_)
+      case None => new Thread(_)
+    }
+
+  private lazy val newThreadPerTaskExecutor: Option[ThreadFactory => ExecutorService] =
     if (!enabled)
-      new Thread(_)
+      None
+    else
+      try {
+        val method = classOf[Executors]
+          .getMethod("newThreadPerTaskExecutor", classOf[ThreadFactory])
+        Some(
+          threadFactory =>
+            method.invoke(null, threadFactory).asInstanceOf[ExecutorService])
+      } catch throwableToNone
+
+  private lazy val newVirtualThreadFactory: Option[ThreadFactory] =
+    if (!enabled)
+      None
     else
       try {
         val builder = classOf[Thread].getMethod("ofVirtual").invoke(null)
+
+        //IllegalAccessException:
+        //try builder.getClass
+        //  .getMethod("name", classOf[String])
+        //  .invoke(builder, "virtual")
+        //  .asInstanceOf[ThreadFactory]
+        //catch { case t: Throwable => logger.debug(t.toStringWithCauses)}
+
         val factory = Class
-          .forName("java.lang.Thread.Builder")
+          .forName("java.lang.Thread$Builder")
           .getMethod("factory")
           .invoke(builder)
           .asInstanceOf[ThreadFactory]
 
-        // Probe
-        factory.newThread(() => {})
+        testThreadFactory(factory)
+        Some(factory)
+      } catch(throwableToNone)
 
-        factory.newThread(_)
-      } catch { case t: Throwable =>
-        throwableToNone(t)
-        new Thread(_)
-      }
+  private def testThreadFactory(factory: ThreadFactory): Unit = {
+    val newThread: Runnable => Thread = runnable =>
+      factory.newThread(runnable)
+
+    val testThread = newThread { () =>
+      logger.debug(
+        s"""Using Java 19 VirtualThreads for some operations "${Thread.currentThread}"""")
+    }
+    testThread.start()
+    testThread.join()
+  }
 
   private def throwableToNone: PartialFunction[Throwable, None.type] = {
     throwable =>
       enabled = false
-      logger.debug(s"${throwable.toStringWithCauses}")
+      logger.debug(s"No VirtualThread: ${throwable.toStringWithCauses}")
       None
   }
 }
