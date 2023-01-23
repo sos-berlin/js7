c6f1bc09a4e76a2f9e431c0fd74bf5dac79d4f61
Joacim Zschimmer <jogit@zschimmer.com>
2023-01-23 07:56:04 +0100

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies
   val fastparseVersion = "2.3.3"
   val circeVersion = "0.14.3"
   val circeGenericExtrasVersion = "0.14.1"
-  val scalaTestVersion = "3.2.14"
+  val scalaTestVersion = "3.2.15"
   val scalaTestCheckVersion = "3.2.14.0"
   val scalaCheckVersion= "1.17.0"
   val sourcecodeVersion = "0.3.0"
@@ -32,7 +32,7 @@ object Dependencies
   val scalaJsDomVersion = "2.2.0"
   val softwaremillTaggingVersion = "2.3.4"
   val diffxVersion = "0.8.2"
-  val reactorVersion = "3.4.24"
+  val reactorVersion = "3.5.2"
   val vavrVersion = "0.10.4"
   val jnaVersion = "5.12.1"
 
