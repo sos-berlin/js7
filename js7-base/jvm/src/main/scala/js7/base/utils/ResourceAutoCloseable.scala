d06758ff9cc064b5cf5a0f0681fc4e61eb81c5dc
Joacim Zschimmer <jogit@zschimmer.com>
2023-03-02 12:06:32 +0100

    ResourceAutoCloseable closes only once

--- js7-base/jvm/src/main/scala/js7/base/utils/ResourceAutoCloseable.scala
+++ js7-base/jvm/src/main/scala/js7/base/utils/ResourceAutoCloseable.scala
@@ -4,6 +4,7 @@ import cats.effect.Resource
 import js7.base.thread.MonixBlocking.syntax.*
 import monix.eval.Task
 import monix.execution.Scheduler
+import monix.execution.atomic.Atomic
 import scala.concurrent.duration.*
 
 final case class ResourceAutoCloseable[A](resource: Resource[Task, A], timeout: Duration)
@@ -11,10 +12,13 @@ final case class ResourceAutoCloseable[A](resource: Resource[Task, A], timeout: 
   extends AutoCloseable
 {
   private lazy val (value_, release) = resource.allocated.await(timeout)
+  private val closed = Atomic(false)
 
   def value: A =
     value_
 
   def close() =
-    release.await(timeout)
+    if (!closed.getAndSet(true)) {
+      release.await(timeout)
+    }
 }
