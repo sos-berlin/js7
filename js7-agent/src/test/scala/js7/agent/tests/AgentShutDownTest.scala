a1e94281a3d1dc43a397e4ce460c71aa96e849ff
Joacim Zschimmer <jogit@zschimmer.com>
2023-03-10 09:18:23 +0100

    Refactor AgentShutDownTest

--- js7-agent/src/test/scala/js7/agent/tests/AgentShutDownTest.scala
+++ js7-agent/src/test/scala/js7/agent/tests/AgentShutDownTest.scala
@@ -1,6 +1,5 @@
 package js7.agent.tests
 
-import akka.actor.ActorSystem
 import js7.agent.TestAgent
 import js7.agent.client.AgentClient
 import js7.agent.configuration.AgentConfiguration
@@ -14,8 +13,9 @@ import js7.base.problem.Checked.Ops
 import js7.base.test.OurTestSuite
 import js7.base.thread.MonixBlocking.syntax.*
 import js7.base.time.ScalaTime.*
+import js7.base.utils.CatsBlocking.BlockingTaskResource
 import js7.common.akkautils.Akkas
-import js7.common.akkautils.Akkas.newActorSystem
+import js7.common.akkautils.Akkas.actorSystemResource
 import js7.common.system.ServerOperatingSystem.operatingSystem
 import js7.data.agent.AgentPath
 import js7.data.controller.ControllerId
@@ -45,38 +45,40 @@ final class AgentShutDownTest extends OurTestSuite with BeforeAndAfterAll with T
     val orderIds = for (i <- 0 until 3) yield OrderId(s"TEST-ORDER-$i")
 
     TestAgent.blockingRun(agentConfiguration,  99.s) { agent =>
-      implicit val actorSystem: ActorSystem = newActorSystem("AgentShutDownTest")
-      val userId = UserId("TEST-USER")
-      closer onClose Akkas.terminateAndWait(actorSystem, 10.s)
+      actorSystemResource("AgentShutDownTest").blockingUse(99.s) { implicit actorSystem =>
+        val userId = UserId("TEST-USER")
+        closer onClose Akkas.terminateAndWait(actorSystem, 10.s)
 
-      val client = AgentClient(agentUri = agent.localUri, Some(userId -> SecretString("TEST-PASSWORD")))
-      client.login() await 99.s
-      client
-        .commandExecute(DedicateAgentDirector(Some(SubagentId("SUBAGENT")), controllerId, agentPath))
-        .await(99.s)
+        val client = AgentClient(agentUri = agent.localUri,
+          Some(userId -> SecretString("TEST-PASSWORD")))
+        client.login() await 99.s
+        client
+          .commandExecute(DedicateAgentDirector(
+            Some(SubagentId("SUBAGENT")), controllerId, agentPath))
+          .await(99.s)
 
-      val subagentId = SubagentId("SUBAGENT")
-      client.commandExecute(AttachItem(SubagentItem(subagentId, agentPath, agent.localUri)))
-        .await(99.s).orThrow
-      client.commandExecute(AttachSignedItem(itemSigner.sign(SimpleTestWorkflow)))
-        .await(99.s).orThrow
+        val subagentId = SubagentId("SUBAGENT")
+        client.commandExecute(AttachItem(SubagentItem(subagentId, agentPath, agent.localUri)))
+          .await(99.s).orThrow
+        client.commandExecute(AttachSignedItem(itemSigner.sign(SimpleTestWorkflow)))
+          .await(99.s).orThrow
 
-      (for (orderId <- orderIds) yield
-        client.commandExecute(AttachOrder(
-          Order(
-            orderId,
-            SimpleTestWorkflow.id /: Position(0),
-            Order.Ready,
-            Map("a" -> StringValue("A"))),
-          TestAgentPath))
-      ) await 99.s
+        (for (orderId <- orderIds) yield
+          client.commandExecute(AttachOrder(
+            Order(
+              orderId,
+              SimpleTestWorkflow.id /: Position(0),
+              Order.Ready,
+              Map("a" -> StringValue("A"))),
+            TestAgentPath))
+        ) await 99.s
 
-      sleep(2.s)
+        sleep(2.s)
 
-      client.commandExecute(ShutDown(Some(SIGKILL))).await(99.s).orThrow
-      agent.untilTerminated.await(99.s)
-      client.close()
-      Akkas.terminate(actorSystem).runAsyncAndForget // Blocking ???
+        client.commandExecute(ShutDown(Some(SIGKILL))).await(99.s).orThrow
+        agent.untilTerminated.await(99.s)
+        client.close()
+      }
     }
 
     TestAgent.blockingRun(agentConfiguration,  99.s) { agent =>
@@ -84,7 +86,6 @@ final class AgentShutDownTest extends OurTestSuite with BeforeAndAfterAll with T
         val processed = agent.eventWatch.await[OrderProcessed](_.key == orderId)
         assert(processed.head.value.event.outcome.isInstanceOf[Outcome.Killed])
       }
-      agent.terminate().await(99.s)
     }
   }
 }
