a2751ac2e8abaec9f851e56474925f93ebd8b484
Joacim Zschimmer <jogit@zschimmer.com>
2023-03-20 17:03:04 +0100

    Guava 31.1-jre

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -55,7 +55,8 @@ object Dependencies
   val diffxScalaTest      = "com.softwaremill.diffx" %% "diffx-scalatest-should" % diffxVersion
 
   val javaxInject         = "javax.inject" % "javax.inject" % "1"
-  val guice               = ("com.google.inject" % "guice" % "5.1.0") :: javaxInject :: Nil
+  val guava               = "com.google.guava" % "guava" % "31.1-jre"
+  val guice               = ("com.google.inject" % "guice" % "5.1.0") :: javaxInject :: guava :: Nil
 
   val typesafeConfig      = "com.typesafe" % "config" % "1.4.2"
 
@@ -79,7 +80,6 @@ object Dependencies
   val intelliJAnnotations = "com.intellij" % "annotations" % "12.0"
 
   val findbugs            = "com.google.code.findbugs" % "jsr305" % "3.0.2"
-  //val guava               = "com.google.guava" % "guava" % "30.1.1-jre"
   val bouncyCastle        = "org.bouncycastle" % "bcpg-jdk15on" % "1.70"
   val hamcrest            = "org.hamcrest" % "hamcrest" % "2.2" ::
                             "org.hamcrest" % "hamcrest-library" % "2.2" :: Nil
