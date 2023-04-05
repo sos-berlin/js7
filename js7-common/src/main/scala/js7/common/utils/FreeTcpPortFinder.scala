bc45673501415d9d37ae294013fdde574948a634
Joacim Zschimmer <jogit@zschimmer.com>
2023-04-05 12:19:44 +0200

    Testing: findFreeTcpPort retrieves first ephemeral port

--- js7-common/src/main/scala/js7/common/utils/FreeTcpPortFinder.scala
+++ js7-common/src/main/scala/js7/common/utils/FreeTcpPortFinder.scala
@@ -2,16 +2,40 @@ package js7.common.utils
 
 import java.net.{BindException, ServerSocket}
 import js7.base.log.Logger
+import js7.base.system.OperatingSystem.isMac
 import scala.math.abs
 import scala.util.Random
+import scala.util.control.NonFatal
+import sys.process.*
 
 object FreeTcpPortFinder
 {
-  private val logger = Logger(getClass)
+  private val logger = Logger[this.type]
+
   // Do not overlap with ephemeral port range to avoid collision.
   // IANA recommends for ephemeral ports the range 49152 to 65535.
   // Linux may use 32767 to 60999, see  /proc/sys/net/ipv4/ip_local_port_range
-  private val availablePorts = 10000 to 32767
+  private val firstEphemeralPort: Int =
+    if (isMac)
+      try {
+        val output = new StringBuilder
+        val processLogger = new ProcessLogger {
+          def out(s: => String) = output.append(s)
+
+          def err(s: => String) = ()
+
+          def buffer[T](f: => T): T = f
+        }
+        "sysctl -n net.inet.ip.portrange.first" ! processLogger
+        output.toString.takeWhile(_ != '\n').toInt
+      } catch { case NonFatal(t) =>
+        logger.error(s"firstEphemeralPort => $t", t)
+        32767
+      }
+    else
+      32767
+
+  private val availablePorts = 20000 to firstEphemeralPort -1
   private val requiredPortCount = 1000
 
   private val freePortNumberIterator = {
