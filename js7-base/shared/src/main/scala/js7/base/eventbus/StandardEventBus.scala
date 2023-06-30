981247acac88311a264e9c1790a5427a323efa96
Joacim Zschimmer <jogit@zschimmer.com>
2023-06-30 06:44:23 +0200

    Logging

--- js7-base/shared/src/main/scala/js7/base/eventbus/StandardEventBus.scala
+++ js7-base/shared/src/main/scala/js7/base/eventbus/StandardEventBus.scala
@@ -1,5 +1,8 @@
 package js7.base.eventbus
 
+import js7.base.eventbus.StandardEventBus.logger
+import js7.base.log.Logger
+import js7.base.utils.ScalaUtils.syntax.RichJavaClass
 import scala.reflect.ClassTag
 
 final class StandardEventBus[E](eventClass: Class[E]) extends ClassEventBus[E]
@@ -7,10 +10,16 @@ final class StandardEventBus[E](eventClass: Class[E]) extends ClassEventBus[E]
   def this()(implicit e: ClassTag[E]) =
     this(e.runtimeClass.asInstanceOf[Class[E]])
 
+  logger.trace(s"new $toString[${eventClass.simpleScalaName}]")
+
   protected type Classifier = E
   protected type ClassifierToEvent[E1 <: E] = E1
 
   protected def classifierSuperclass = eventClass
 
   protected def classify(event: E) = event.getClass
+}
+
+object StandardEventBus {
+  private val logger = Logger[this.type]
 }
