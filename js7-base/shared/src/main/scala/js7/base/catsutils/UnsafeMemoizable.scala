c8be784388fa7b2bbef775cd9b79ec499022bab7
Joacim Zschimmer <jogit@zschimmer.com>
2023-10-09 10:17:16 +0200

    JS-2098 Migrate to Cats Effect 3: UnsafeMemoizable

--- js7-base/shared/src/main/scala/js7/base/catsutils/UnsafeMemoizable.scala
+++ js7-base/shared/src/main/scala/js7/base/catsutils/UnsafeMemoizable.scala
@@ -1,13 +1,13 @@
 package js7.base.catsutils
 
 import cats.effect.concurrent.Deferred
+import cats.effect.syntax.bracket.*
 import cats.effect.syntax.syncEffect.*
-import cats.effect.{Concurrent, SyncEffect, SyncIO}
+import cats.effect.{Concurrent, SyncEffect, SyncIO, concurrent}
+import cats.syntax.applicativeError.*
 import cats.syntax.flatMap.*
-import cats.syntax.functor.*
 import java.util.concurrent.atomic.AtomicBoolean
 import js7.base.utils.Lazy
-import monix.eval.Task
 
 /** Monix Task like unsafe memoize for any Cats Concurrent. */
 trait UnsafeMemoizable[F[_]]:
@@ -18,21 +18,20 @@ trait UnsafeMemoizable[F[_]]:
 
 object UnsafeMemoizable:
 
-  given taskMemoizable: UnsafeMemoizable[Task] with
-    extension[A](task: Task[A])
-      def unsafeMemoize: Task[A] =
-        task.memoize
-
   given concurrentMemoizable[F[_]](using F: Concurrent[F]): UnsafeMemoizable[F] with
     extension[A](leftSide: F[A])
       def unsafeMemoize: F[A] =
-        val triggered = new AtomicBoolean(false)
-        val deferred = Deferred.unsafe[F, A]
+        val triggered = AtomicBoolean(false)
+        val deferred = Deferred.unsafe[F, Either[Throwable, A]]
         F.defer:
           if !triggered.getAndSet(true) then
-            leftSide.flatMap(a => deferred.complete(a).as(a))
+            leftSide
+              .attempt
+              .flatMap(tried =>
+                deferred.complete(tried) >> F.fromEither(tried))
+              .uncancelable // TODO Allow cancellation, then synchronize `triggered`
           else
-            deferred.get
+            deferred.get.flatMap(F.fromEither)
 
   given syncEffectMemoizable[F[_]](using F: SyncEffect[F]): UnsafeMemoizable[F] with
     extension[A](leftSide: F[A])
--- js7-base/shared/src/test/scala/js7/base/catsutils/UnsafeMemoizableTest.scala
+++ js7-base/shared/src/test/scala/js7/base/catsutils/UnsafeMemoizableTest.scala
@@ -1,11 +1,11 @@
 package js7.base.catsutils
 
-import cats.FlatMap
-import cats.effect.{Concurrent, IO, Sync, SyncIO}
-import cats.syntax.flatMap.*
-import cats.syntax.functor.*
+import cats.effect.{Concurrent, ContextShift, IO, Sync, SyncIO, Timer}
+import cats.syntax.all.*
+import cats.{FlatMap, effect}
 import java.util.concurrent.atomic.AtomicInteger
 import js7.base.catsutils.UnsafeMemoizable.given
+import js7.base.log.Logger
 import js7.base.test.OurAsyncTestSuite
 import monix.eval.Task
 import org.scalatest.Assertion
@@ -14,16 +14,13 @@ import scala.concurrent.ExecutionContext
 final class UnsafeMemoizableTest extends OurAsyncTestSuite:
 
   "without unsafeMemoize" in:
-    given Concurrent[IO] = makeConcurrent()
+    given ContextShift[IO] = IO.contextShift(ExecutionContext.global)
     check[IO](identity, 3).unsafeToFuture()
 
   "unsafeMemoize with Cats IO" in:
-    given Concurrent[IO] = makeConcurrent()
-    check[IO](_.unsafeMemoize, 1).unsafeToFuture()
-
-  private def makeConcurrent(): Concurrent[IO] =
-    implicit val cs = IO.contextShift(ExecutionContext.global)
-    implicitly[Concurrent[IO]]
+    given ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+    given Timer[IO] = IO.timer(ExecutionContext.global)
+    checkIO(_.unsafeMemoize, 1).unsafeToFuture()
 
   "unsafeMemoize with Cats SyncIO (experimental only)" in:
     check[SyncIO](_.unsafeMemoize, 1).unsafeRunSync()
@@ -40,19 +37,39 @@ final class UnsafeMemoizableTest extends OurAsyncTestSuite:
     import monix.execution.Scheduler.Implicits.traced
     check0[Task](1).runToFuture
 
-  private def check0[F[_]: Concurrent: FlatMap](expected: Int): F[Assertion] =
+  private def check0[F[_]: Concurrent: ContextShift: FlatMap](expected: Int): F[Assertion] =
     // These two calls are equivalent
-    (for
+    for
       _ <- check[F](_.unsafeMemoize, expected)
       _ <- check[F](UnsafeMemoizable.concurrentMemoizable.unsafeMemoize(_), expected)
-    yield succeed)
+    yield succeed
 
   private def check[F[_]: Sync: FlatMap](f: F[Int] => F[Int], expected: Int): F[Assertion] =
     val called = new AtomicInteger(0)
     // TODO How to test parallel execution?
-    val x = f(Sync[F].delay(called.incrementAndGet()))
-    (for
+    val x: F[Int] = f(Sync[F].delay(called.incrementAndGet()))
+    val fail: F[Int] = f(Sync[F].raiseError(new RuntimeException("TEST")))
+    for
+      _ <- fail.attempt
       _ <- x
       _ <- x
       a <- x
-    yield assert(a == expected))
+    yield assert(a == expected)
+
+  private def checkIO(f: IO[Int] => IO[Int], expected: Int)(using ContextShift[IO], Timer[IO])
+  : IO[Assertion] =
+    val called = new AtomicInteger(0)
+    // TODO How to test parallel execution?
+    val increment: IO[Int] = f(Sync[IO].delay(called.incrementAndGet()))
+    val fail: IO[Int] = f(Sync[IO].raiseError(new RuntimeException("TEST")))
+    // TODO How to test a cancelled operation?
+    //val cancel: IO[Int] = ?
+    for
+      _ <- fail.attempt
+      //_ <- cancel
+      //_ <- cancel
+      _ <- IO(Logger[this.type].info("cancel fertig"))
+      _ <- increment
+      _ <- increment
+      a <- increment
+    yield assert(a == expected)
