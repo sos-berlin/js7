00bff39bda9eb63629851916c95348f99eefc5cd
Joacim Zschimmer <jogit@zschimmer.com>
2023-11-08 05:34:36 +0100

    JS-2096 On AgentCoupled, check if AgentRef must be attached (v2.6)

--- js7-controller/src/main/scala/js7/controller/agent/AgentDriver.scala
+++ js7-controller/src/main/scala/js7/controller/agent/AgentDriver.scala
@@ -43,11 +43,6 @@ import js7.data.cluster.ClusterWatchId
 import js7.data.cluster.ClusterWatchProblems.ClusterNodeLossNotConfirmedProblem
 import js7.data.controller.{ControllerRunId, ControllerState}
 import js7.data.event.{AnyKeyedEvent, EventId, KeyedEvent, Stamped}
-import js7.data.controller.ControllerState
-import js7.data.delegate.DelegateCouplingState.{Coupled, Resetting}
-import js7.data.event.KeyedEvent.NoKey
-import js7.data.event.{AnyKeyedEvent, Event, EventId, EventRequest, KeyedEvent, Stamped}
-import js7.data.item.BasicItemEvent.ItemAttachable
 import js7.data.item.ItemAttachedState.{Attachable, Attached}
 import js7.data.item.{InventoryItemKey, ItemAttachedState, SignableItem, UnsignedItem, UnsignedSimpleItemPath}
 import js7.data.node.NodeId
@@ -437,7 +432,7 @@ extends Service.StoppableByRequest
       //adoptedEventId <- Resource.eval(Task(state.adoptedEventId))
       afterEventId <- Resource.eval(agentRefState.map(_.eventId))
       directorDriver <- DirectorDriver.resource(
-        agentPath, afterEventId,
+        agentDriver, agentPath, afterEventId,
         client,
         dedicateAgentIfNeeded, onCouplingFailed, onCoupled, onDecoupled,
         onEventsFetched,
