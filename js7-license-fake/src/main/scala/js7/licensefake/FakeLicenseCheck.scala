e05421531782b85b0e2a75a007f6d21f35aa864c
Joacim Zschimmer <jogit@zschimmer.com>
2024-01-12 12:21:16 +0100

    JS-2098 Migrate to Cats Effect 3 - js7-license

---
+++ js7-license-fake/src/main/scala/js7/licensefake/FakeLicenseCheck.scala
@@ -0,0 +1,16 @@
+package js7.licensefake
+
+import java.nio.file.Files.isDirectory
+import js7.license.{LicenseCheck, LicenseCheckContext}
+
+final class FakeLicenseCheck extends LicenseCheck:
+  private var context: LicenseCheckContext = null
+
+  override def initialize(context: LicenseCheckContext) =
+    this.context = context
+
+  def hasLicense(productName: String) =
+    if !isDirectory(context.configDirectory) then throw new AssertionError // will never happen
+    true
+
+  override def toString = "FakeLicenseCheck"
