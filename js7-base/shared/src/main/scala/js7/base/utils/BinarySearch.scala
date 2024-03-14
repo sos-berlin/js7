cc168d5f2264455b7d045692cac027a071ed8e80
Joacim Zschimmer <jogit@zschimmer.com>
2024-03-14 14:22:38 +0100

    FIX BinarySearch

--- js7-base/shared/src/main/scala/js7/base/utils/BinarySearch.scala
+++ js7-base/shared/src/main/scala/js7/base/utils/BinarySearch.scala
@@ -4,21 +4,43 @@ import scala.annotation.tailrec
 
 object BinarySearch:
 
-  def binarySearch(indexedSeq: IndexedSeq[Int], value: Int): (Int, Boolean) =
-    binarySearch(0, indexedSeq.length, indexedSeq(_).compare(value))
+  def binarySearch[A](indexedSeq: IndexedSeq[A])(value: A)(using ordering: Ordering[A])
+  : (Int, Boolean) =
+    binarySearchNew(0, indexedSeq.length): i =>
+      ordering.compare(value, indexedSeq(i))
 
+  def binarySearch[A, B](indexedSeq: IndexedSeq[A], convert: A => B)(value: B)
+    (using ordering: Ordering[B])
+  : (Int, Boolean) =
+    binarySearchNew(0, indexedSeq.length): i =>
+      ordering.compare(value, convert(indexedSeq(i)))
+
+  @deprecated("Use binarySearchNew with reverse comparison")
   def binarySearch(start: Int, end: Int, compareTo: Int => Int): (Int, Boolean) =
+    binarySearchNew(start, end)(i => -compareTo(i))
+
+  /** Binary search in an ordered sequence.
+   * `compareTo` with an index as argument should return
+   * - 0 if the indexed values equals the searched value.
+   * - a negative number if the searched value is less than the indexed value.
+   * - a positive number if the searched value is greated than the indexed value.
+   * argument is an index
+   * @param start the first index
+   * @param end the index after the last one.
+   * @param compareTo argument is an index */
+  def binarySearchNew(start: Int, end: Int)(compareTo: Int => Int): (Int, Boolean) =
     @tailrec def loop(low: Int, high: Int): (Int, Boolean) =
-      if low <= high then
-        val middle = (low + high) >>> 1
+      // high is the index *after*, high - low == length
+      if low < high then
+        val middle = low + ((high - low) >>> 1)
         val cmp = compareTo(middle)
         if cmp < 0 then
-          loop(low + 1, high)
+          loop(low, middle)
         else if cmp > 0 then
-          loop(low, high - 1)
+          loop(middle + 1, high)
         else
           middle -> true
       else
         low -> false
 
-    loop(start, end - 1)
+    loop(start, end)
--- js7-base/shared/src/test/scala/js7/base/utils/BinarySearchTest.scala
+++ js7-base/shared/src/test/scala/js7/base/utils/BinarySearchTest.scala
@@ -1,20 +1,63 @@
 package js7.base.utils
 
 import js7.base.test.OurTestSuite
-import js7.base.utils.BinarySearch.binarySearch
+import js7.base.utils.BinarySearch.{binarySearch, binarySearchNew}
 
 final class BinarySearchTest extends OurTestSuite:
+
   "binarySearch" in:
-    assert(binarySearch(Vector.empty, 7) == (0, false))
+    locally:
+      assert(binarySearch(Vector.empty)(7) == (0, false))
 
-    assert(binarySearch(Vector(30), 20) == (0, false))
-    assert(binarySearch(Vector(30), 30) == (0, true))
-    assert(binarySearch(Vector(30), 40) == (1, false))
+      assert(binarySearch(Vector(2))(1) == (0, false))
+      assert(binarySearch(Vector(2))(2) == (0, true))
+      assert(binarySearch(Vector(2))(3) == (1, false))
 
-    assert(binarySearch(Vector(20, 30, 40), 15) == (0, false))
-    assert(binarySearch(Vector(20, 30, 40), 20) == (0, true))
-    assert(binarySearch(Vector(20, 30, 40), 25) == (1, false))
-    assert(binarySearch(Vector(20, 30, 40), 30) == (1, true))
-    assert(binarySearch(Vector(20, 30, 40), 35) == (2, false))
-    assert(binarySearch(Vector(20, 30, 40), 40) == (2, true))
-    assert(binarySearch(Vector(20, 30, 40), 56) == (3, false))
+    locally:
+      val seq = Vector(20, 30, 40)
+      assert(binarySearch(seq)(15) == (0, false))
+      assert(binarySearch(seq)(20) == (0, true))
+      assert(binarySearch(seq)(25) == (1, false))
+      assert(binarySearch(seq)(30) == (1, true))
+      assert(binarySearch(seq)(35) == (2, false))
+      assert(binarySearch(seq)(40) == (2, true))
+      assert(binarySearch(seq)(56) == (3, false))
+
+    locally:
+      val n = 1_000_000
+      val seq = Vector.from(0 until n).map(_.toString)
+
+      var comparisons = 0
+      val compare: String => Int => Int =
+        searched =>
+          Logger.trace("┈" * 30)
+          comparisons = 0
+          index =>
+            comparisons += 1
+            val r = searched.toInt.compare(seq(index).toInt)
+            val cmp = if r < 0 then "<" else if r > 0 then ">" else "="
+            Logger.trace(s"$searched $cmp ${seq(index)} ")
+            r
+
+      assert(binarySearchNew(0, 0)(compare("-1")) == (0, false) && comparisons == 0)
+      assert(binarySearchNew(0, 1)(compare("-1")) == (0, false) && comparisons == 1)
+
+      assert(binarySearchNew(0, 7)(compare("-1")) == (0, false) && comparisons == 3)
+      assert(binarySearchNew(0, 7)(compare("0")) == (0, true) && comparisons == 3)
+      assert(binarySearchNew(0, 7)(compare("1")) == (1, true) && comparisons == 2)
+      assert(binarySearchNew(0, 7)(compare("2")) == (2, true) && comparisons == 3)
+      assert(binarySearchNew(0, 7)(compare("3")) == (3, true) && comparisons == 1)
+      assert(binarySearchNew(0, 7)(compare("4")) == (4, true) && comparisons == 3)
+      assert(binarySearchNew(0, 7)(compare("5")) == (5, true) && comparisons == 2)
+      assert(binarySearchNew(0, 7)(compare("6")) == (6, true) && comparisons == 3)
+      assert(binarySearchNew(0, 7)(compare("7")) == (7, false) && comparisons == 3)
+
+      assert(binarySearchNew(0, n)(compare("-1")) == (0, false) && comparisons == 20)
+      assert(binarySearchNew(0, n)(compare("0")) == (0, true) && comparisons == 20)
+      assert(binarySearchNew(0, n)(compare("1")) == (1, true) && comparisons == 19)
+      assert(binarySearchNew(0, n)(compare("2")) == (2, true) && comparisons == 20)
+      assert(binarySearchNew(0, n)(compare("3")) == (3, true) && comparisons == 18)
+      assert(binarySearchNew(0, n)(compare("250000")) == (250000, true) && comparisons == 2)
+      assert(binarySearchNew(0, n)(compare("499999")) == (499999, true) && comparisons == 19)
+      assert(binarySearchNew(0, n)(compare("500000")) == (500000, true) && comparisons == 1)
+      assert(binarySearchNew(0, n)(compare("1000000")) == (1000000, false) && comparisons == 19)
