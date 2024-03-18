d22051ec3c6440c33e06c8159d3e19dba22ee905
Joacim Zschimmer <jogit@zschimmer.com>
2024-03-18 19:48:02 +0100

    logging

--- js7-common/src/main/scala/js7/common/scalautil/Synchronizer.scala
+++ js7-common/src/main/scala/js7/common/scalautil/Synchronizer.scala
@@ -2,6 +2,7 @@ package js7.common.scalautil
 
 import java.util.concurrent.locks.ReentrantLock
 import js7.base.log.Logger
+import js7.base.log.Logger.syntax.*
 import js7.base.time.ScalaTime.*
 import js7.common.scalautil.Synchronizer.*
 import scala.concurrent.blocking
@@ -16,20 +17,19 @@ final class Synchronizer(what: String):
 
   /** Blocking synchronization with some debug messages logged. */
   def synchronize[A](body: => A): A =
-    try
-      logger.trace(s"Start synchronize '$what': wait for lock (${synchronizeLock.getQueueLength} in queue)")
-      if !synchronizeLock.tryLock() then
-        blocking:
-          if !synchronizeLock.tryLock(LogAfter.toMillis, MILLISECONDS) then
-            logger.debug(s"🟠 Start synchronize '$what': waiting for lock since ${LogAfter.pretty} (#${synchronizeLock.getQueueLength} in queue)")
-            synchronizeLock.lock()
-            logger.debug(s"🟢 synchronize '$what': continuing")
-      body
-    finally
-      logger.trace(s"End synchronize '$what': release lock (${synchronizeLock.getQueueLength} in queue)")
-      synchronizeLock.unlock()
+    logger.traceCall(s"synchronize '$what': wait for lock (${synchronizeLock.getQueueLength} in queue)"):
+      try
+        if !synchronizeLock.tryLock() then
+          blocking:
+            if !synchronizeLock.tryLock(LogAfter.toMillis, MILLISECONDS) then
+              logger.debug(s"🟠 Start synchronize '$what': waiting for lock since ${LogAfter.pretty} (#${synchronizeLock.getQueueLength} in queue)")
+              synchronizeLock.lock()
+              logger.debug(s"🟢 synchronize '$what': continuing")
+        body
+      finally
+        synchronizeLock.unlock()
 
 
 object Synchronizer:
-  private val LogAfter = 100.ms
+  private val LogAfter = 10.ms
   private val logger = Logger[this.type]
