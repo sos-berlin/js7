ba220e10257a22d77ed1e7d5e2aa91f566c79955
Joacim Zschimmer <jogit@zschimmer.com>
2024-06-03 08:18:28 +0200

    logging: log4j2-stderr.xml

--- bin/sbt-batch.cmd
+++ bin/sbt-batch.cmd
@@ -2,6 +2,6 @@
 setlocal
 
 rem set SBT_OPTS=
-set JAVA_OPTS=-Xmx2500M -XX:MaxMetaspaceSize=1500M -Dlog4j.configurationFile=project/log4j2.xml -Dsbt.log.noformat=true -enableassertions %JAVA_OPTS%
+set JAVA_OPTS=-Xmx2500M -XX:MaxMetaspaceSize=1500M -Dlog4j2.configurationFile=project/log4j2.xml -Dsbt.log.noformat=true -enableassertions %JAVA_OPTS%
 sbt.bat --supershell=never --no-colors %*
 endlocal
