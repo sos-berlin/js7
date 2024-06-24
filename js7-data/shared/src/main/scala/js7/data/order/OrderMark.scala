1e74c62d18e2e37b7ad14175b4ac4e361ace05eb
Joacim Zschimmer <jogit@zschimmer.com>
2024-06-24 07:11:53 +0200

    logging

--- js7-data/shared/src/main/scala/js7/data/order/OrderMark.scala
+++ js7-data/shared/src/main/scala/js7/data/order/OrderMark.scala
@@ -6,6 +6,8 @@ import js7.base.circeutils.CirceUtils.DecodeWithDefaults
 import js7.base.circeutils.typed.{Subtype, TypedJsonCodec}
 import js7.base.utils.Big
 import js7.base.utils.IntelliJUtils.intelliJuseImport
+import js7.base.utils.ScalaUtils.parameterListToString
+import js7.base.utils.ScalaUtils.syntax.RichBoolean
 import js7.data.command.{CancellationMode, SuspensionMode}
 import js7.data.order.OrderEvent.OrderResumed
 import js7.data.workflow.position.Position
@@ -15,16 +17,26 @@ sealed trait OrderMark
 object OrderMark
 {
   final case class Cancelling(mode: CancellationMode)
-  extends OrderMark
+  extends OrderMark {
+    override def toString = "Cancelling" +
+      parameterListToString((mode != CancellationMode.Default) ? mode)
+  }
 
   final case class Suspending(mode: SuspensionMode = SuspensionMode.standard)
-  extends OrderMark
+  extends OrderMark {
+    override def toString = "Suspending" +
+      parameterListToString((mode != SuspensionMode.standard) ? mode)
+  }
 
   final case class Resuming(
     position: Option[Position] = None,
+    // TODO rename as historyOperations like in OrderResumed
     historicOperations: Seq[OrderResumed.HistoryOperation] = Nil,
     asSucceeded: Boolean = false)
-  extends OrderMark with Big
+  extends OrderMark with Big {
+    override def toString = "Resuming" +
+      parameterListToString(position.view, historicOperations, asSucceeded ? "asSucceeded")
+  }
 
   implicit val jsonCodec: TypedJsonCodec[OrderMark] = TypedJsonCodec(
     Subtype(deriveCodec[Cancelling]),
