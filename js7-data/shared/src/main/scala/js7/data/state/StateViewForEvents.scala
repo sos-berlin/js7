b1778e02f4273c08d08daa8a16dbd9998f85d2fe
Joacim Zschimmer <jogit@zschimmer.com>
2024-07-06 08:12:54 +0200

    JS-2135 undo: refactor ifSkippedThenMove

--- js7-data/shared/src/main/scala/js7/data/state/StateViewForEvents.scala
+++ js7-data/shared/src/main/scala/js7/data/state/StateViewForEvents.scala
@@ -1,9 +1,6 @@
 package js7.data.state
 
-import js7.base.utils.ScalaUtils.syntax.RichBoolean
-import js7.data.order.Order
-import js7.data.order.OrderEvent.{OrderActorEvent, OrderDetachable, OrderMoved}
-import js7.data.workflow.WorkflowPathControlPath
+import js7.data.order.OrderEvent.{OrderActorEvent, OrderDetachable}
 
 object StateViewForEvents:
 
@@ -14,21 +11,3 @@ object StateViewForEvents:
         OrderDetachable :: Nil
       else
         events
-
-    def ifSkippedThenMove(order: Order[Order.State]): Option[OrderMoved] =
-      !order.isFailed ?&
-        skippedReason(order).map: reason =>
-          OrderMoved(order.position.increment, Some(reason))
-
-    private def skippedReason(order: Order[Order.State]): Option[OrderMoved.Reason] =
-      state.isSkippedDueToWorkflowPathControl(order) ?
-        OrderMoved.SkippedDueToWorkflowPathControl
-
-    private def isSkippedDueToWorkflowPathControl(order: Order[Order.State]): Boolean =
-      !order.isState[Order.BetweenCycles] &&
-        state
-          .pathToWorkflowPathControl.get(WorkflowPathControlPath(order.workflowPath))
-          .exists: control =>
-            state.workflowPositionToLabel(order.workflowPosition)
-              .toOption.flatten
-              .exists(control.skip.contains)
