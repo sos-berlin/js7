fbdbe4cb92878609c22851a1ed7380869b222d46
Joacim Zschimmer <jogit@zschimmer.com>
2024-09-27 13:31:35 +0200

    JS-2145 Test for »if $js7SubagentProcessCount == 0 then 1 else missing«

--- js7-tests/src/test/scala/js7/tests/subagent/SubagentPriorityTest.scala
+++ js7-tests/src/test/scala/js7/tests/subagent/SubagentPriorityTest.scala
@@ -11,8 +11,8 @@ import js7.base.time.ScalaTime.*
 import js7.base.utils.ScalaUtils.syntax.RichEither
 import js7.common.utils.FreeTcpPortFinder.findFreeLocalUri
 import js7.data.item.BasicItemEvent.ItemAttached
-import js7.data.order.OrderEvent.{OrderProcessingStarted, OrderTerminated}
-import js7.data.order.{FreshOrder, OrderId}
+import js7.data.order.OrderEvent.{OrderAttached, OrderProcessingStarted, OrderTerminated}
+import js7.data.order.{FreshOrder, Order, OrderId}
 import js7.data.subagent.SubagentItemStateEvent.SubagentCoupled
 import js7.data.subagent.{SubagentBundle, SubagentBundleId, SubagentId, SubagentItem}
 import js7.data.value.expression.Expression.StringConstant
@@ -27,6 +27,11 @@ import js7.tests.testenv.BlockingItemUpdater
 import js7.tests.testenv.DirectoryProvider.toLocalSubagentId
 
 final class SubagentPriorityTest extends OurTestSuite, SubagentTester, BlockingItemUpdater:
+
+  override protected val controllerConfig = config"""
+    js7.auth.users.TEST-USER.permissions = [ UpdateItem ]
+    js7.controller.agent-driver.command-batch-delay = 0ms
+    js7.controller.agent-driver.event-buffer-delay = 0ms"""
 
   override protected def agentConfig = config"""
     js7.auth.subagents.A-SUBAGENT = "$localSubagentId's PASSWORD"
@@ -64,7 +69,7 @@ final class SubagentPriorityTest extends OurTestSuite, SubagentTester, BlockingI
   private lazy val startSubagets: Unit =
     idToRelease
 
-  "Use $js7TestMeteringValue from Subagent" in :
+  "Use $js7TestMeteringValue from Subagent" in:
     startSubagets
 
     val subagentBundle = SubagentBundle(
@@ -102,6 +107,7 @@ final class SubagentPriorityTest extends OurTestSuite, SubagentTester, BlockingI
 
   "Use $js7SubagentProcessCount and $js7ClusterSubagentProcessCount" in :
     startSubagets
+    ASemaphoreJob.reset()
 
     val subagentBundle = SubagentBundle(
       SubagentBundleId("BUNDLE-2"),
@@ -139,6 +145,56 @@ final class SubagentPriorityTest extends OurTestSuite, SubagentTester, BlockingI
       ASemaphoreJob.continue(2)
       eventWatch.await[OrderTerminated](_.key == aOrderId)
       eventWatch.await[OrderTerminated](_.key == bOrderId)
+
+      deleteItems(workflow.path, subagentBundle.path)
+
+  "Use $js7SubagentProcessCount" in:
+    startSubagets
+    ASemaphoreJob.reset()
+
+    val subagentBundle = SubagentBundle(
+      SubagentBundleId("BUNDLE-3"),
+      Map(
+        aSubagentId -> expr("if $js7SubagentProcessCount == 0 then 1 else missing"),
+        bSubagentId -> expr("if $js7SubagentProcessCount == 0 then 1 else missing")))
+
+    val workflow = Workflow(
+      WorkflowPath("WORKFLOW-3"),
+      Seq(
+        ASemaphoreJob.execute(
+          agentPath,
+          subagentBundleId = Some(StringConstant(subagentBundle.id.string)),
+          processLimit = 100)))
+
+      updateItems(workflow :: subagentBundle :: subagentItems*)
+      for id <- subagentItems.map(_.id) do
+        eventWatch.await[ItemAttached](_.event.key == id)
+        eventWatch.await[SubagentCoupled](_.key == id)
+
+      val aOrderId, bOrderId, cOrderId = nextOrderId()
+
+      controller.api.addOrder(FreshOrder(aOrderId, workflow.path, deleteWhenTerminated = true))
+        .await(99.s).orThrow
+      val aStarted = eventWatch.awaitNext[OrderProcessingStarted](_.key == aOrderId)
+        .head.value.event
+
+      controller.api.addOrder(FreshOrder(bOrderId, workflow.path, deleteWhenTerminated = true))
+        .await(99.s).orThrow
+      val bStarted = eventWatch.awaitNext[OrderProcessingStarted](_.key == bOrderId)
+        .head.value.event
+
+      assert(aStarted.subagentId == Some(aSubagentId) && bStarted.subagentId == Some(bSubagentId))
+
+      controller.api.addOrder(FreshOrder(cOrderId, workflow.path, deleteWhenTerminated = true))
+        .await(99.s).orThrow
+      eventWatch.awaitNext[OrderAttached](_.key == cOrderId)
+      sleep(100.ms)
+      assert(controllerState.idToOrder(cOrderId).state == Order.Fresh) // Process not started
+
+      ASemaphoreJob.continue(3)
+      eventWatch.await[OrderTerminated](_.key == aOrderId)
+      eventWatch.await[OrderTerminated](_.key == bOrderId)
+      eventWatch.await[OrderTerminated](_.key == cOrderId)
 
       deleteItems(workflow.path, subagentBundle.path)
 
