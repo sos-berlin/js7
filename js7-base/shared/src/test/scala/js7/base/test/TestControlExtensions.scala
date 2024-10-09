cb6e5e344193d4c3a04475ec5099390544cc232f
Joacim Zschimmer <jogit@zschimmer.com>
2024-10-09 13:15:15 +0200

    TestControlExtensions

--- js7-base/shared/src/test/scala/js7/base/test/TestControlExtensions.scala
+++ js7-base/shared/src/test/scala/js7/base/test/TestControlExtensions.scala
@@ -3,11 +3,16 @@ package js7.base.test
 import cats.effect.IO
 import cats.effect.Outcome
 import cats.effect.testkit.TestControl
+import cats.effect.unsafe.IORuntimeConfig
 import org.scalatest.Assertions.fail
+import scala.concurrent.duration.FiniteDuration
 
 object TestControlExtensions:
 
   extension[A](control: TestControl[A])
+    def runFor(duration: FiniteDuration): IO[A] =
+      control.tickFor(duration) *> finish
+
     def finish: IO[A] =
       control.tick *> successOrFail
 
@@ -18,3 +23,13 @@ object TestControlExtensions:
         case Some(Outcome.Errored(t)) => throw t
         case Some(outcome) => fail(s"Unexpected TestControl.results => $outcome")
         case None => fail("Tested program has not terminated")
+
+
+  extension (x: TestControl.type)
+    def executeFor[A](duration: FiniteDuration)(
+      program: IO[A],
+      config: IORuntimeConfig = IORuntimeConfig(),
+      seed: Option[String] = None)
+    : IO[A] =
+      TestControl.execute(program, config, seed).flatMap:
+        _.runFor(duration)
