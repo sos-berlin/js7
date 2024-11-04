9f21a105996bf2504caabe6323260d0e71d93959
Joacim Zschimmer <jogit@zschimmer.com>
2024-11-04 09:40:42 +0100

    Replace STM-based MVar: CoupledApiVar

--- js7-common-http/shared/src/main/scala/js7/common/http/CoupledApiVar.scala
+++ js7-common-http/shared/src/main/scala/js7/common/http/CoupledApiVar.scala
@@ -2,14 +2,15 @@ package js7.common.http
 
 import cats.effect.IO
 import cats.syntax.flatMap.*
-import js7.base.catsutils.UnsafeMemoizable.unsafeMemoize
+import cats.syntax.option.*
+import fs2.concurrent.SignallingRef
+import js7.base.catsutils.UnsafeMemoizable.memoize
 import js7.base.generic.Completed
 import js7.base.log.Logger
 import js7.base.problem.Checked
 import js7.base.problem.Checked.Ops
 import js7.base.session.SessionApi
 import js7.base.time.ScalaTime.*
-import js7.base.utils.MVar
 import js7.common.http.CoupledApiVar.*
 import js7.common.http.RecouplingStreamReader.TerminatedProblem
 
@@ -20,53 +21,44 @@ import js7.common.http.RecouplingStreamReader.TerminatedProblem
 private[http] final class CoupledApiVar[Api <: SessionApi]:
 
   // The only Left value is TerminatedProblem
-  private val coupledApiMVar = MVar.empty[IO, Checked[Api]].unsafeMemoize
+  private val coupledApi = memoize(SignallingRef[IO].of(none[Checked[Api]]))
   @volatile private var stopped = false
 
   def isStopped = stopped
 
   def terminate: IO[Unit] =
     ZeroDuration.tailRecM: delay =>
-      IO {
+      IO.defer:
         logger.trace("stopped = true")
         stopped = true
-      } *>
         IO.delay(delay) *>
-        invalidate *>
-        coupledApiMVar.flatMap(_.tryPut(Left(TerminatedProblem)))
-          .map(if _ then Right(()) else Left(10.ms/*just in case this loop is endless*/))
+          invalidate *>
+          coupledApi.flatMap:
+            _.tryUpdate(_ => Some(Left(TerminatedProblem)))
+          .map:
+            if _ then Right(()) else Left(10.ms /*just in case this loop is endless*/)
 
   def invalidate: IO[Completed] =
-    coupledApiMVar.flatMap(_.tryTake)
-      .map(_ => Completed)
-
-  def read: IO[Api] =
-    coupledApiMVar.flatMap(_
-      .read
-      .map(_.orThrow))
+    coupledApi.flatMap(_.set(none))
+      .as(Completed)
 
   def tryRead: IO[Option[Api]] =
-    coupledApiMVar.flatMap(_
-      .tryRead
+    coupledApi.flatMap(_
+      .get
       .map(_.map(_.orThrow)))
 
-  def take: IO[Api] =
-    coupledApiMVar.flatMap(_
-      .take
-      .map(_.orThrow))
-
   def tryTake: IO[Option[Api]] =
-    coupledApiMVar.flatMap(_
-      .tryTake
+    coupledApi.flatMap(_
+      .modify(none -> _)
       .map(_.map(_.orThrow)))
 
   def isTerminated: IO[Boolean] =
-    coupledApiMVar.flatMap(_.tryRead).map:
+    coupledApi.flatMap(_.get).map:
       case Some(Left(TerminatedProblem)) => true
       case _ => false
 
   def put(api: Api): IO[Unit] =
-    coupledApiMVar.flatMap(_.put(Right(api)))
+    coupledApi.flatMap(_.set(Some(Right(api))))
 
 object CoupledApiVar:
   private val logger = Logger[this.type]
