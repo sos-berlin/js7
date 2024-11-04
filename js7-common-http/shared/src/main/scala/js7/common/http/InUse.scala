60cdcef3e6afbf39d8b3a0b9449cf0d2e512995a
Joacim Zschimmer <jogit@zschimmer.com>
2024-11-04 09:40:44 +0100

    Replace STM-based MVar: InUse

--- js7-common-http/shared/src/main/scala/js7/common/http/InUse.scala
+++ js7-common-http/shared/src/main/scala/js7/common/http/InUse.scala
@@ -1,37 +1,38 @@
 package js7.common.http
 
+import cats.effect.std.Mutex
 import cats.effect.{IO, Resource, ResourceIO}
-import js7.base.catsutils.UnsafeMemoizable.unsafeMemoize
+import js7.base.catsutils.UnsafeMemoizable.memoize
 import js7.base.log.Logger
+import js7.base.utils.Atomic
 import js7.base.utils.Atomic.extensions.*
 import js7.base.utils.CatsUtils.syntax.logWhenItTakesLonger
-import js7.base.utils.{Atomic, MVar}
 import js7.common.http.InUse.*
 
 private final class InUse:
 
   private val inUse = Atomic(false)
-  private val inUseVar = MVar.empty[IO, Unit].unsafeMemoize
+  private val mutex = memoize(Mutex[IO].map(_.lock))
 
   def is: Boolean =
     inUse.get()
 
+  /** Not really synchronized with mutex, but we don't use it asynchronously. */
   def get: IO[Boolean] =
-    inUseVar.flatMap(_.tryRead).map(_.isDefined)
+    IO(inUse.get())
 
   def resource[A](api: A): ResourceIO[Unit] =
-    Resource.make(
-      acquire =
-        inUseVar.flatMap(_.put(())).logWhenItTakesLonger *>
-          IO:
-            logger.trace(s"↘ $api: inUse := true")
-            inUse := true)(
-      release = _ => IO.defer:
-        logger.trace(s"↙ $api: inUse := false")
-        inUse := false
-        inUseVar.flatMap(_.tryTake).void)
+    Resource:
+      mutex.flatMap(_.allocated).logWhenItTakesLonger.flatMap: (_, release) =>
+        IO:
+          logger.trace(s"↘ $api: inUse := true")
+          inUse := true
+          () -> IO.defer:
+            logger.trace(s"↙ $api: inUse := false")
+            inUse := false
+            release
 
-  override def toString = s"InUse(${inUse.get})"
+  override def toString = s"InUse(${inUse.get()})"
 
 
 private object InUse:
