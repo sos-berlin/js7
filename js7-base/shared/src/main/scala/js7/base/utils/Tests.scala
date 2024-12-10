963a3c6f3eabed65596ddb537d953f873160af84
Joacim Zschimmer <jogit@zschimmer.com>
2024-12-10 20:16:08 +0100

    testing: -Djs7.noTest

--- js7-base/shared/src/main/scala/js7/base/utils/Tests.scala
+++ js7-base/shared/src/main/scala/js7/base/utils/Tests.scala
@@ -1,5 +1,6 @@
 package js7.base.utils
 
+import js7.base.convert.As.StringAsBoolean
 import js7.base.log.Logger
 
 object Tests:
@@ -13,8 +14,9 @@ object Tests:
       hasPackagePrefix(Set("org.jetbrains.plugins.scala.testingSupport.scalaTest.ScalaTestRunner"))
     val isScalaTest = hasPackagePrefix(Set("org.scalatest."))
     val isSbt = hasPackagePrefix(Set("xsbt.boot."))
-    val isTest = isScalaTest || isSbt
-    val isStrict = isTest || sys.props.contains("js7.strict")
+    val isTest = (isScalaTest || isSbt)
+      && !sys.props.get("js7.noTest").fold(false)(StringAsBoolean(_))
+    val isStrict = isTest || sys.props.get("js7.strict").fold(false)(StringAsBoolean(_))
 
     (isIntelliJIdea, isScalaTest, isSbt, isTest, isStrict)
 
