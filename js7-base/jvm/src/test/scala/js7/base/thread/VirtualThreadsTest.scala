a8453f3b53633831f1ddfb5af9c73c8f67af3724
Joacim Zschimmer <jogit@zschimmer.com>
2024-12-10 20:41:49 +0100

    JS-2188 FIX No more virtual threads because they block with >256 reads

---
+++ js7-base/jvm/src/test/scala/js7/base/thread/VirtualThreadsTest.scala
@@ -0,0 +1,96 @@
+package js7.base.thread
+
+import java.util.concurrent.{Semaphore, ThreadFactory}
+import js7.base.test.OurTestSuite
+import js7.base.time.ScalaTime.*
+import js7.base.time.Stopwatch.itemsPerSecondString
+import js7.base.utils.Atomic.extensions.*
+import js7.base.utils.Tests.isIntelliJIdea
+import js7.base.utils.{Atomic, Tests}
+import scala.concurrent.TimeoutException
+import scala.concurrent.duration.{Deadline, FiniteDuration, SECONDS}
+import scala.util.control.NonFatal
+
+final class VirtualThreadsTest extends OurTestSuite:
+
+  private val logger = Logger[this.type]
+
+  "Simple test" in:
+    val n = 10_000
+    runThreads(Vector.fill(n)(() => ()), timeout = 10.s)
+
+  "Read stdout of processes in 257 virtual threads will fail" in:
+    if !isIntelliJIdea then
+      logger.info("VirtualThreadsTest would fail")
+      pending
+
+    // n > 256 will let the test fail //
+    val n = 256 + 1
+    val processes =
+      val pb = new ProcessBuilder("sleep", "99999")
+      (1 to n).toVector.map(_ => pb.start)
+
+    try
+      runThreads(
+        processes.map: process =>
+          () =>
+            val len = process.getInputStream /*stdout*/.read()
+            assert(len == -1 /*EOF*/),
+        whenAllStarted = () => processes.foreach(_.toHandle.destroyForcibly()),
+        timeout = 10.s)
+    catch case NonFatal(t) =>
+      processes.foreach(_.toHandle.destroyForcibly())
+      processes.foreach(_.waitFor())
+      throw t
+
+    processes.foreach(_.waitFor())
+
+
+  private def runThreads(
+    runnables: Vector[() => Unit],
+    whenAllStarted: () => Unit = () => (),
+    timeout: FiniteDuration)
+  : Unit =
+    VirtualThreads.newVirtualThreadFactory match
+      case None =>
+        alert("Virtual threads are not enabled")
+        pending
+
+      case Some(threadFactory) =>
+        val n = runnables.size
+        val t = Deadline.now
+        val terminatedSemaphore = new Semaphore(0)
+        val runningSemaphore = new Semaphore(0)
+        val count = Atomic(0L)
+
+        logger.info(s"Starting $n virtual threads")
+
+        val threads = runnables.map: runnable =>
+          val thread = threadFactory.newThread: () =>
+            val cnt = count += 1
+            try
+              //Logger.infoCall(s"Virtual thread $cnt"):
+                runningSemaphore.release()
+                runnable()
+                terminatedSemaphore.acquire()
+            catch case NonFatal(t) =>
+              logger.error(t.toString)
+            finally
+              count -= 1
+          thread.start()
+          thread
+
+        // Wait until all threads are running
+        val acquired = runningSemaphore.tryAcquire(n, timeout.toSeconds, SECONDS)
+        if !acquired then
+          throw new TimeoutException(s"Not all virtual threads started within $timeout")
+        assert(count.get == n)
+        logger.info:
+          s"✔️  All $n virtual threads are running · " + itemsPerSecondString(t.elapsed, n)
+
+        whenAllStarted()
+
+        // Release the threads
+        terminatedSemaphore.release(n)
+        threads.foreach(_.join())
+        assert(count.get == 0)
---
+++ js7-tests/src/test/scala/js7/tests/ExecuteInParallelTest.scala
@@ -0,0 +1,91 @@
+package js7.tests
+
+import cats.effect.IO
+import fs2.Stream
+import js7.base.configutils.Configs.*
+import js7.base.log.Logger
+import js7.base.problem.Checked.*
+import js7.base.test.OurTestSuite
+import js7.base.thread.CatsBlocking.syntax.*
+import js7.base.time.ScalaTime.*
+import js7.base.time.Stopwatch.itemsPerSecondString
+import js7.base.utils.Tests.isIntelliJIdea
+import js7.data.agent.AgentPath
+import js7.data.command.CancellationMode
+import js7.data.controller.ControllerCommand.CancelOrders
+import js7.data.job.ShellScriptExecutable
+import js7.data.order.OrderEvent.{OrderCancelled, OrderStdoutWritten}
+import js7.data.order.{FreshOrder, OrderEvent, OrderId}
+import js7.data.value.NamedValues
+import js7.data.workflow.instructions.Execute
+import js7.data.workflow.instructions.executable.WorkflowJob
+import js7.data.workflow.{Workflow, WorkflowPath}
+import js7.tests.ExecuteInParallelTest.*
+import js7.tests.testenv.{BlockingItemUpdater, ControllerAgentForScalaTest}
+import org.scalactic.source
+import scala.concurrent.duration.{Deadline, Duration}
+
+final class ExecuteInParallelTest
+  extends OurTestSuite, ControllerAgentForScalaTest, BlockingItemUpdater:
+
+  protected val agentPaths = Seq(agentPath)
+  protected val items = Nil
+  override protected val controllerConfig = config"""
+    js7.journal.slow-check-state = off
+    js7.journal.log.info-events = []
+    js7.auth.users.TEST-USER.permissions = [ UpdateItem ]
+    """
+  override protected def agentConfig = config"""
+    js7.journal.slow-check-state = off
+    js7.journal.log.info-events = []
+    js7.job.execution.signed-script-injection-allowed = on
+    """
+
+  "Execute a job many times in parallel" in:
+    // Virtual threads will block with more than 128 processes (256 read operations) !!!
+    val n = 128 + 1
+    val workflow = Workflow.of:
+      Execute.Anonymous(WorkflowJob(
+        agentPath,
+        ShellScriptExecutable:
+          """echo STARTED
+            |sleep 99999
+            |""".stripMargin,
+        processLimit = n))
+
+    withItem(workflow): workflow=>
+      val t = Deadline.now
+      val orderIds = (1 to n).map(i => OrderId(s"#2024-12-09#$i"))
+      info_(s"Starting $n shell processes")
+      controller.api.addOrders:
+        Stream.iterable(orderIds).map:
+          FreshOrder(_, workflow.path, deleteWhenTerminated = true)
+      .map(_.orThrow)
+      .productR:
+        IO.blocking:
+          info_(f"✔️  ${t.elapsed.pretty}%-6s $n orders added")
+          for _ <- 1 to n do
+            controller.eventWatch.awaitNext[OrderStdoutWritten]()
+          info_(f"✔️  ${t.elapsed.pretty}%-6s $n shell processes are running")
+      .productR:
+        controller.api.executeCommand:
+          CancelOrders(orderIds, CancellationMode.kill(immediately = true))
+        .map(_.orThrow)
+      .productR:
+        IO.blocking:
+          for _ <- 1 to n do
+            controller.eventWatch.awaitNext[OrderCancelled]()
+          info_(f"✔️  ${t.elapsed.pretty}%-6s $n shell processes terminated")
+      .timed.map: (duration, _) =>
+        val msg = itemsPerSecondString(duration, n, "orders")
+        info_(msg)
+      .await(if isIntelliJIdea then Duration.Inf else 99.s)
+
+  private def info_(msg: String): Unit =
+    note(s"ExecuteInParallelTest $msg")
+    logger.info(msg)
+
+
+object ExecuteInParallelTest:
+  private val logger = Logger[this.type]
+  private val agentPath = AgentPath("AGENT")
