6924d3c801d8b8d1b01e4195eb7935e7b6adff5d
Joacim Zschimmer <jogit@zschimmer.com>
2024-12-27 16:20:25 +0100

    JS-2160 test: Agent shutdown no longer fail-over

---
+++ js7-tests/src/test/scala/js7/tests/cluster/agent/AgentShutdownAndRecoupleClusterTest.scala
@@ -0,0 +1,66 @@
+package js7.tests.cluster.agent
+
+import cats.effect.ResourceIO
+import js7.agent.TestAgent
+import js7.base.thread.CatsBlocking.syntax.await
+import js7.base.time.ScalaTime.*
+import js7.base.utils.AllocatedForJvm.useSync
+import js7.common.utils.FreeTcpPortFinder.findFreeLocalUri
+import js7.data.agent.{AgentPath, AgentRef}
+import js7.data.cluster.ClusterEvent.{ClusterActiveNodeShutDown, ClusterCoupled, ClusterNodesAppointed, ClusterWatchRegistered}
+import js7.data.subagent.{SubagentBundle, SubagentBundleId, SubagentId, SubagentItem}
+import js7.data.value.expression.Expression.NumericConstant
+import js7.tests.cluster.agent.AgentShutdownAndRecoupleClusterTest.*
+import js7.tests.cluster.controller.ControllerClusterTester
+import js7.tests.cluster.controller.ControllerClusterTester.*
+import js7.tests.testenv.DirectorEnv
+
+final class AgentShutdownAndRecoupleClusterTest extends ControllerClusterTester:
+
+  protected override val agentPaths = Nil
+
+  private lazy val subagentItems = Seq(
+    SubagentItem(SubagentId("SUBAGENT-0"), agentPath, findFreeLocalUri()),
+    SubagentItem(SubagentId("SUBAGENT-1"), agentPath, findFreeLocalUri()))
+
+  override protected def items =
+    Seq(
+      TestWorkflow,
+      AgentRef(agentPath, subagentIds),
+      SubagentBundle(
+        SubagentBundleId("SUBAGENT-BUNDLE"),
+        Map(
+          subagentIds(0) -> NumericConstant(2),
+          subagentIds(1) -> NumericConstant(1)))
+    ) ++ subagentItems
+
+  "Cluster replicates journal files properly" in:
+    withControllerAndBackupWithoutAgents(): (primary, backup, _) =>
+      runControllers(primary, backup): (_, _) =>
+        def directorEnvResource(index: Int): ResourceIO[DirectorEnv] =
+          primary.directorEnvResource(subagentItems(index),
+            otherSubagentIds = Seq(subagentItems(1 - index).id),
+            isClusterBackup = index > 0)
+
+        directorEnvResource(0).useSync(99.s): primaryEnv =>
+          directorEnvResource(1).useSync(99.s): backupEnv =>
+            primaryEnv.testAgentResource.useSync(99.s): primaryDirector =>
+              backupEnv.testAgentResource.useSync(99.s): backupDirector =>
+                primaryDirector.eventWatch.awaitNext[ClusterNodesAppointed]()
+                primaryDirector.eventWatch.awaitNext[ClusterWatchRegistered]()
+                primaryDirector.eventWatch.awaitNext[ClusterCoupled]()
+                primaryDirector.terminate().await(99.s)
+                backupDirector.terminate().await(99.s)
+
+            primaryEnv.testAgentResource.useSync(99.s): primaryDirector =>
+              backupEnv.testAgentResource.useSync(99.s): backupDirector =>
+                primaryDirector.eventWatch.awaitNext[ClusterActiveNodeShutDown]()
+                primaryDirector.eventWatch.awaitNext[ClusterCoupled]()
+
+
+object AgentShutdownAndRecoupleClusterTest:
+  private val agentPath = AgentPath("AGENT")
+  private val subagentIds = Seq(
+    SubagentId("SUBAGENT-0"),
+    SubagentId("SUBAGENT-1"))
+
