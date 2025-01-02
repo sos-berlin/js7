6d0687edad704914e4feb8dc3e888d04382a3fc7
Joacim Zschimmer <jogit@zschimmer.com>
2025-01-02 19:51:47 +0100

    strictly: assert iff isStrict

--- js7-base/shared/src/main/scala/js7/base/utils/Assertions.scala
+++ js7-base/shared/src/main/scala/js7/base/utils/Assertions.scala
@@ -2,16 +2,34 @@ package js7.base.utils
 
 import js7.base.system.OperatingSystem.isWindows
 import js7.base.utils.ScalaUtils.syntax.*
+import js7.base.utils.Tests.isStrict
 
 object Assertions:
 
+  /** Like assertThat, but only if isStrict.
+    */
+  inline def strictly(inline predicate: sourcecode.Text[Boolean])
+    (using
+      inline fullName: sourcecode.FullName,
+      inline filename: sourcecode.FileName,
+      inline line: sourcecode.Line)
+  : Unit =
+    if isStrict then
+      assertThat(predicate)
+
   def assertThat(predicate: sourcecode.Text[Boolean])
-    (implicit fullName: sourcecode.FullName, filename: sourcecode.FileName, line: sourcecode.Line)
+    (using
+      fullName: sourcecode.FullName,
+      filename: sourcecode.FileName,
+      line: sourcecode.Line)
   : Unit =
     assertThat(predicate, "")
 
   def assertThat(predicate: sourcecode.Text[Boolean], clue: => String)
-    (implicit fullName: sourcecode.FullName, filename: sourcecode.FileName, line: sourcecode.Line)
+    (using
+      fullName: sourcecode.FullName,
+      filename: sourcecode.FileName,
+      line: sourcecode.Line)
   : Unit =
     if !predicate.value then
       val c = clue
