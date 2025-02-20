a37f8fe144ac72061b566083cd62857a3e7429e2
Joacim Zschimmer <jogit@zschimmer.com>
2025-02-20 07:07:22 +0100

    JS-2100 add test

---
+++ js7-data/shared/src/test/scala/js7/data/plan/PlanSchemaStateTest.scala
@@ -0,0 +1,62 @@
+package js7.data.plan
+
+import io.circe.syntax.EncoderOps
+import js7.base.circeutils.CirceUtils.{CompactPrinter, JsonStringInterpolator}
+import js7.base.test.OurTestSuite
+import js7.base.utils.ScalaUtils.syntax.*
+import js7.data.board.{BoardPath, PlannedBoard}
+import js7.data.controller.ControllerState
+import js7.data.item.ItemRevision
+import js7.data.order.OrderId
+import js7.data.value.StringValue
+import js7.data.value.expression.Expression
+import js7.data.value.expression.Expression.{expr, exprFun}
+
+final class PlanSchemaStateTest extends OurTestSuite:
+
+  "toSnapshotStream JSON" in :
+    val planSchemaState = PlanSchemaState(
+      PlanSchema(
+        PlanSchemaId("DailyPlan"),
+        expr"1",
+        Some(exprFun"day => $$day < $$openingDay"),
+        namedValues = Map(
+          "openingDay" -> StringValue("")),
+        Some(ItemRevision(1))),
+      namedValues = Map(
+        "openingDay" -> StringValue("2025-02-20")),
+      toPlan = Map(
+        PlanKey("2025-02-20") -> Plan(
+          PlanSchemaId("DailyPlan") / "2025-02-20",
+          Set(OrderId("#2025-02-20#")),
+          Map(
+            BoardPath("BOARD") -> PlannedBoard(
+              PlanSchemaId("DailyPlan") / "2025-02-20" / BoardPath("BOARD"),
+              toNoticePlace = Map.empty)),
+          isClosed = false)))
+
+      val snapshots = planSchemaState.toSnapshotStream
+        .map(_
+          .asJson(ControllerState.snapshotObjectJsonCodec)
+          .printWith(CompactPrinter))
+        .map(s => io.circe.parser.parse(s).orThrow)
+        .compile.toVector
+
+    assert(snapshots == List(
+      json"""{
+        "TYPE" : "PlanSchema",
+        "id" : "DailyPlan",
+        "planKeyExpr" : "1",
+        "planIsClosedFunction" : "day => $$day < $$openingDay",
+        "namedValues" : {
+          "openingDay" : ""
+        },
+        "itemRevision" : 1
+      }""",
+      json"""{
+        "TYPE" : "PlanSchemaState",
+        "id" : "DailyPlan",
+        "namedValues" : {
+          "openingDay" : "2025-02-20"
+        }
+      }"""))
