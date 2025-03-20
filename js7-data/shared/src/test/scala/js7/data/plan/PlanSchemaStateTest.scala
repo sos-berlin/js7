ee6132589547c6b070033a19bebac3328ef3ecb8
Joacim Zschimmer <jogit@zschimmer.com>
2025-03-20 21:02:35 +0100

    JS-2100 tests

--- js7-data/shared/src/test/scala/js7/data/plan/PlanSchemaStateTest.scala
+++ js7-data/shared/src/test/scala/js7/data/plan/PlanSchemaStateTest.scala
@@ -2,6 +2,7 @@ package js7.data.plan
 
 import io.circe.syntax.EncoderOps
 import js7.base.circeutils.CirceUtils.{CompactPrinter, JsonStringInterpolator}
+import js7.base.problem.Problem
 import js7.base.test.OurTestSuite
 import js7.base.time.ScalaTime.*
 import js7.base.utils.ScalaUtils.syntax.*
@@ -9,6 +10,9 @@ import js7.data.board.{BoardPath, PlannedBoard}
 import js7.data.controller.ControllerState
 import js7.data.item.ItemRevision
 import js7.data.order.OrderId
+import js7.data.plan.PlanEvent.PlanOpened
+import js7.data.plan.PlanSchemaEvent.PlanSchemaChanged
+import js7.data.plan.PlanStatus.Open
 import js7.data.value.StringValue
 import js7.data.value.expression.Expression
 import js7.data.value.expression.Expression.exprFun
@@ -38,33 +42,64 @@ final class PlanSchemaStateTest extends OurTestSuite:
               PlanSchemaId("DailyPlan") / "2025-02-20" / BoardPath("BOARD"),
               toNoticePlace = Map.empty)))))
 
-      val snapshots = planSchemaState.toSnapshotStream
-        .map(_
-          .asJson(ControllerState.snapshotObjectJsonCodec)
-          .printWith(CompactPrinter))
-        .map(s => io.circe.parser.parse(s).orThrow)
-        .compile.toVector
+    val snapshots = planSchemaState.toSnapshotStream
+      .map(_
+        .asJson(ControllerState.snapshotObjectJsonCodec)
+        .printWith(CompactPrinter))
+      .map(s => io.circe.parser.parse(s).orThrow)
+      .compile.toVector
 
     assert(snapshots == List(
       json"""{
-        "TYPE" : "PlanSchema",
-        "id" : "DailyPlan",
-        "unknownPlanIsClosedFunction" : "day => $$day < $$openingDay",
-        "namedValues" : {
-          "openingDay" : ""
+        "TYPE": "PlanSchema",
+        "id": "DailyPlan",
+        "unknownPlanIsClosedFunction": "day => $$day < $$openingDay",
+        "namedValues": {
+          "openingDay": ""
         },
-        "itemRevision" : 1
+        "itemRevision": 1
       }""",
       json"""{
-        "TYPE" : "PlanSchemaState",
-        "id" : "DailyPlan",
+        "TYPE": "PlanSchemaState",
+        "id": "DailyPlan",
         "finishedPlanRetentionPeriod": 3600,
-        "namedValues" : {
-          "openingDay" : "2025-02-20"
+        "namedValues": {
+          "openingDay": "2025-02-20"
         }
       }""",
       json"""{
-        "TYPE" : "Plan",
-        "planId" : [ "DailyPlan", "2025-02-20" ],
-        "status" : "Closed"
+        "TYPE": "Plan",
+        "planId": [ "DailyPlan", "2025-02-20" ],
+        "status": "Closed"
       }"""))
+
+  "Change PlanStatus" - {
+    lazy val planSchemaId = PlanSchemaId("DailyPlan")
+    lazy val planSchema = PlanSchema(
+      planSchemaId,
+      Some(exprFun"day => $$day < $$openingDay"),
+      namedValues = Map(
+        "openingDay" -> StringValue("")))
+
+    lazy val planSchemaState0 = planSchema.toInitialItemState.applyEvent:
+      PlanSchemaChanged(
+        finishedPlanRetentionPeriod = Some(0.s),
+        namedValues = Some(Map(
+          "openingDay" -> StringValue("2025-03-20"))))
+    .orThrow
+
+    "Open (reopen) a Plan before openingDay" in:
+      // Open (reopen) a Plan before openingDay //
+      val planSchemaState = planSchemaState0.applyPlanEvent(PlanKey("2025-03-01"), PlanOpened).orThrow
+
+      assert(planSchemaState.toPlan == Map(
+        PlanKey("2025-03-01") -> Plan(planSchemaId / PlanKey("2025-03-01"), Open)))
+      assert(planSchemaState.toSnapshotStream.compile.toList == List(
+        planSchema,
+        PlanSchemaState.Snapshot(planSchemaId, 0.s, Map("openingDay" -> StringValue("2025-03-20"))),
+        Plan.Snapshot(planSchemaId / "2025-03-01", Open)))
+
+    "Open (reopen) a Plan after openingDay" in:
+      assert(planSchemaState0.applyPlanEvent(PlanKey("2025-03-31"), PlanOpened) == Left:
+        Problem("Plan:DailyPlan╱2025-03-31 is already Open"))
+  }
---
+++ js7-data/shared/src/test/scala/js7/data/plan/PlanTest.scala
@@ -0,0 +1,83 @@
+package js7.data.plan
+
+import io.circe.Codec
+import js7.base.circeutils.CirceUtils.JsonStringInterpolator
+import js7.base.circeutils.typed.TypedJsonCodec
+import js7.base.problem.Problem
+import js7.base.test.OurTestSuite
+import js7.base.time.ScalaTime.*
+import js7.base.time.TimestampForTests.ts
+import js7.base.utils.ScalaUtils.syntax.RichEither
+import js7.data.board.NoticeEvent.NoticeDeleted
+import js7.data.board.{BoardPath, Notice, NoticeKey, PlannedBoard}
+import js7.data.order.OrderId
+import js7.data.plan.PlanEvent.{PlanClosed, PlanDeleted, PlanFinished, PlanOpened}
+import js7.data.plan.PlanStatus.{Closed, Deleted, Open}
+import js7.tester.CirceJsonTester.testJson
+
+final class PlanTest extends OurTestSuite:
+
+  "JSON" in:
+    given Codec.AsObject[Plan.Snapshot] = TypedJsonCodec(Plan.subtype)
+    testJson(
+      Plan.Snapshot(
+        PlanSchemaId("DailyPlan") / "2025-03-20",
+        Open),
+      json"""{
+        "TYPE": "Plan",
+        "planId": [ "DailyPlan", "2025-03-20" ],
+        "status": "Open"
+      }""")
+
+  "PlanStatus changes" in:
+    val now = ts"2025-03-20T12:00:00Z"
+
+    val planId = PlanSchemaId("DailyPlan") / "2025-03-20"
+    val orderId = OrderId("ORDER")
+    val boardPath = BoardPath("BOARD")
+    val noticeId = planId / boardPath / NoticeKey("NOTICE")
+
+    var plan = Plan(planId, Deleted)
+    assert(plan.addOrders(orderId :: Nil) == Left:
+      Problem(s"$planId does not accept orders because it is Deleted"))
+
+    assert(plan.changePlanStatusEvents(Open, now, 1.h).map(_.toList) == Right:
+      List(planId <-: PlanOpened))
+    plan = plan.applyEvent(PlanOpened).orThrow
+
+    assert(plan.changePlanStatusEvents(Open, now, 1.h) == Left:
+      Problem(s"$planId is already Open"))
+
+    assert(plan.changePlanStatusEvents(Closed, now, 1.h).map(_.toList) == Right:
+      List(
+        planId <-: PlanClosed,
+        planId <-: PlanFinished(now)))
+
+    assert(plan.changePlanStatusEvents(Closed, now, 0.h).map(_.toList) == Right:
+      List(
+        planId <-: PlanClosed,
+        planId <-: PlanFinished(now),
+        planId <-: PlanDeleted))
+
+    assert(plan.status == Open)
+    plan = plan.addOrders(orderId :: Nil).orThrow
+    plan = plan.addBoard:
+      PlannedBoard(planId / boardPath)
+        .addNotice(Notice(noticeId)).orThrow
+
+    assert(plan.changePlanStatusEvents(Closed, now, 1.h).map(_.toList) == Right:
+      List(
+        planId <-: PlanClosed))
+    plan = plan.applyEvent(PlanClosed).orThrow
+
+    plan = plan.removeOrders(orderId :: Nil)
+
+    assert(plan.maybePlanFinished(now, 1.h).toList ==
+      List(
+        planId <-: PlanFinished(now)))
+
+    assert(plan.maybePlanFinished(now, 0.h).toList ==
+      List(
+        planId <-: PlanFinished(now),
+        boardPath <-: NoticeDeleted(noticeId.plannedNoticeKey),
+        planId <-: PlanDeleted))
