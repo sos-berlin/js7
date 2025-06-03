a521c4a16b1291f6947c2f8fd85d35fabadff03c
Joacim Zschimmer <jogit@zschimmer.com>
2025-06-03 09:03:22 +0200

    test foldMap

--- js7-base/shared/src/test/scala/js7/base/utils/CatsTest.scala
+++ js7-base/shared/src/test/scala/js7/base/utils/CatsTest.scala
@@ -1,10 +1,35 @@
 package js7.base.utils
 
-import cats.syntax.traverse.*
+import cats.syntax.all.*
 import js7.base.test.OurTestSuite
 
 final class CatsTest extends OurTestSuite:
 
   "sequence" in:
-    val list = List(Left(1), Right(2), Left(3), Right(4))
-    assert(list.sequence == Left(1))
+    assert(List(Left(1), Right(2), Left(3)).sequence == Left(1))
+    assert(List(Right(1), Right(2), Right(3)).sequence == Right(List(1, 2, 3)))
+
+
+  "foldMap, foldMapA, foldMapM" - {
+    val mixed = List("1", "x", "3", "y")
+    val numbers = List("1", "2", "3")
+
+    def toInt(a: String) =
+      try Right(a.toInt)
+      catch case _: NumberFormatException => Left(s"Not a number: $a")
+
+    "foldMap" in:
+      // foldMap is for a Foldable
+      assert(mixed.foldMap(toInt) == Left(s"Not a number: x"))
+      assert(numbers.foldMap(toInt) == Right(6))
+
+    "foldMapM" in:
+      // foldMap is for a Monad
+      assert(mixed.foldMapM(toInt) == Left(s"Not a number: x"))
+      assert(numbers.foldMapM(toInt) == Right(6))
+
+    "foldMapA" in:
+      // foldMap is for an Applicative
+      assert(mixed.foldMapA(toInt) == Left(s"Not a number: x"))
+      assert(numbers.foldMapA(toInt) == Right(6))
+  }
