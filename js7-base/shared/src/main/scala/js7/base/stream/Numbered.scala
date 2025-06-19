e193cd5bafdad8d10f77adf891419bc43613b6e6
Joacim Zschimmer <jogit@zschimmer.com>
2025-06-19 07:24:59 +0200

    JS-2207 Numbered(0, x) serializes as x

--- js7-base/shared/src/main/scala/js7/base/stream/Numbered.scala
+++ js7-base/shared/src/main/scala/js7/base/stream/Numbered.scala
@@ -2,8 +2,12 @@ package js7.base.stream
 
 import io.circe.syntax.EncoderOps
 import io.circe.{Decoder, DecodingFailure, Encoder, Json}
+import izumi.reflect.Tag
 import js7.base.circeutils.CirceUtils.requireJson
 
+/**
+  * @param number use 0 when number is irrelevant.
+  */
 final case class Numbered[+A](number: Long, value: A):
 
   override def toString =
@@ -14,31 +18,22 @@ final case class Numbered[+A](number: Long, value: A):
 
 
 object Numbered:
-  implicit def jsonEncoder[A: Encoder]: Encoder.AsArray[Numbered[A]] =
-    stamped => Vector(
-      stamped.number.asJson,
-      stamped.value.asJson)
 
-//  implicit def jsonDecoder[A: Decoder]: Decoder[Numbered[A]] =
-//    c => {
-//      val c1 = c.downArray
-//      for {
-//        number <- c1.as[Long]
-//        a <- c1.downN(1).as[A]
-//      } yield Numbered(number, a)
-//    }
+  given jsonEncoder[A: Encoder]: Encoder[Numbered[A]] =
+    case Numbered(0, v) => v.asJson
+    case Numbered(n, v) => Json.arr(n.asJson, v.asJson)
 
-  implicit def jsonDecoder[A: Decoder]: Decoder[Numbered[A]] =
+  given jsonDecoder[A: {Decoder, Tag as A}]: Decoder[Numbered[A]] =
     c => c.values match
       case Some(fields: IndexedSeq[Json]) =>
         for
           _ <- requireJson(fields.sizeIs == 2, DecodingFailure(
-            "For Numbered, a JSON array with exactly two elements expected",
+            s"For Numbered[${A.tag}], a JSON array with exactly two elements is expected",
             c.history))
           number <- fields(0).as[Long]
           a <- fields(1).as[A]
-        yield Numbered(number, a)
+        yield
+          Numbered(number, a)
 
       case _ =>
         c.as[A].map(Numbered(0, _))
-        //Left(DecodingFailure("Numbered (a JSON array) expected", c.history))
--- js7-base/shared/src/test/scala/js7/base/stream/NumberedTest.scala
+++ js7-base/shared/src/test/scala/js7/base/stream/NumberedTest.scala
@@ -3,14 +3,15 @@ package js7.base.stream
 import io.circe.DecodingFailure
 import js7.base.circeutils.CirceUtils.JsonStringInterpolator
 import js7.base.test.OurTestSuite
-import js7.tester.CirceJsonTester.{testJson, testJsonDecoder}
+import js7.tester.CirceJsonTester.testJson
 
 final class NumberedTest extends OurTestSuite:
-  "JSON" in:
-    testJson(Numbered(1L, "ONE"),json""" [ 1, "ONE" ]""")
 
-    // A only yields Numbered[A](0, a)
-    testJsonDecoder(Numbered(0, "ONE"),json""" "ONE"""")
+  "JSON" in:
+    testJson(Numbered(0L, "NULL"), json""" "NULL" """)
+    testJson(Numbered(1L, "ONE"), json""" [ 1, "ONE" ]""")
 
     assert(json"""[ 1, "ONE", "garbage" ]""".as[Numbered[String]] ==
-      Left(DecodingFailure("For Numbered, a JSON array with exactly two elements expected", Nil)))
+      Left(DecodingFailure(
+        "For Numbered[String], a JSON array with exactly two elements is expected",
+        Nil)))
