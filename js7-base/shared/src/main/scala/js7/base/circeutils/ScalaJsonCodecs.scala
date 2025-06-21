e5d62679f1d2d98f706b882dac8eb02e547fe281
Joacim Zschimmer <jogit@zschimmer.com>
2025-06-21 06:29:20 +0200

    BitSetCodec

--- js7-base/shared/src/main/scala/js7/base/circeutils/ScalaJsonCodecs.scala
+++ js7-base/shared/src/main/scala/js7/base/circeutils/ScalaJsonCodecs.scala
@@ -1,6 +1,8 @@
 package js7.base.circeutils
 
-import io.circe.{Decoder, DecodingFailure, Encoder, Json}
+import io.circe.syntax.EncoderOps
+import io.circe.{Codec, Decoder, DecodingFailure, Encoder, Json}
+import scala.collection.immutable.BitSet
 import scala.concurrent.duration.*
 
 /**
@@ -21,3 +23,12 @@ object ScalaJsonCodecs:
               case Some(bigDecimal) => Right(Duration((bigDecimal * 1000000000).toLong, NANOSECONDS))
               case None => Left(DecodingFailure(s"Unrecognized duration: ${cursor.value}", cursor.history))
       case None => Left(DecodingFailure(s"Unrecognized FiniteDuration: ${cursor.value}", cursor.history))
+
+
+  given BitSetCodec: Codec[BitSet] = Codec.from(BitSetDecoder, BitSetEncoder)
+
+  given BitSetEncoder: Encoder[BitSet] =
+    bitSet => Json.fromValues(bitSet.view.map(_.asJson))
+
+  given BitSetDecoder: Decoder[BitSet] =
+    _.value.as[Array[Int]].map(_.to(BitSet))
