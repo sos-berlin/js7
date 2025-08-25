546508bd82232b2b92f44b57a473eca353029003
Joacim Zschimmer <jogit@zschimmer.com>
2025-08-25 08:27:41 +0200

    StreamIO, StreamPure

--- js7-base/shared/src/main/scala/js7/base/fs2utils/Fs2Utils.scala
+++ js7-base/shared/src/main/scala/js7/base/fs2utils/Fs2Utils.scala
@@ -5,6 +5,9 @@ import fs2.Pipe
 
 object Fs2Utils:
 
+  type StreamIO[A] = fs2.Stream[IO, A]
+  type StreamPure[A] = fs2.Stream[fs2.Pure, A]
+
   def bytesToLines: Pipe[IO, Byte, String] =
     _.through(fs2.text.utf8.decode)
       .through(fs2.text.lines)
