a1159852ed39528c6200a226dc9c87edb14be3c6
Joacim Zschimmer <jogit@zschimmer.com>
2025-09-11 13:37:41 +0200

    safer haltJava

--- js7-base/jvm/src/main/scala/js7/base/system/startup/Halt.scala
+++ js7-base/jvm/src/main/scala/js7/base/system/startup/Halt.scala
@@ -19,9 +19,16 @@ object Halt:
 
   private def haltJava2(msg: String, exitCode: Int = Js7ReturnCodes.Halt, warnOnly: Boolean)
   : Nothing =
-    System.err.println()
+    try System.err.println()
+    catch case _: Throwable => {}
+
     printlnWithClockIgnoringException(msg)
-    if warnOnly then logger.warn(msg) else logger.error(msg)
-    Log4j.shutdown(fast = true)
+
+    try if warnOnly then logger.warn(msg) else logger.error(msg)
+    catch case _: Throwable => {}
+
+    try Log4j.shutdown(fast = true)
+    catch case _: Throwable => {}
+
     sys.runtime.halt(exitCode)
     throw new Error("sys.runtime.halt failed")
