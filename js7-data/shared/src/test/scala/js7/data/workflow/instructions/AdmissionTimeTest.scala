db76e3eca8ac55c8380a78b9f4f9c612ceda23e8
Joacim Zschimmer <jogit@zschimmer.com>
2025-10-20 12:44:04 +0200

    JS-2204 AdmissionTime JSON test

---
+++ js7-data/shared/src/test/scala/js7/data/workflow/instructions/AdmissionTimeTest.scala
@@ -0,0 +1,32 @@
+package js7.data.workflow.instructions
+
+import js7.base.circeutils.CirceUtils.JsonStringInterpolator
+import js7.base.test.OurTestSuite
+import js7.base.time.{AdmissionTimeScheme, DailyPeriod}
+import js7.data.workflow.{Instruction, Workflow}
+import js7.tester.CirceJsonTester.testJson
+import scala.concurrent.duration.*
+
+final class AdmissionTimeTest extends OurTestSuite:
+
+  "JSON" in:
+    import Instructions.jsonCodec
+    testJson[Instruction](
+      AdmissionTime(AdmissionTimeScheme(Seq(DailyPeriod(3600, 1.minute)))):
+        Workflow.empty,
+      json"""{
+        "TYPE": "AdmissionTime",
+         "admissionTimeScheme": {
+           "periods": [
+             {
+               "TYPE": "DailyPeriod",
+               "duration": 60,
+               "secondOfDay": 3600
+             }
+           ]
+         },
+         "block": {
+           "instructions": []
+         }
+       }"""
+    )
