6a5d7a59c99ff12ebe52cfee0de8736e3197e861
Joacim Zschimmer <jogit@zschimmer.com>
2026-02-20 07:35:27 +0100

    JS-2155 Java 17 polyfill: CharSequence.getChars

--- js7-base/shared/src/main/scala/js7/base/system/Java17Polyfill.scala
+++ js7-base/shared/src/main/scala/js7/base/system/Java17Polyfill.scala
@@ -8,6 +8,19 @@ object Java17Polyfill:
    * and imports should be kept. */
   def java17Polyfill(): Unit = {}
 
+  extension (charSequence: CharSequence)
+    /** Since Java 25. */
+    def getChars(srcBegin: Int, srcEnd: Int, dst: Array[Char], dstBegin: Int): Unit =
+      charSequence match
+        case string: String =>
+          string.getChars(srcBegin, srcEnd, dst, dstBegin)
+        case _ =>
+          var i = srcBegin
+          val off = dstBegin - srcBegin
+          while i < srcEnd do
+            dst(off + i) = charSequence.charAt(i)
+            i += 1
+
   extension (thread: Thread)
     /** Since Java 19. */
     def threadId: Long =
--- js7-base/shared/src/test/scala/js7/base/utils/Java17PolyfillTest.scala
+++ js7-base/shared/src/test/scala/js7/base/utils/Java17PolyfillTest.scala
@@ -6,8 +6,20 @@ import scala.annotation.nowarn
 
 final class Java17PolyfillTest extends OurTestSuite:
 
+  java17Polyfill() // require import
+
   "javaVersion" in:
     assert(Thread.currentThread.threadId ==
       (Thread.currentThread().getId(): @nowarn("msg=deprecated")))
 
-  java17Polyfill()
+  "CharSequence" - {
+    "getChars" in:
+      val string = "abcd"
+      val array = Array.fill(4)('•')
+
+      (string: CharSequence).getChars(1, 3, array, 1)
+      assert(array.mkString == "•bc•")
+
+      assert:
+        (string: CharSequence).getChars(1, 3, array, 1) == string.getChars(1, 3, array, 1)
+  }
