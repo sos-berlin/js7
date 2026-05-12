eeb907886da3dad4cc3f3a901610214f29cec052
Joacim Zschimmer <jogit@zschimmer.com>
2026-05-12 11:33:04 +0200

    Optimise Proxy event throughput: test

--- js7-tests/src/test/scala/js7/tests/controller/proxy/JournaledProxyTest.scala
+++ js7-tests/src/test/scala/js7/tests/controller/proxy/JournaledProxyTest.scala
@@ -1,11 +1,15 @@
 package js7.tests.controller.proxy
 
+import cats.effect.IO
 import fs2.Stream
 import js7.base.Js7Version
 import js7.base.auth.Admission
+import js7.base.catsutils.CatsEffectExtensions.orThrow
 import js7.base.configutils.Configs.*
 import js7.base.fs2utils.StreamExtensions.*
 import js7.base.generic.Completed
+import js7.base.log.AnsiEscapeCodes.bold
+import js7.base.log.Logger
 import js7.base.problem.Checked.*
 import js7.base.problem.Problem
 import js7.base.system.OperatingSystem.isWindows
@@ -13,9 +17,12 @@ import js7.base.test.OurTestSuite
 import js7.base.thread.CatsBlocking.syntax.*
 import js7.base.thread.Futures.implicits.SuccessFuture
 import js7.base.time.ScalaTime.*
+import js7.base.time.Stopwatch.itemsPerSecondString
 import js7.base.utils.CatsUtils.Nel
 import js7.base.utils.CatsUtils.syntax.*
 import js7.base.utils.Lazy
+import js7.base.utils.ScalaUtils.syntax.foldMap
+import js7.base.utils.Tests.isIntelliJIdea
 import js7.common.pekkoutils.ProvideActorSystem
 import js7.controller.client.PekkoHttpControllerApi
 import js7.data.Problems.ItemVersionDoesNotMatchProblem
@@ -27,9 +34,13 @@ import js7.data.item.VersionedEvent.VersionedItemAdded
 import js7.data.job.RelativePathExecutable
 import js7.data.order.OrderEvent.{OrderAdded, OrderAttachable, OrderAttached, OrderDetachable, OrderDetached, OrderFailed, OrderFinished, OrderMoved, OrderProcessed, OrderProcessingStarted, OrderStarted, OrderStdoutWritten, OrderTerminated}
 import js7.data.order.OrderOutcome.Succeeded
-import js7.data.order.{FreshOrder, OrderEvent, OrderId}
+import js7.data.order.{FreshOrder, OrderEvent, OrderId, OrderOutcome}
 import js7.data.value.NamedValues
+import js7.data.workflow.Workflow
 import js7.data.workflow.position.Position
+import js7.data_for_java.reactor.ReactorConverters.asFlux
+import js7.launcher.OrderProcess
+import js7.launcher.internal.InternalJob
 import js7.proxy.ControllerApi
 import js7.proxy.configuration.ProxyConfs
 import js7.proxy.data.event.EventAndState
@@ -40,6 +51,7 @@ import js7.tests.testenv.ControllerAgentForScalaTest
 import js7.tests.testenv.DirectoryProvider.{script, toLocalSubagentId}
 import org.apache.pekko.actor.ActorSystem
 import org.scalatest.BeforeAndAfterAll
+import scala.concurrent.duration.Deadline
 
 final class JournaledProxyTest
 extends OurTestSuite, BeforeAndAfterAll, ProvideActorSystem, ControllerAgentForScalaTest:
@@ -54,7 +66,13 @@ extends OurTestSuite, BeforeAndAfterAll, ProvideActorSystem, ControllerAgentForS
         password = "plain:PROXYS-PASSWORD-FOR-PRIMARY"
         permissions = [ UpdateItem ]
       }
+      TEST-USER.permissions = [ UpdateItem ]
     }
+    """
+
+  override protected def agentConfig = config"""
+    js7.job.execution.signed-script-injection-allowed = on
+    js7.order.stdout-stderr.event-size = $stdoutEventSize
     """
 
   private implicit def implicitActorSystem: ActorSystem = actorSystem
@@ -144,7 +162,42 @@ extends OurTestSuite, BeforeAndAfterAll, ProvideActorSystem, ControllerAgentForS
     assert(proxy.currentState.keyTo(AgentRefState)(agentPath)
       .platformInfo.map(_.js7Version) contains Js7Version)
 
+  "Speed with Flux" in:
+    if isIntelliJIdea then
+      withItem(
+        Workflow.of:
+          SpeedTestJob.execute(agentPath)
+      ): workflow =>
+        val orderId = OrderId("SPEED")
+        val t = Deadline.now
+        val n =
+          proxy.stream()
+            .onStart:
+              controller.api.addOrder(FreshOrder(orderId, workflow.path, deleteWhenTerminated = true))
+                .orThrow.void
+            .asFlux
+            .takeUntil:
+              case EventAndState(Stamped(_, _, KeyedEvent(o, _: OrderTerminated)), _, _) =>
+                o == orderId
+              case _ => false
+            .count
+            .block()
+        logger.info(bold(itemsPerSecondString(t.elapsed, n, "events")))
+
 
 object JournaledProxyTest:
+  private val logger = Logger[this.type]
+  private val stdoutEventSize = 100
   private val agentPath = AgentPath("AGENT")
   private val subagentId = toLocalSubagentId(agentPath)
+
+  final class SpeedTestJob extends InternalJob:
+    // Emit many tiny events, as fast as possible
+    def toOrderProcess(step: Step): OrderProcess =
+      OrderProcess:
+        val line = ("+" * (stdoutEventSize - 1) + "\n") * 100
+        (1 to 10_000).foldMap: _ =>
+          step.writeOut(line).void
+        .as(OrderOutcome.succeeded)
+
+  object SpeedTestJob extends InternalJob.Companion[SpeedTestJob]
