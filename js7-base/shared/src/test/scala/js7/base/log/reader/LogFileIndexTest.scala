afb381e6ee919e49050f11fbf09d967915bb002c
Joacim Zschimmer <jogit@zschimmer.com>
2026-07-27 10:21:47 +0200

    JS-2155 fix test

--- js7-base/shared/src/test/scala/js7/base/log/reader/LogFileIndexTest.scala
+++ js7-base/shared/src/test/scala/js7/base/log/reader/LogFileIndexTest.scala
@@ -218,7 +218,7 @@ object LogFileIndexTest:
           if gzip then new GZIPOutputStream(out) else out,
           UTF_8)
     .use: writer =>
-      writer.write(s"$startTime ...\n")
+      writer.write(s"$startTime Begin ...\n")
       IO.blocking:
         val t = Deadline.now
         meterWrite:
