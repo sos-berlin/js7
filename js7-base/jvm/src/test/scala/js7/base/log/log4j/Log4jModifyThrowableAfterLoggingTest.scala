9e43d24fe9f92eb4801f1eeed1e713cbf84c9f37
Joacim Zschimmer <jogit@zschimmer.com>
2026-08-11 09:47:56 +0200

    Disable test of old log4j

--- js7-base/jvm/src/test/scala/js7/base/log/log4j/Log4jModifyThrowableAfterLoggingTest.scala
+++ js7-base/jvm/src/test/scala/js7/base/log/log4j/Log4jModifyThrowableAfterLoggingTest.scala
@@ -1,16 +1,19 @@
-package js7.base.log.log4j
-
-import js7.base.test.OurTestSuite
-import org.apache.logging.log4j.LogManager
-
-final class Log4jModifyThrowableAfterLoggingTest extends OurTestSuite:
-
-  "test" in:
-    final class MyException extends Exception:
-      private var i = 0
-      override def hashCode(): Int =
-        i += 1
-        i
-
-    val logger = LogManager.getLogger("TestLog4jNullPointerException")
-    logger.error("TEST", new MyException)
+//package js7.base.log.log4j
+//
+//import js7.base.test.OurTestSuite
+//import org.apache.logging.log4j.LogManager
+//
+// This test was for log4j before 2.26.1.
+// Disabled, because it may log an ugly stack trace to stdout.
+//
+//final class Log4jModifyThrowableAfterLoggingTest extends OurTestSuite:
+//
+//  "test" in:
+//    final class MyException extends Exception:
+//      private var i = 0
+//      override def hashCode(): Int =
+//        i += 1
+//        i
+//
+//    val logger = LogManager.getLogger("TestLog4jNullPointerException")
+//    logger.error("TEST", new MyException)
